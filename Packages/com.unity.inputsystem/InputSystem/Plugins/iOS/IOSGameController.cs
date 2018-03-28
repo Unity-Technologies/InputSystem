34b9fb83045c8ee4853500b1899afa436d838553
Tomas Dirvanauskas <tomas@unity3d.com>
2018-03-28 14:04:09 +0300

    [iOS] Refactor iOSGameController

--- Packages/com.unity.inputsystem/InputSystem/Plugins/iOS/IOSGameController.cs
+++ Packages/com.unity.inputsystem/InputSystem/Plugins/iOS/IOSGameController.cs
@@ -5,66 +5,90 @@ using ISX.Utilities;
 
 namespace ISX.Plugins.iOS.LowLevel
 {
+    public enum IOSButton
+    {
+        DpadUp,
+        DpadDown,
+        DpadLeft,
+        DpadRight,
+        LeftStick,
+        RightStick,
+        LeftShoulder,
+        RightShoulder,
+        LeftTrigger,
+        RightTrigger,
+        X,
+        Y,
+        A,
+        B
+    };
+
+    public enum IOSAxis
+    {
+        LeftStickX,
+        LeftStickY,
+        RightStickX,
+        RightStickY
+    };
+    
     [StructLayout(LayoutKind.Sequential)]
     public unsafe struct IOSGameControllerState : IInputStateTypeInfo
     {
-        enum Button
-        {
-            DpadUp,
-            DpadDown,
-            DpadLeft,
-            DpadRight,
-            LeftStick,
-            RightStick,
-            LeftShoulder,
-            RightShoulder,
-            LeftTrigger,
-            RightTrigger,
-            X,
-            Y,
-            A,
-            B
-        };
-
-        enum Axis
-        {
-            LeftStickX,
-            LeftStickY,
-            RightStickX,
-            RightStickY
-        };
-
         public static FourCC kFormat = new FourCC('I', 'G', 'C', ' ');
         public const int kMaxButtons = 14;
         public const int kMaxAxis = 4;
 
         [InputControl(name = "dpad")]
-        [InputControl(name = "dpad/up", bit = (uint)Button.DpadUp)]
-        [InputControl(name = "dpad/right", bit = (uint)Button.DpadRight)]
-        [InputControl(name = "dpad/down", bit = (uint)Button.DpadDown)]
-        [InputControl(name = "dpad/left", bit = (uint)Button.DpadLeft)]
-        [InputControl(name = "buttonSouth", bit = (uint)Button.A)]
-        [InputControl(name = "buttonWest", bit = (uint)Button.X)]
-        [InputControl(name = "buttonNorth", bit = (uint)Button.Y)]
-        [InputControl(name = "buttonEast", bit = (uint)Button.B)]
-        [InputControl(name = "leftStickPress", bit = (uint)Button.LeftStick)]
-        [InputControl(name = "rightStickPress", bit = (uint)Button.RightStick)]
-        [InputControl(name = "leftShoulder", bit = (uint)Button.LeftShoulder)]
-        [InputControl(name = "rightShoulder", bit = (uint)Button.RightShoulder)]
+        [InputControl(name = "dpad/up", bit = (uint)IOSButton.DpadUp)]
+        [InputControl(name = "dpad/right", bit = (uint)IOSButton.DpadRight)]
+        [InputControl(name = "dpad/down", bit = (uint)IOSButton.DpadDown)]
+        [InputControl(name = "dpad/left", bit = (uint)IOSButton.DpadLeft)]
+        [InputControl(name = "buttonSouth", bit = (uint)IOSButton.A)]
+        [InputControl(name = "buttonWest", bit = (uint)IOSButton.X)]
+        [InputControl(name = "buttonNorth", bit = (uint)IOSButton.Y)]
+        [InputControl(name = "buttonEast", bit = (uint)IOSButton.B)]
+        [InputControl(name = "leftStickPress", bit = (uint)IOSButton.LeftStick)]
+        [InputControl(name = "rightStickPress", bit = (uint)IOSButton.RightStick)]
+        [InputControl(name = "leftShoulder", bit = (uint)IOSButton.LeftShoulder)]
+        [InputControl(name = "rightShoulder", bit = (uint)IOSButton.RightShoulder)]
         public uint buttons;
         
-        [InputControl(name = "leftTrigger", offset = sizeof(uint) + sizeof(float) * (uint)Button.LeftTrigger)]
-        [InputControl(name = "rightTrigger", offset = sizeof(uint) + sizeof(float) * (uint)Button.RightTrigger)]
+        [InputControl(name = "leftTrigger", offset = sizeof(uint) + sizeof(float) * (uint)IOSButton.LeftTrigger)]
+        [InputControl(name = "rightTrigger", offset = sizeof(uint) + sizeof(float) * (uint)IOSButton.RightTrigger)]
         public fixed float buttonValues[kMaxButtons];
 
         private const uint kAxisOffset = sizeof(uint) + sizeof(float) * kMaxButtons;
-        [InputControl(name = "leftStick", offset = (uint)Axis.LeftStickX * sizeof(float) + kAxisOffset)]
-        [InputControl(name = "rightStick", offset = (uint)Axis.RightStickX * sizeof(float) + kAxisOffset)]
+        [InputControl(name = "leftStick", offset = (uint)IOSAxis.LeftStickX * sizeof(float) + kAxisOffset)]
+        [InputControl(name = "rightStick", offset = (uint)IOSAxis.RightStickX * sizeof(float) + kAxisOffset)]
         public fixed float axisValues[kMaxAxis];
 
         public FourCC GetFormat()
         {
             return kFormat;
+        }
+        
+        public IOSGameControllerState WithButton(IOSButton button, bool value = true, float rawValue = 1.0f)
+        {
+            fixed(float* buttonsPtr = buttonValues)
+            {
+                buttonsPtr[(int)button] = rawValue;
+            }
+
+            if (value)
+                buttons |= (uint) 1 << (int) button;
+            else
+                buttons &= ~(uint) 1 << (int)button;
+            
+            return this;
+        }
+
+        public IOSGameControllerState WithAxis(IOSAxis axis, float value)
+        {
+            fixed(float* axisPtr = this.axisValues)
+            {
+                axisPtr[(int)axis] = value;
+            }
+            return this;
         }
     }
 }
