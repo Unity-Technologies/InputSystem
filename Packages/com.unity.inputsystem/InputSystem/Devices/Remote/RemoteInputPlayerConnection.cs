53c3f7a6de9652bd7426899819f2e9150935a475
dougp <dougp@unity3d.com>
2018-05-11 10:21:48 -0400

    FIX: Udpated ifdefs for 4.x to include NET_STANDARD_2_0)

--- Packages/com.unity.inputsystem/InputSystem/Devices/Remote/RemoteInputPlayerConnection.cs
+++ Packages/com.unity.inputsystem/InputSystem/Devices/Remote/RemoteInputPlayerConnection.cs
@@ -2,7 +2,7 @@ using System;
 using UnityEngine.Experimental.Input.Utilities;
 using UnityEngine.Networking.PlayerConnection;
 
-#if !(NET_4_0 || NET_4_6)
+#if !(NET_4_0 || NET_4_6 || NET_STANDARD_2_0)
 using UnityEngine.Experimental.Input.Net35Compatibility;
 #endif
 
