f508e62ec349bfc75fae02e6c2a2186dd4acc199
Tomas <tomas@unity3d.com>
2018-05-15 11:44:36 +0300

    fix auto merge issue

--- Packages/com.unity.inputsystem/Tests/InputSystem/CoreTests.cs
+++ Packages/com.unity.inputsystem/Tests/InputSystem/CoreTests.cs
@@ -29,19 +29,6 @@ using UnityEngine.Experimental.Input.Net35Compatibility;
 // of the system (e.g. they make assumptions about how Gamepad is set up).
 class CoreTests : InputTestFixture
 {
-    class Vector3Comparer : IComparer<Vector3>
-    {
-        private float m_Epsilon;
-        public Vector3Comparer(float epsilon)
-        {
-            m_Epsilon = epsilon;
-        }
-
-        public int Compare(Vector3 a, Vector3 b)
-        {
-            return Math.Abs(a.x - b.x) < m_Epsilon && Math.Abs(a.y - b.y) < m_Epsilon && Math.Abs(a.z - b.z) < m_Epsilon ? 0 : 1;
-        }
-    }
     // The test categories give the feature area associated with the test:
     //
     //     a) Controls
@@ -4296,7 +4283,6 @@ class CoreTests : InputTestFixture
     [Category("Devices")]
     public void Devices_CanGetGyroReading()
     {
-        ////FIXME: Move non gyro values to other tests
         var gyro = InputSystem.AddDevice<Gyroscope>();
         var value = new Vector3(0.987f, 0.654f, 0.321f);
         InputSystem.QueueStateEvent(gyro, new GyroscopeState {angularVelocity = value});
@@ -4343,7 +4329,6 @@ class CoreTests : InputTestFixture
 
         Assert.That(LinearAcceleration.current, Is.SameAs(sensor));
         Assert.That(sensor.acceleration.ReadValue(), Is.EqualTo(value).Within(0.00001));
-
     }
 
     private void ValidateSensorControl(Vector3Control control, Vector3 targetValue)
@@ -4422,88 +4407,6 @@ class CoreTests : InputTestFixture
         Assert.That(sensor.attitude.ReadValue().eulerAngles, Is.EqualTo(new Vector3(angles.x, angles.y, angles.z + 90)).Using(new Vector3Comparer(0.0001f)));
 
         testRuntime.screenOrientation = ScreenOrientation.Portrait;
-        Assert.That(sensor.attitude.ReadValue().eulerAngles, Is.EqualTo(angles).Using(new Vector3Comparer(0.0001f)));
-
-        InputConfiguration.CompensateSensorsForScreenOrientation = false;
-        Assert.That(sensor.attitude.ReadValue().eulerAngles, Is.EqualTo(angles).Using(new Vector3Comparer(0.0001f)));
-    }
-
-    private void ValidateSensorControl(Vector3Control control, Vector3 targetValue)
-    {
-        InputConfiguration.CompensateSensorsForScreenOrientation = true;
-
-        InputRuntime.s_Instance.screenOrientation = ScreenOrientation.LandscapeLeft;
-        Assert.That(control.ReadValue(), Is.EqualTo(new Vector3(-targetValue.y, targetValue.x, targetValue.z)).Using(new Vector3Comparer(0.0001f)));
-
-        InputRuntime.s_Instance.screenOrientation = ScreenOrientation.PortraitUpsideDown;
-        Assert.That(control.ReadValue(), Is.EqualTo(new Vector3(-targetValue.x, -targetValue.y, targetValue.z)).Using(new Vector3Comparer(0.0001f)));
-
-        InputRuntime.s_Instance.screenOrientation = ScreenOrientation.LandscapeRight;
-        Assert.That(control.ReadValue(), Is.EqualTo(new Vector3(targetValue.y, -targetValue.x, targetValue.z)).Using(new Vector3Comparer(0.0001f)));
-
-        InputRuntime.s_Instance.screenOrientation = ScreenOrientation.Portrait;
-        Assert.That(control.ReadValue(), Is.EqualTo(targetValue).Using(new Vector3Comparer(0.0001f)));
-
-        InputConfiguration.CompensateSensorsForScreenOrientation = false;
-        Assert.That(control.ReadValue(), Is.EqualTo(targetValue).Using(new Vector3Comparer(0.0001f)));
-    }
-
-    [Test]
-    [Category("Devices")]
-    public void Devices_CanCompensateAccelerometerValues()
-    {
-        var sensor = InputSystem.AddDevice<Accelerometer>();
-        var value = new Vector3(0.123f, 0.456f, 0.789f);
-        InputSystem.QueueStateEvent(sensor, new AccelerometerState { acceleration = value });
-        InputSystem.Update();
-
-        ValidateSensorControl(sensor.acceleration, value);
-    }
-
-    [Test]
-    [Category("Devices")]
-    public void Devices_CanCompensateGyroValues()
-    {
-        var sensor = InputSystem.AddDevice<Gyroscope>();
-        var value = new Vector3(0.123f, 0.456f, 0.789f);
-        InputSystem.QueueStateEvent(sensor, new GyroscopeState { angularVelocity = value });
-        InputSystem.Update();
-
-        ValidateSensorControl(sensor.angularVelocity, value);
-    }
-
-    [Test]
-    [Category("Devices")]
-    public void Devices_CanCompensateGravityValues()
-    {
-        var sensor = InputSystem.AddDevice<Gravity>();
-        var value = new Vector3(0.123f, 0.456f, 0.789f);
-        InputSystem.QueueStateEvent(sensor, new GravityState { gravity = value });
-        InputSystem.Update();
-
-        ValidateSensorControl(sensor.gravity, value);
-    }
-
-    [Test]
-    [Category("Devices")]
-    public void Devices_CanCompensateAttitudeValues()
-    {
-        var sensor = InputSystem.AddDevice<Attitude>();
-        var angles = new Vector3(11, 22, 33);
-        InputSystem.QueueStateEvent(sensor, new AttitudeState { attitude = Quaternion.Euler(angles) });
-        InputSystem.Update();
-
-        InputConfiguration.CompensateSensorsForScreenOrientation = true;
-        InputRuntime.s_Instance.screenOrientation = ScreenOrientation.LandscapeLeft;
-        Assert.That(sensor.attitude.ReadValue().eulerAngles, Is.EqualTo(new Vector3(angles.x, angles.y, angles.z + 270)).Using(new Vector3Comparer(0.0001f)));
-
-        InputRuntime.s_Instance.screenOrientation = ScreenOrientation.PortraitUpsideDown;
-        Assert.That(sensor.attitude.ReadValue().eulerAngles, Is.EqualTo(new Vector3(angles.x, angles.y, angles.z + 180)).Using(new Vector3Comparer(0.0001f)));
-
-        InputRuntime.s_Instance.screenOrientation = ScreenOrientation.LandscapeRight;
-        Assert.That(sensor.attitude.ReadValue().eulerAngles, Is.EqualTo(new Vector3(angles.x, angles.y, angles.z + 90)).Using(new Vector3Comparer(0.0001f)));
-
-        InputRuntime.s_Instance.screenOrientation = ScreenOrientation.Portrait;
         Assert.That(sensor.attitude.ReadValue().eulerAngles, Is.EqualTo(angles).Using(new Vector3Comparer(0.0001f)));
 
         InputConfiguration.CompensateSensorsForScreenOrientation = false;
