d32e97f239a63ebfba74f6b77b82b5da1d62e4b0
Tom Baird <tomb@unity3d.com>
2018-06-15 14:43:42 -0700

    Removed all unused elements of Buffered Rumble

--- Packages/com.unity.inputsystem/InputSystem/Plugins/XR/Haptics/BufferedRumble.cs
+++ Packages/com.unity.inputsystem/InputSystem/Plugins/XR/Haptics/BufferedRumble.cs
@@ -5,46 +5,6 @@ namespace UnityEngine.Experimental.Input.Plugins.XR.Haptics
         public HapticCapabilities capabilities { get; private set; }
         InputDevice device { get; set; }
 
-        bool m_IsPaused;
-        /// <summary>
-        /// This allows you to pause the actual device motors.  This doesn't affect the current intensity, but prevents that intensity from being sent to the device.
-        /// </summary>
-        public bool isPaused
-        {
-            get
-            {
-                return m_IsPaused;
-            }
-            set
-            {
-                if (m_IsPaused != value)
-                {
-                    m_IsPaused = value;
-                    UpdateMotorSpeed();
-                }
-            }
-        }
-
-        /// <summary>
-        /// A quick accessor to verify that the intensity is greater than 0, and that the rumble motor is not paused.
-        /// </summary>
-        public bool isRumbling
-        {
-            get
-            {
-                return !m_IsPaused;
-            }
-        }
-
-        /// <summary>
-        /// Resets the rumble motor state to defaults, which is an intensity of 0 and unpaused.
-        /// </summary>
-        public void Reset()
-        {
-            m_IsPaused = false;
-            UpdateMotorSpeed();
-        }
-
         public BufferedRumble(InputDevice device)
         {
             this.device = device;
@@ -52,12 +12,12 @@ namespace UnityEngine.Experimental.Input.Plugins.XR.Haptics
             var command = GetHapticCapabilitiesCommand.Create();
             device.ExecuteCommand(ref command);
             capabilities = command.capabilities;
-
-            m_IsPaused = false;
         }
 
-        private void UpdateMotorSpeed()
+        public void EnqueueRumble(int channel, byte[] samples)
         {
+            var command = SendBufferedHapticCommand.Create(channel, samples);
+            device.ExecuteCommand(ref command);
         }
     }
 }
