80104f4abb8c5baf2fab0577d82c83f338fd834b
Tomas Dirvanauskas <tomas@unity3d.com>
2018-08-31 11:46:01 +0300

    Fix formatiing

--- Packages/com.unity.inputsystem/InputSystem/Devices/Sensor.cs
+++ Packages/com.unity.inputsystem/InputSystem/Devices/Sensor.cs
@@ -48,7 +48,6 @@ namespace UnityEngine.Experimental.Input.LowLevel
             get { return new FourCC('G', 'R', 'V', ' '); }
         }
 
-
         [InputControl(processors = "CompensateDirection")]
         public Vector3 gravity;
 
