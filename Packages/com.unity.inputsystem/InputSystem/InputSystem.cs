4a1c3f1196624d157bfa739246ec06e485f61969
dougp <dougp@unity3d.com>
2018-09-11 13:39:48 -0400

    PUBLISH: 0.0.7

--- Packages/com.unity.inputsystem/InputSystem/InputSystem.cs
+++ Packages/com.unity.inputsystem/InputSystem/InputSystem.cs
@@ -42,7 +42,7 @@ using UnityEngine.Experimental.Input.Net35Compatibility;
 // Keep this in sync with "Packages/com.unity.inputsystem/package.json".
 // NOTE: Unfortunately, System.Version doesn't use semantic versioning so we can't include
 //       "-preview" suffixes here.
-[assembly: AssemblyVersion("0.0.6")]
+[assembly: AssemblyVersion("0.0.7")]
 
 namespace UnityEngine.Experimental.Input
 {
