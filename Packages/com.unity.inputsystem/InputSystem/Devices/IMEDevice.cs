755aabb673d4dae11082e082a509dbc3df6bc5fb
Tom Baird <tomb@unity3d.com>
2018-09-12 09:19:26 -0700

    Forgotten file while moving IMECompositionStringEvent.

--- Packages/com.unity.inputsystem/InputSystem/Devices/IMEDevice.cs
+++ Packages/com.unity.inputsystem/InputSystem/Devices/IMEDevice.cs
@@ -7,41 +7,6 @@ using Unity.Collections.LowLevel.Unsafe;
 
 namespace UnityEngine.Experimental.Input.LowLevel
 {
-    [StructLayout(LayoutKind.Explicit, Size = InputEvent.kBaseEventSize + sizeof(int) + (sizeof(char) * kIMECharBufferSize))]
-    public unsafe struct IMECompositionStringEvent : IInputEventTypeInfo
-    {
-        // These needs to match the native ImeCompositionStringInputEventData settings
-        public const int kIMECharBufferSize = 16;
-        public const int Type = 0x494D4553;
-
-        [FieldOffset(0)]
-        public InputEvent baseEvent;
-
-        [FieldOffset(InputEvent.kBaseEventSize)]
-        public int size;
-
-        [FieldOffset(InputEvent.kBaseEventSize + sizeof(int))]
-        public fixed char buffer[kIMECharBufferSize];
-
-        public unsafe string AsString()
-        {
-            if (size == 0)
-                return "";
-
-            string result;
-            fixed (char* b = buffer)
-            {
-                result = new string(b, 0, size);
-            }
-            return result;
-        }
-
-        public FourCC GetTypeStatic()
-        {
-            return Type;
-        }
-    }
-
     [StructLayout(LayoutKind.Sequential)]
     public unsafe struct IMEDeviceState : IInputStateTypeInfo
     {
