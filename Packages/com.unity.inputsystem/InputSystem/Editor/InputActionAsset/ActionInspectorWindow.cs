f0e575a4ec4919597924abe012f5eb91c1480a0a
Tomek Paszek <tomek@unity3d.com>
2018-09-12 14:35:56 +0200

    Fixed changes loss in an unsaved asset on domain reload

--- Packages/com.unity.inputsystem/InputSystem/Editor/InputActionAsset/ActionInspectorWindow.cs
+++ Packages/com.unity.inputsystem/InputSystem/Editor/InputActionAsset/ActionInspectorWindow.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using UnityEditor;
 using UnityEditor.Callbacks;
 using UnityEditor.IMGUI.Controls;
-using UnityEngine.Experimental.Input.Utilities;
 
 namespace UnityEngine.Experimental.Input.Editor
 {
@@ -161,11 +160,15 @@ namespace UnityEngine.Experimental.Input.Editor
                 InitializeReferenceToImportedAssetObject();
             }
 
-            // Duplicate the asset along 1:1. Unlike calling Clone(), this will also preserve
-            // GUIDs.
-            m_AssetObjectForEditing = Instantiate(m_ImportedAssetObject);
-            m_AssetObjectForEditing.hideFlags = HideFlags.HideAndDontSave;
-            m_AssetObjectForEditing.name = m_ImportedAssetObject.name;
+            if (m_AssetObjectForEditing == null)
+            {
+                // Duplicate the asset along 1:1. Unlike calling Clone(), this will also preserve
+                // GUIDs.
+                m_AssetObjectForEditing = Instantiate(m_ImportedAssetObject);
+                m_AssetObjectForEditing.hideFlags = HideFlags.HideAndDontSave;
+                m_AssetObjectForEditing.name = m_ImportedAssetObject.name;
+            }
+
             m_AssetJson = null;
             m_SerializedObject = new SerializedObject(m_AssetObjectForEditing);
         }
