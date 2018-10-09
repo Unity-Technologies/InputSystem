09e9ce79afbacf19887cabef6fd8e468afa9f733
Yusuke Ebata <yusuke@unity3d.com>
2018-10-10 06:21:30 +0900

    FIX: Colors being read incorrectly on Switch controller (#278).

--- Packages/com.unity.inputsystem/Tests/InputSystem/Plugins/SwitchTests.cs
+++ Packages/com.unity.inputsystem/Tests/InputSystem/Plugins/SwitchTests.cs
@@ -92,6 +92,11 @@ public class SwitchTests : InputTestFixture
                         ((NPadStatusReport*)commandPtr)->npadId = NPad.NpadId.Handheld;
                         ((NPadStatusReport*)commandPtr)->orientation = NPad.Orientation.Vertical;
                         ((NPadStatusReport*)commandPtr)->styleMask = NPad.NpadStyle.Handheld;
+
+                        ((NPadStatusReport*)commandPtr)->colorLeftMain = ColorToNNColor(Color.red);
+                        ((NPadStatusReport*)commandPtr)->colorLeftSub = ColorToNNColor(Color.black);
+                        ((NPadStatusReport*)commandPtr)->colorRightMain = ColorToNNColor(Color.cyan);
+                        ((NPadStatusReport*)commandPtr)->colorRightSub = ColorToNNColor(Color.gray);
                         return 1;
                     }
                     else if (commandPtr->type == QueryUserIdCommand.Type)
@@ -107,6 +112,17 @@ public class SwitchTests : InputTestFixture
         Assert.That(controller.npadId, Is.EqualTo(NPad.NpadId.Handheld));
         Assert.That(controller.orientation, Is.EqualTo(NPad.Orientation.Vertical));
         Assert.That(controller.styleMask, Is.EqualTo(NPad.NpadStyle.Handheld));
+        Assert.That(controller.leftControllerColor.Main, Is.EqualTo((Color32)Color.red));
+        Assert.That(controller.leftControllerColor.Sub, Is.EqualTo((Color32)Color.black));
+        Assert.That(controller.rightControllerColor.Main, Is.EqualTo((Color32)Color.cyan));
+        Assert.That(controller.rightControllerColor.Sub, Is.EqualTo((Color32)Color.gray));
+    }
+
+    private int ColorToNNColor(Color color)
+    {
+        Color32 color32 = color;
+
+        return (int)color32.r | ((int)color32.g << 8) | ((int)color32.b << 16) | ((int)color32.a << 24);
     }
 
     [Test]
