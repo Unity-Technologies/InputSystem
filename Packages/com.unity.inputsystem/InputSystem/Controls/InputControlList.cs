75d6519500e2e938f32fa52dcc56f84823029b49
dougp <dougp@unity3d.com>
2018-10-29 10:49:14 -0400

    Fix known Rosyln warning on bit shifting operations.

--- Packages/com.unity.inputsystem/InputSystem/Controls/InputControlList.cs
+++ Packages/com.unity.inputsystem/InputSystem/Controls/InputControlList.cs
@@ -228,7 +228,16 @@ namespace UnityEngine.Experimental.Input
                 ? ArrayHelpers.IndexOfReference(device.m_ChildrenForEachControl, control) + 1
                 : 0;
 
-            return ((ulong)deviceIndex << 32) | (ulong)controlIndex;
+            // There is a known documented bug with the new Rosyln
+            // compiler where it warns on casts with following line that
+            // was perfectly legaly in previous CSC compiler.  
+            // Below is silly conversion to get rid of warning, or we can pragma
+            // out the warning.
+            //return ((ulong)deviceIndex << 32) | (ulong)controlIndex;
+            var shiftedDeviceIndex = (ulong) deviceIndex << 32;
+            var unsignedControlIndex = (ulong) controlIndex;
+
+            return shiftedDeviceIndex | unsignedControlIndex;
         }
 
         private static TControl FromIndex(ulong index)
