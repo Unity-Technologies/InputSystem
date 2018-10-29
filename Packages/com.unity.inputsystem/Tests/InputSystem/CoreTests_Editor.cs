6053192a57d4cde58d19e80c78c2e67cfb34ec5e
dougp <dougp@unity3d.com>
2018-10-29 10:18:26 -0400

    Disable unfixable warning with serialization.

--- Packages/com.unity.inputsystem/Tests/InputSystem/CoreTests_Editor.cs
+++ Packages/com.unity.inputsystem/Tests/InputSystem/CoreTests_Editor.cs
@@ -20,6 +20,9 @@ partial class CoreTests
     [Serializable]
     struct PackageJson
     {
+        // Need to pragma out this warning, its a known limitation 
+        // of the compiler to warn when the field is assigned thru serialization
+        #pragma warning disable CS0649
         public string version;
     }
 
