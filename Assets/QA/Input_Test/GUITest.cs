531045d287c31e4546502a1ce5eaefcb4e8c7858
Tom Baird <tomb@unity3d.com>
2018-11-05 14:51:51 -0800

    NEW: Support touch and tracked input in UI input module (#288).

--- Assets/QA/Input_Test/GUITest.cs
+++ Assets/QA/Input_Test/GUITest.cs
@@ -1,77 +1,22 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class GUITest : UIBehaviour, ISelectHandler, IDeselectHandler, IMoveHandler, ISubmitHandler, ICancelHandler
+public class GUITest : MonoBehaviour
 {
-    protected override void OnEnable()
+    public GameObject initialSelectedGameObject;
+
+    public void OnEnable()
     {
-        base.OnEnable();
-
-        EventSystem current = EventSystem.current;
-        if (current != null)
-            EventSystem.current.SetSelectedGameObject(gameObject);
-    }
-
-    protected override void OnDisable()
-    {
-        base.OnDisable();
-
-        EventSystem current = EventSystem.current;
-        if (current != null)
-            EventSystem.current.SetSelectedGameObject(null);
+        if (initialSelectedGameObject != null)
+        {
+            var eventSystem = EventSystem.current;
+            if (eventSystem != null)
+                eventSystem.SetSelectedGameObject(initialSelectedGameObject);
+        }
     }
 
     public void OnButtonClick()
     {
-        Debug.Log("Button Click Recieved");
-    }
-
-    public void OnSelect(BaseEventData eventData)
-    {
-        Debug.Log("Selected Event Recieved");
-        eventData.Use();
-    }
-
-    public void OnDeselect(BaseEventData eventData)
-    {
-        Debug.Log("Deselected Event Recieved");
-        eventData.Use();
-    }
-
-    public void OnMove(AxisEventData eventData)
-    {
-        string directionStr = "Unknown";
-        switch (eventData.moveDir)
-        {
-            case MoveDirection.Up:
-                directionStr = "Up";
-                break;
-            case MoveDirection.Down:
-                directionStr = "Down";
-                break;
-            case MoveDirection.Left:
-                directionStr = "Left";
-                break;
-            case MoveDirection.Right:
-                directionStr = "Right";
-                break;
-            case MoveDirection.None:
-                directionStr = "None";
-                break;
-        }
-        Debug.Log(string.Format("Move Event Recieved: [{0}] [{1}]", directionStr, eventData.moveVector));
-        eventData.Use();
-    }
-
-    public void OnSubmit(BaseEventData eventData)
-    {
-        Debug.Log("Submit Event Recieved");
-        eventData.Use();
-    }
-
-    public void OnCancel(BaseEventData eventData)
-    {
-        Debug.Log("Cancel Event Recieved");
-        eventData.Use();
+        Debug.Log("Button Click Received");
     }
 }
