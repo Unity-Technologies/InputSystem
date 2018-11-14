7654ffc9a2d7b42827f044348c98a252e389ec1c
dougp <dougp@unity3d.com>
2018-11-14 16:36:56 -0500

    NEW: Add framework for warnings test for 2019.1

--- Automation/EditorTests/Assets/Editor/ScriptCompilersTests.cs
+++ Automation/EditorTests/Assets/Editor/ScriptCompilersTests.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 using NUnit.Framework;
 using UnityEditor;
 using UnityEditor.Modules;
@@ -18,63 +19,76 @@ using ieu = UnityEditorInternal.InternalEditorUtility;
 public class ScriptCompilersTests
     {
 
-        static CompilerMessage[] CompileCSharp(string code)
+    [Test]
+    public void MonoCSharpCompilerWarningTest()
+    {
+        var messages = "";
+        Assert.IsEmpty(messages, "Compiler returned unexpected messages");
+    }
+
+    static CompilerMessage[] CompileCSharp(string code)
+    {
+        var supportedLanguage = (SupportedLanguage)Activator.CreateInstance(typeof(CSharpLanguage));
+        var island = CreateMonoIsland(supportedLanguage);
+
+        using (var compiler = new MonoCSharpCompiler(island, false))
         {
-            var supportedLanguage = (SupportedLanguage)Activator.CreateInstance(typeof(CSharpLanguage));
-            var island = CreateMonoIsland(supportedLanguage);
-
-            using (var compiler = new MonoCSharpCompiler(island, false))
-            {
-                return Compile(compiler, island, code);
-            }
+            return Compile(compiler, island, code);
         }
+    }
 
-        static MonoIsland CreateMonoIsland(SupportedLanguage language)
+    static MonoIsland CreateMonoIsland(SupportedLanguage language)
+    {
+        var inputFilePath = Path.GetTempFileName();
+        var outputAssemblyPath = Path.GetTempFileName();
+
+        var options = EditorScriptCompilationOptions.BuildingForEditor;
+        var buildTarget = UnityEditor.EditorUserBuildSettings.activeBuildTarget;
+        var buildTargetGroup = UnityEditor.EditorUserBuildSettings.activeBuildTargetGroup;
+        var defines = ieu.GetCompilationDefines(options, buildTargetGroup, buildTarget);
+
+        var references = new List<string>();
+        references.Add(ieu.GetEngineAssemblyPath());
+        references.Add(ieu.GetEngineCoreModuleAssemblyPath());
+        references.Add(ieu.GetEditorAssemblyPath());
+        references.AddRange(ModuleUtils.GetAdditionalReferencesForUserScripts());
+
+        var apiCompatibilityLevel = PlayerSettings.GetApiCompatibilityLevel(EditorUserBuildSettings.activeBuildTargetGroup);
+
+        var scriptAssembly = new ScriptAssembly
         {
-            var inputFilePath = Path.GetTempFileName();
-            var outputAssemblyPath = Path.GetTempFileName();
+            Filename = AssetPath.GetFileName(outputAssemblyPath),
+            Flags = AssemblyFlags.None
+        };
 
-            var options = EditorScriptCompilationOptions.BuildingForEditor;
-            var buildTarget = UnityEditor.EditorUserBuildSettings.activeBuildTarget;
-            var buildTargetGroup = UnityEditor.EditorUserBuildSettings.activeBuildTargetGroup;
-            var defines = ieu.GetCompilationDefines(options, buildTargetGroup, buildTarget);
+        references.AddRange(MonoLibraryHelpers.GetSystemLibraryReferences(apiCompatibilityLevel, buildTarget, language, true, scriptAssembly));
 
-            var references = new List<string>();
-            references.Add(ieu.GetEngineAssemblyPath());
-            references.Add(ieu.GetEngineCoreModuleAssemblyPath());
-            references.Add(ieu.GetEditorAssemblyPath());
-            references.AddRange(ModuleUtils.GetAdditionalReferencesForUserScripts());
+        MonoIsland island = new MonoIsland(buildTarget, apiCompatibilityLevel, true, new[] {inputFilePath},
+            references.ToArray(), defines, outputAssemblyPath);
 
-            var apiCompatibilityLevel = PlayerSettings.GetApiCompatibilityLevel(EditorUserBuildSettings.activeBuildTargetGroup);
+        return island;
+    }
 
-            references.AddRange(MonoLibraryHelpers.GetSystemLibraryReferences(apiCompatibilityLevel, buildTarget, language, true, outputAssemblyPath));
+    static CompilerMessage[] Compile(ScriptCompilerBase compiler, MonoIsland island, string code)
+    {
+        var inputSourcePath = island._files[0];
+        var assemblyOutputPath = island._output;
 
-            MonoIsland island = new MonoIsland(buildTarget, apiCompatibilityLevel, true, new[] { inputFilePath },
-                references.ToArray(), defines, outputAssemblyPath);
+        File.WriteAllText(inputSourcePath, code);
 
-            return island;
-        }
+        compiler.BeginCompiling();
+        compiler.WaitForCompilationToFinish();
 
-        static CompilerMessage[] Compile(ScriptCompilerBase compiler, MonoIsland island, string code)
-        {
-            var inputSourcePath = island._files[0];
-            var assemblyOutputPath = island._output;
+        Assert.IsTrue(compiler.Poll(), "Compilation is not finished");
 
-            File.WriteAllText(inputSourcePath, code);
+        var messages = compiler.GetCompilerMessages();
 
-            compiler.BeginCompiling();
-            compiler.WaitForCompilationToFinish();
+        if (messages.Count(m => m.type == CompilerMessageType.Error) == 0)
+            Assert.True(File.Exists(assemblyOutputPath), "Output assembly does not exist after successful compile");
 
-            Assert.IsTrue(compiler.Poll(), "Compilation is not finished");
+        File.Delete(inputSourcePath);
+        File.Delete(assemblyOutputPath);
 
-            var messages = compiler.GetCompilerMessages();
-
-            if (messages.Count(m => m.type == CompilerMessageType.Error) == 0)
-                Assert.True(File.Exists(assemblyOutputPath), "Output assembly does not exist after successful compile");
-
-            File.Delete(inputSourcePath);
-            File.Delete(assemblyOutputPath);
-
-            return messages;
-        }
+        return messages;
+    }
 }
