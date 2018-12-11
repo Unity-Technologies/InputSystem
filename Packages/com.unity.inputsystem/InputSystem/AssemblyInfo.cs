0b46a673f5f07a7853367ee2cd9cc96f2d5cb8c5
dougp <dougp@unity3d.com>
2018-12-11 14:28:12 -0500

    PUBLISH: 0.0.14-preview

--- Packages/com.unity.inputsystem/InputSystem/AssemblyInfo.cs
+++ Packages/com.unity.inputsystem/InputSystem/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.CompilerServices;
 // Keep this in sync with "Packages/com.unity.inputsystem/package.json".
 // NOTE: Unfortunately, System.Version doesn't use semantic versioning so we can't include
 //       "-preview" suffixes here.
-[assembly: AssemblyVersion("0.0.13")]
+[assembly: AssemblyVersion("0.0.14")]
 
 [assembly: InternalsVisibleTo("Unity.InputSystem.Tests.Editor")]
 [assembly: InternalsVisibleTo("Unity.InputSystem.Tests")]
