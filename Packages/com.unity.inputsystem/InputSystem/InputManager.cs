62d6641a257bf6c9304fcbeea9a313d0eca5a1b6
Tomas Dirvanauskas <tomas@unity3d.com>
2019-01-29 16:09:15 +0200

    Remove TouchPositionTransformProcessor, we do this now in native backend. Related changeset - https://ono.unity3d.com/unity/unity/changeset/89057ca71d0f5dc537d23f5c4e18d10cd81f9b09

--- Packages/com.unity.inputsystem/InputSystem/InputManager.cs
+++ Packages/com.unity.inputsystem/InputSystem/InputManager.cs
@@ -1452,7 +1452,6 @@ namespace UnityEngine.Experimental.Input
             //processors.AddTypeRegistration("Curve", typeof(CurveProcessor));
             processors.AddTypeRegistration("CompensateDirection", typeof(CompensateDirectionProcessor));
             processors.AddTypeRegistration("CompensateRotation", typeof(CompensateRotationProcessor));
-            processors.AddTypeRegistration("TouchPositionTransform", typeof(TouchPositionTransformProcessor));
 
             #if UNITY_EDITOR
             processors.AddTypeRegistration("AutoWindowSpace", typeof(EditorWindowSpaceProcessor));
