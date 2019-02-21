e0b9d6a0e1e7fbfae8443ead5c8f03bb9420cf4a
Tom Baird <tomb@unity3d.com>
2019-02-20 16:40:49 -0800

    Simple Boilerplate for SDL Template Builder

---
+++ Packages/com.unity.inputsystem/InputSystem/Plugins/Linux/SDLDeviceBuilder.cs
@@ -0,0 +1,131 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine.Experimental.Input.LowLevel;
+using UnityEngine.Experimental.Input.Utilities;
+using System.Text;
+using UnityEngine.Experimental.Input.Layouts;
+
+namespace UnityEngine.Experimental.Input.Plugins.Linux
+{
+    [Serializable]
+    class SDLLayoutBuilder
+    {
+        [SerializeField]
+        string parentLayout;
+        [SerializeField]
+        string interfaceName;
+        [SerializeField]
+        SDLDeviceDescriptor descriptor;
+
+        static string SanitizeName(string originalName)
+        {
+            int stringLength = originalName.Length;
+            var sanitizedName = new StringBuilder(stringLength);
+            for (int i = 0; i < stringLength; i++)
+            {
+                char letter = originalName[i];
+                if (char.IsUpper(letter) || char.IsLower(letter) || char.IsDigit(letter))
+                {
+                    sanitizedName.Append(letter);
+                }
+            }
+            return sanitizedName.ToString();
+        }
+
+        internal static string OnFindLayoutForDevice(int deviceId, ref InputDeviceDescription description, string matchedLayout, IInputRuntime runtime)
+        {
+            // If the device isn't a XRInput, we're not interested.
+            if (description.interfaceName != DeviceInterfaces.kXRInterfaceCurrent)
+            {
+                return null;
+            }
+
+            // If the description doesn't come with a XR SDK descriptor, we're not
+            // interested either.
+            if (string.IsNullOrEmpty(description.capabilities))
+            {
+                return null;
+            }
+
+            // Try to parse the SDL descriptor.
+            SDLDeviceDescriptor deviceDescriptor;
+            try
+            {
+                deviceDescriptor = SDLDeviceDescriptor.FromJson(description.capabilities);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+
+            if (deviceDescriptor == null)
+            {
+                return null;
+            }
+
+            if (string.IsNullOrEmpty(matchedLayout))
+            {
+                matchedLayout = "Joystick";
+            }
+
+            string layoutName = null;
+            if (string.IsNullOrEmpty(description.manufacturer))
+            {
+                layoutName = string.Format("{0}::{1}", SanitizeName(description.interfaceName),
+                    SanitizeName(description.product));
+            }
+            else
+            {
+                layoutName = string.Format("{0}::{1}::{2}", SanitizeName(description.interfaceName), SanitizeName(description.manufacturer), SanitizeName(description.product));
+            }
+
+            var layout = new SDLLayoutBuilder { descriptor = deviceDescriptor, parentLayout = matchedLayout, interfaceName = description.interfaceName };
+            InputSystem.RegisterLayoutBuilder(() => layout.Build(), layoutName, matchedLayout);
+            
+            return layoutName;
+        }
+
+        internal InputControlLayout Build()
+        {
+            var builder = new InputControlLayout.Builder
+            {
+                stateFormat = new FourCC('L', 'J', 'O', 'Y'),
+                extendsLayout = parentLayout
+            };
+
+            foreach (var feature in descriptor.inputFeatures)
+            {
+                switch (feature.featureType)
+                {
+                    case JoystickFeatureType.Axis:
+                        {
+
+                        }
+                        break;
+                    case JoystickFeatureType.Ball:
+                        {
+
+                        }
+                        break;
+                    case JoystickFeatureType.Button:
+                        {
+
+                        }
+                        break;
+                    case JoystickFeatureType.Hat:
+                        {
+
+                        }
+                        break;
+                    default:
+                        {
+                            throw new NotImplementedException(String.Format("SDLLayoutBuilder.Build: Trying to build an SDL device with an unknown feature named {0} of type {1}.", feature.name, feature.featureType));
+                        }
+
+                }
+            }
+
+            return builder.Build();
+        }
+    }
+}
---
+++ Packages/com.unity.inputsystem/InputSystem/Plugins/Linux/SDLSupport.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine.Experimental.Input.Layouts;
+
+namespace UnityEngine.Experimental.Input.Plugins.Linux
+{
+    static class DeviceInterfaces
+    {
+        /// <summary>
+        /// The current interface code sent with devices to identify as Linux SDL devices.
+        /// </summary>
+        public const string kXRInterfaceCurrent = "SDL";
+    }
+
+    // These structures are not explicitly assigned, but they are filled in via JSON serialization coming from matching structs in native.
+#pragma warning disable 0649
+
+    enum JoystickFeatureType
+    {
+        Invalid = 0,
+        Axis,
+        Ball,
+        Button, 
+        Hat,
+
+        Max
+    }
+
+    struct SDLFeatureDescriptor
+    {
+        public string name;
+        public JoystickFeatureType featureType;
+        public int size;
+        public int offset;
+        public int bit;
+        public Int32 min;
+        public Int32 max;
+        public Int32 fuzz;
+        public Int32 flat;
+    }
+
+    //Sync to XRInputDeviceDefinition in XRInputDeviceDefinition.h
+    [Serializable]
+    class SDLDeviceDescriptor
+    {
+        public string product;
+        public string manufacturer;
+        public string version;
+        public string guid;
+        public List<SDLFeatureDescriptor> inputFeatures;
+
+        internal string ToJson()
+        {
+            return JsonUtility.ToJson(this);
+        }
+
+        internal static SDLDeviceDescriptor FromJson(string json)
+        {
+            return JsonUtility.FromJson<SDLDeviceDescriptor>(json);
+        }
+    }
+#pragma warning restore 0649
+
+    /// <summary>
+    /// A small helper class to aid in initializing and registering SDL devices and layout builders.
+    /// </summary>
+    public static class SDLSupport
+    {
+        /// <summary>
+        /// Registers all initial templates and the generalized layout builder with the InputSystem.
+        /// </summary>
+        public static void Initialize()
+        {
+            InputSystem.onFindLayoutForDevice += SDLLayoutBuilder.OnFindLayoutForDevice;
+        }
+    }
+}
