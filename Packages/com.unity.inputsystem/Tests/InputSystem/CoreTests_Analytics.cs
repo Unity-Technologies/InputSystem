fe6b48c766c9913a2c5b2a6ce6d1eda7d34954ab
Jonas Echterhoff <jonas@unity3d.com>
2019-03-11 10:26:09 +0100

    Register analytics events on first use instead of in initialization

--- Packages/com.unity.inputsystem/Tests/InputSystem/CoreTests_Analytics.cs
+++ Packages/com.unity.inputsystem/Tests/InputSystem/CoreTests_Analytics.cs
@@ -18,45 +18,21 @@ partial class CoreTests
 {
     [Test]
     [Category("Analytics")]
-    public void Analytics_RegistersEventsWhenInitialized()
-    {
-        var receivedNames = new List<string>();
-        var receivedMaxPerHours = new List<int>();
-        var receivedMaxPropertiesPerEvents = new List<int>();
-
-        runtime.onRegisterAnalyticsEvent =
-            (name, maxPerHour, maxPropertiesPerEvent) =>
-        {
-            receivedNames.Add(name);
-            receivedMaxPerHours.Add(maxPerHour);
-            receivedMaxPropertiesPerEvents.Add(maxPropertiesPerEvent);
-        };
-
-        // The test fixture has already initialized the input system.
-        // Create a new manager to test registration.
-        var manager = new InputManager();
-        var settings = ScriptableObject.CreateInstance<InputSettings>();
-        manager.Initialize(runtime, settings);
-
-        Assert.That(receivedNames,
-            Is.EquivalentTo(new[]
-            {
-                InputAnalytics.kEventStartup, InputAnalytics.kEventFirstUserInteraction, InputAnalytics.kEventShutdown
-            }));
-        Assert.That(receivedMaxPerHours.Count, Is.EqualTo(3));
-        Assert.That(receivedMaxPropertiesPerEvents.Count, Is.EqualTo(3));
-    }
-
-    [Test]
-    [Category("Analytics")]
     public void Analytics_ReceivesStartupEventOnFirstUpdate()
     {
+        var registeredNames = new List<string>();
         string receivedName = null;
         object receivedData = null;
+
+        runtime.onRegisterAnalyticsEvent = (name, maxPerHour, maxPropertiesPerEvent) =>
+        {
+            registeredNames.Add(name);
+        };
 
         runtime.onSendAnalyticsEvent =
             (name, data) =>
         {
+            Assert.That(registeredNames.Contains(name));
             Assert.That(receivedName, Is.Null);
             receivedName = name;
             receivedData = data;
@@ -185,11 +161,19 @@ partial class CoreTests
         InputSystem.QueueStateEvent(gamepad, new GamepadState());
         InputSystem.Update(InputUpdateType.Fixed);
 
+        var registeredNames = new List<string>();
         string receivedName = null;
         object receivedData = null;
+
+        runtime.onRegisterAnalyticsEvent = (name, maxPerHour, maxPropertiesPerEvent) =>
+        {
+            registeredNames.Add(name);
+        };
+
         runtime.onSendAnalyticsEvent =
             (name, data) =>
         {
+            Assert.That(registeredNames.Contains(name));
             Assert.That(receivedData, Is.Null);
             receivedName = name;
             receivedData = data;
