51ca47e10bfb619b2c4570d0fe35795e9e2a57bb
dougp <dougp@unity3d.com>
2019-04-23 13:51:47 -0400

    NEW: Add Tanks demo project to repository.

---
+++ Assets/Tanks/Assets/Scripts/UI/UIDirectionControl.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Complete
+{
+    public class UIDirectionControl : MonoBehaviour
+    {
+        // This class is used to make sure world space UI
+        // elements such as the health bar face the correct direction.
+
+        public bool m_UseRelativeRotation = true;       // Use relative rotation should be used for this gameobject?
+
+
+        private Quaternion m_RelativeRotation;          // The local rotatation at the start of the scene.
+
+
+        private void Start ()
+        {
+            m_RelativeRotation = transform.parent.localRotation;
+        }
+
+
+        private void Update ()
+        {
+            if (m_UseRelativeRotation)
+                transform.rotation = m_RelativeRotation;
+        }
+    }
+}
