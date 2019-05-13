Unity-Technologies/InputSystem

commit 289025f5a09aa71af96625056b47807ae36ad773

NEW: Add support for Vive Knuckles using legacy XR input (#503).

Packages/com.unity.inputsystem/InputSystem/Plugins/XR/Vive.cs
@@ -95,6 +95,60 @@ namespace UnityEngine.Experimental.Input.Plugins.XR
     }
 
     [InputControlLayout]
+    public class KnucklesController : XRControllerWithRumble
+    {
+ 
+        [InputControl(aliases = new[] { "B",  "Primary"})]
+        public ButtonControl primaryButton { get; private set; }
+
+        [InputControl(aliases = new[] { "JoystickOrPadPressed" })]        
+        public ButtonControl trackpadPressed { get; private set; }
+        [InputControl(aliases = new[] { "JoystickOrPadTouched" })]
+        public ButtonControl trackpadTouched { get; private set; }
+        [InputControl(aliases = new[] { "Primary2DAxis" })]
+        public Vector2Control trackpad { get; private set; }
+
+        public AxisControl grip { get; private set; }
+
+        [InputControl(aliases = new[] { "A",  "GripButton" })]
+        public ButtonControl gripPressed { get; private set; }
+
+
+        public AxisControl trigger { get; private set; }
+        public ButtonControl triggerPressed { get; private set; }
+
+        public IntegerControl trackingState { get; private set; }
+        public ButtonControl isTracked { get; private set; }
+        public Vector3Control devicePosition { get; private set; }
+        public QuaternionControl deviceRotation { get; private set; }
+        public Vector3Control deviceVelocity { get; private set; }
+        public Vector3Control deviceAngularVelocity { get; private set; }
+
+
+
+
+        protected override void FinishSetup(InputDeviceBuilder builder)
+        {
+            base.FinishSetup(builder);
+                    
+            gripPressed = builder.GetControl<ButtonControl>("gripPressed");
+            primaryButton = builder.GetControl<ButtonControl>("primary");            
+            trackpadPressed = builder.GetControl<ButtonControl>("trackpadPressed");
+            trackpadTouched = builder.GetControl<ButtonControl>("trackpadTouched");
+            trackpad = builder.GetControl<Vector2Control>("trackpad");
+            trigger = builder.GetControl<AxisControl>("trigger");
+            triggerPressed = builder.GetControl<ButtonControl>("triggerPressed");
+
+            trackingState = builder.GetControl<IntegerControl>("trackingState");
+            isTracked = builder.GetControl<ButtonControl>("isTracked");
+            devicePosition = builder.GetControl<Vector3Control>("devicePosition");
+            deviceRotation = builder.GetControl<QuaternionControl>("deviceRotation");
+            deviceVelocity = builder.GetControl<Vector3Control>("deviceVelocity");
+            deviceAngularVelocity = builder.GetControl<Vector3Control>("deviceAngularVelocity");
+        }
+    }
+
+    [InputControlLayout]
     public class ViveLighthouse : InputDevice
     {
         public IntegerControl trackingState { get; private set; }

Assets/Tests/InputSystem/Plugins/XRTests.cs
@@ -177,6 +177,7 @@ internal class XRTests : InputTestFixture
     [TestCase("VIVE Tracker Pro PVT S/N LHR-OBDAA26C", "HTC", typeof(ViveTracker))]
     [TestCase("OPenVR Controller(VIVE Tracker Pro PVT)", "HTC", typeof(HandedViveTracker))]
     [TestCase("HTC V2-XD/XE", "HTC", typeof(ViveLighthouse))]
+    [TestCase("OpenVR Controller(Knuckles EV3.0 Left) - Left","Valve", typeof(KnucklesController))]
     public void Devices_KnownDevice_UsesSpecializedDeviceType(string name, string manufacturer, Type expectedDeviceType)
     {
         var deviceDescription = CreateSimpleDeviceDescriptionByRole(DeviceRole.Generic);

Packages/com.unity.inputsystem/InputSystem/Plugins/XR/XRSupport.cs
@@ -169,6 +169,11 @@ namespace UnityEngine.Experimental.Input.Plugins.XR
                     .WithInterface(XRUtilities.kXRInterfaceMatchAnyVersion)
                     .WithManufacturer("HTC")
                     .WithProduct(@"^(OpenVR Controller\(((Vive. Controller)|(VIVE. Controller)|(Vive Controller)))"));
+            InputSystem.RegisterLayout<KnucklesController>(
+                matches: new InputDeviceMatcher()
+                    .WithInterface(XRUtilities.kXRInterfaceMatchAnyVersion)
+                    .WithManufacturer("Valve")
+                    .WithProduct(@"^(OpenVR Controller\(Knuckles)"));
             InputSystem.RegisterLayout<ViveTracker>(
                 matches: new InputDeviceMatcher()
                     .WithInterface(XRUtilities.kXRInterfaceMatchAnyVersion)

commit 2fd75e45b1551f36be7accea658a9fa4a3c913e2

Fix formatting broken by PR #503

Packages/com.unity.inputsystem/InputSystem/Plugins/XR/Vive.cs
@@ -97,11 +97,10 @@ namespace UnityEngine.InputSystem.Plugins.XR
     [InputControlLayout]
     public class KnucklesController : XRControllerWithRumble
     {
- 
         [InputControl(aliases = new[] { "B",  "Primary"})]
         public ButtonControl primaryButton { get; private set; }
 
-        [InputControl(aliases = new[] { "JoystickOrPadPressed" })]        
+        [InputControl(aliases = new[] { "JoystickOrPadPressed" })]
         public ButtonControl trackpadPressed { get; private set; }
         [InputControl(aliases = new[] { "JoystickOrPadTouched" })]
         public ButtonControl trackpadTouched { get; private set; }
@@ -125,14 +124,12 @@ namespace UnityEngine.InputSystem.Plugins.XR
         public Vector3Control deviceAngularVelocity { get; private set; }
 
 
-
-
         protected override void FinishSetup(InputDeviceBuilder builder)
         {
             base.FinishSetup(builder);
-                    
+
             gripPressed = builder.GetControl<ButtonControl>("gripPressed");
-            primaryButton = builder.GetControl<ButtonControl>("primary");            
+            primaryButton = builder.GetControl<ButtonControl>("primary");
             trackpadPressed = builder.GetControl<ButtonControl>("trackpadPressed");
             trackpadTouched = builder.GetControl<ButtonControl>("trackpadTouched");
             trackpad = builder.GetControl<Vector2Control>("trackpad");

Assets/Tests/InputSystem/Plugins/XRTests.cs
@@ -177,7 +177,7 @@ internal class XRTests : InputTestFixture
     [TestCase("VIVE Tracker Pro PVT S/N LHR-OBDAA26C", "HTC", typeof(ViveTracker))]
     [TestCase("OPenVR Controller(VIVE Tracker Pro PVT)", "HTC", typeof(HandedViveTracker))]
     [TestCase("HTC V2-XD/XE", "HTC", typeof(ViveLighthouse))]
-    [TestCase("OpenVR Controller(Knuckles EV3.0 Left) - Left","Valve", typeof(KnucklesController))]
+    [TestCase("OpenVR Controller(Knuckles EV3.0 Left) - Left", "Valve", typeof(KnucklesController))]
     public void Devices_KnownDevice_UsesSpecializedDeviceType(string name, string manufacturer, Type expectedDeviceType)
     {
         var deviceDescription = CreateSimpleDeviceDescriptionByRole(DeviceRole.Generic);