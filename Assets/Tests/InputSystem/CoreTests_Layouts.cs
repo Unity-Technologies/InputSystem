fff0d4a48021f8e685c0faa14258ef754a6b650e
Jonas Echterhoff <jonas@unity3d.com>
2019-05-23 11:26:44 +0200

    Fix test again after exception change

--- Assets/Tests/InputSystem/CoreTests_Layouts.cs
+++ Assets/Tests/InputSystem/CoreTests_Layouts.cs
@@ -889,7 +889,7 @@ partial class CoreTests
         InputSystem.RegisterLayout(json);
 
         Assert.That(() => InputSystem.AddDevice("MyDevice"),
-            Throws.TypeOf<ArgumentException>().With.Property("Message").Contain("Duplicate control"));
+            Throws.TypeOf<InvalidOperationException>().With.Property("Message").Contain("Duplicate control"));
     }
 
     [Test]
