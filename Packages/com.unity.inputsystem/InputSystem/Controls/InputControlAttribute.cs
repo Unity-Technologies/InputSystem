6481674fc57198fb70ccf57d78f2a663d4a9464b
jonas echterhoff <jonas@unity3d.com>
2019-05-23 16:03:13 +0200

    FIX: More misc analyzer fixes (#636)

--- Packages/com.unity.inputsystem/InputSystem/Controls/InputControlAttribute.cs
+++ Packages/com.unity.inputsystem/InputSystem/Controls/InputControlAttribute.cs
@@ -37,10 +37,12 @@ namespace UnityEngine.InputSystem.Layouts
         public string name { get; set; }
         public string format { get; set; }
         public string usage { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1819:PropertiesShouldNotReturnArrays", Justification = "According to MSDN, this message can be ignored for attribute parameters, as there are no better alternatives.")]
         public string[] usages { get; set; }
         public string parameters { get; set; }
         public string processors { get; set; }
         public string alias { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1819:PropertiesShouldNotReturnArrays", Justification = "According to MSDN, this message can be ignored for attribute parameters, as there are no better alternatives.")]
         public string[] aliases { get; set; }
         public string useStateFrom { get; set; }
         public uint bit { get; set; } = InputStateBlock.InvalidOffset;
