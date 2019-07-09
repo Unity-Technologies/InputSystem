ad516075eb1c14d82b98f09fe0d862fea1916856
JiaUnity <33637761+JiaUnity@users.noreply.github.com>
2019-07-09 10:21:33 -0700

    CHANGE: Update QA test scenes (#718).

--- Assets/QA/Input_Test/Scripts/Input/TouchISX.cs
+++ Assets/QA/Input_Test/Scripts/Input/TouchISX.cs
@@ -14,24 +14,47 @@ public class TouchISX : MonoBehaviour
 
     private InputAction m_touchAction;
 
+    [Header("Script to Show More Info")]
+    public TouchDebugInfo m_touchInfo;
+
     // Use this for initialization
     void Start()
     {
         m_touchAction = new InputAction(name: "TouchAction", binding: "<touchscreen>/touch*"); // Not using <touch> in order to not get primaryTouch, too.
         m_touchAction.performed += callbackContext => TouchInput(callbackContext.control as TouchControl);
-        m_touchAction.canceled += callbackContext => TouchInput(callbackContext.control as TouchControl);
+        m_touchAction.canceled += callbackContext => EndTouchInput(callbackContext.control as TouchControl);
         m_touchAction.Enable();
     }
 
-    private void OnEnable()
+    void OnEnable()
     {
-        if (m_touchAction != null)
-            m_touchAction.Enable();
+        m_touchAction?.Enable();
     }
 
-    private void OnDisable()
+    void OnDisable()
     {
-        m_touchAction.Disable();
+        m_touchAction?.Disable();
+    }
+
+    void Update()
+    {
+        Touchscreen touchscreen = Touchscreen.current;
+        if (touchscreen != null && m_touchInfo != null)
+        {
+            m_touchInfo.MaxISXCount = touchscreen.touches.Count;
+
+            for (int i = 0; i < touchscreen.touches.Count; i++)
+            {
+                TouchControl touch = touchscreen.touches[i];
+                string touchInfo = touch.touchId.ReadValue() + "\n"
+                    + touch.phase.ReadValue().ToString() + "\n"
+                    + touch.position.ReadValue().ToString() + "\n"
+                    + touch.pressure.ReadValue().ToString() + "\n"
+                    + touch.radius.ReadValue().ToString() + "\n"
+                    + touch.delta.ReadValue().ToString();
+                m_touchInfo.AddNewInputInfo(touchInfo, i);
+            }
+        }
     }
 
     private void TouchInput(TouchControl control)
