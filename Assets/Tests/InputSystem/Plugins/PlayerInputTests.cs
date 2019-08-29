89f68a5b8b66489e684b5f087759129b79612c40
Jonas Echterhoff <jonas@unity3d.com>
2019-08-29 14:24:32 +0200

    Fix il2cpp build

--- Assets/Tests/InputSystem/Plugins/PlayerInputTests.cs
+++ Assets/Tests/InputSystem/Plugins/PlayerInputTests.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
+using UnityEngine.Scripting;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.Controls;
 using UnityEngine.InputSystem.LowLevel;
@@ -1553,6 +1554,9 @@ internal class PlayerInputTests : InputTestFixture
         }
     }
 
+
+    // il2cpp internally crashes if this gets stripped. Need to investigate, but for now we preserve it.
+    [Preserve]
     private class PlayerInputEventListener : MonoBehaviour, IListener
     {
         public List<Message> messages { get; } = new List<Message>();
@@ -1585,6 +1589,8 @@ internal class PlayerInputTests : InputTestFixture
         }
     }
 
+    // il2cpp internally crashes if this gets stripped. Need to investigate, but for now we preserve it.
+    [Preserve]
     private class PlayerInputCSharpEventListener : MonoBehaviour, IListener
     {
         public List<Message> messages { get; } = new List<Message>();
@@ -1616,6 +1622,8 @@ internal class PlayerInputTests : InputTestFixture
         }
     }
 
+    // il2cpp internally crashes if this gets stripped. Need to investigate, but for now we preserve it.
+    [Preserve]
     private class PlayerManagerEventListener : MonoBehaviour, IListener
     {
         public List<Message> messages { get; } = new List<Message>();
@@ -1640,6 +1648,8 @@ internal class PlayerInputTests : InputTestFixture
         }
     }
 
+    // il2cpp internally crashes if this gets stripped. Need to investigate, but for now we preserve it.
+    [Preserve]
     private class PlayerManagerCSharpEventListener : MonoBehaviour, IListener
     {
         public List<Message> messages { get; } = new List<Message>();
