1e8e7835594616ce81cf340ee426918957f4dc29
Jonas Echterhoff <jonas@unity3d.com>
2019-08-30 14:27:36 +0200

    dump editor log; fail on editor error

--- ExternalSampleProjects/publish.sh
+++ ExternalSampleProjects/publish.sh
@@ -4,7 +4,13 @@ do
 	mkdir -p ${Sample}Assets/ExternalSamplesUtility/Editor
 	ln -s ../../Packages ${Sample}Packages
 	cp ExternalSampleProjects/ExternalSamplesUtility.cs ${Sample}Assets/ExternalSamplesUtility/Editor/
-	${Editor} -batchmode -projectPath $Sample -executeMethod ExternalSamplesUtility.${Method}
+	${Editor} -batchmode -projectPath $Sample -executeMethod ExternalSamplesUtility.${Method} -logFile upm-ci~/${Sample}Editor.log
+	echo Editor returned $?
+	if [ $? -eq 0 ]; then
+		rm ${Sample}Assets/ExternalSamplesUtility/Editor/ExternalSamplesUtility.cs
+		rm ${Sample}Packages
+		exit 1;
+	fi
 	rm ${Sample}Assets/ExternalSamplesUtility/Editor/ExternalSamplesUtility.cs
 	rm ${Sample}Packages
 done
