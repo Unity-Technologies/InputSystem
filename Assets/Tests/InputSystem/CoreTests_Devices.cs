b670f72c9dc4c66116df702b30defc8605d50e99
Rene Damm <rene@unity3d.com>
2019-09-05 17:57:03 +0200

    FIX: Formatting.

--- Assets/Tests/InputSystem/CoreTests_Devices.cs
+++ Assets/Tests/InputSystem/CoreTests_Devices.cs
@@ -155,37 +155,37 @@ partial class CoreTests
     [Category("Devices")]
     public void Devices_CanCreateDevice_FromLayoutVariant()
     {
-	    var json = @"
-			{
-				""name"" : ""TestDevice"",
-				""controls"" : [
-					{ ""name"" : ""VariantAControl"", ""variants"" : ""A"", ""layout"" : ""Button"" },
-					{ ""name"" : ""VariantBControl"", ""variants"" : ""B"", ""layout"" : ""Button"" },
-					{ ""name"" : ""VariantCControl"", ""variants"" : ""C"", ""layout"" : ""Button"" },
-					{ ""name"" : ""VariantABControl"", ""variants"" : ""A,B"", ""layout"" : ""Button"" },
-					{ ""name"" : ""NoVariantControl"", ""layout"" : ""Button"" }
-				]
-			}
-		";
-	    
-	    InputSystem.RegisterLayout(json);
-		
+        const string json = @"
+            {
+                ""name"" : ""TestDevice"",
+                ""controls"" : [
+                    { ""name"" : ""VariantAControl"", ""variants"" : ""A"", ""layout"" : ""Button"" },
+                    { ""name"" : ""VariantBControl"", ""variants"" : ""B"", ""layout"" : ""Button"" },
+                    { ""name"" : ""VariantCControl"", ""variants"" : ""C"", ""layout"" : ""Button"" },
+                    { ""name"" : ""VariantABControl"", ""variants"" : ""A,B"", ""layout"" : ""Button"" },
+                    { ""name"" : ""NoVariantControl"", ""layout"" : ""Button"" }
+                ]
+            }
+        ";
+
+        InputSystem.RegisterLayout(json);
+
         var variantA = InputDevice.Build<InputDevice>(layoutName: "TestDevice", layoutVariants: "A");
         var variantB = InputDevice.Build<InputDevice>(layoutName: "TestDevice", layoutVariants: "B");
         var noVariant = InputDevice.Build<InputDevice>(layoutName: "TestDevice");
-        
+
         Assert.That(variantA.TryGetChildControl("VariantAControl"), Is.Not.Null);
         Assert.That(variantA.TryGetChildControl("VariantBControl"), Is.Null);
         Assert.That(variantA.TryGetChildControl("VariantCControl"), Is.Null);
         Assert.That(variantA.TryGetChildControl("VariantABControl"), Is.Not.Null);
         Assert.That(variantA.TryGetChildControl("NoVariantControl"), Is.Not.Null);
-        
+
         Assert.That(variantB.TryGetChildControl("VariantAControl"), Is.Null);
         Assert.That(variantB.TryGetChildControl("VariantBControl"), Is.Not.Null);
         Assert.That(variantB.TryGetChildControl("VariantCControl"), Is.Null);
         Assert.That(variantB.TryGetChildControl("VariantABControl"), Is.Not.Null);
         Assert.That(variantB.TryGetChildControl("NoVariantControl"), Is.Not.Null);
-        
+
         Assert.That(noVariant.TryGetChildControl("VariantAControl"), Is.Null);
         Assert.That(noVariant.TryGetChildControl("VariantBControl"), Is.Null);
         Assert.That(noVariant.TryGetChildControl("VariantCControl"), Is.Null);
