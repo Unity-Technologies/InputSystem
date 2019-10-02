35ae1edd31a024457a49ab8bb90b4398c4e22c4f
Jonas Echterhoff <jonas@unity3d.com>
2019-10-02 10:11:03 +0200

    fix docs generatiobn

--- Packages/com.unity.inputsystem/InputSystem/Plugins/iOS/IOSGameController.cs
+++ Packages/com.unity.inputsystem/InputSystem/Plugins/iOS/IOSGameController.cs
@@ -1,4 +1,4 @@
-#if UNITY_EDITOR || UNITY_IOS || UNITY_TVOS
+#if UNITY_EDITOR || UNITY_IOS || UNITY_TVOS || PACKAGE_DOCS_GENERATION
 using System.Runtime.InteropServices;
 using UnityEngine.InputSystem.DualShock;
 using UnityEngine.InputSystem.Layouts;
