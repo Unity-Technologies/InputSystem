6f91af68cc88683370e086ea95491fd056f7468e
Tom Baird <tomb@unity3d.com>
2020-05-11 12:42:59 -0700

    Added Docs for IXRRumble interface.

--- Packages/com.unity.inputsystem/InputSystem/Plugins/XR/GenericXRDevice.cs
+++ Packages/com.unity.inputsystem/InputSystem/Plugins/XR/GenericXRDevice.cs
@@ -89,21 +89,48 @@ namespace UnityEngine.InputSystem.XR
         }
     }
 
+    /// <summary>
+    /// This interfaces is used by various <cref="InputDevice"> instances that can process haptic impulses.
+    /// </summary>
     public interface IXRRumble
     {
+        /// <summary>
+        /// True when the underlying system will be able to react to <cref="IXRRumble.SendImpulse"> commands.  In order to see what channels are available see ,<cref="IXRRumble.numChannels">
+        /// </summary>
         bool supportsImpulse { get; }
+        /// <summary>
+        /// The number of channels available to receive a <cref="IXRRumble.SendImpulse"> command to.  Each channel can represent a motor, or diffrent reaction from the underlying device.
+        /// </summary>
         int numChannels { get; }
 
+        /// <summary>
+        /// Sends an impulse of a set <cref="amplitude"> and <cref="duration">.
+        /// </summary>
+        /// <remarks>This will always send the impulse over channel 0, see <cref="bool SendImpulse(int, float, float)"> for information on channels</remarks>
+        /// <param name="amplitude">The overall power of the impulse from 0.0 to 1.0, where 1.0 is the strongest impulse available to the device.</param>
+        /// <param name="duration">How long, in seconds, the impulse should last for.</param>
+        /// <returns>True if the impulse was successfully received, false otherwise.</returns>
         bool SendImpulse(float amplitude, float duration);
+
+        /// <summary>
+        /// Sends an impulse of a set <cref="amplitude"> and <cref="duration">.
+        /// </summary>
+        /// <param name="channel">A channel represents an individual motor or other output that can react to impulses being sent to the same source <cref="InputDevice"></param>
+        /// <param name="amplitude">The overall power of the impulse from 0.0 to 1.0, where 1.0 is the strongest impulse available to the device.</param>
+        /// <param name="duration">How long, in seconds, the impulse should last for.</param>
+        /// <returns>True if the impulse was successfully received, false otherwise.</returns>
         bool SendImpulse(int channel, float amplitude, float duration);
     }
 
     /// <summary>
-    /// Identifies a controller that is capable of rumble or haptics.
+    /// A specialized sub group of XRController that supports basic rumble commands.
     /// </summary>
     [Preserve]
     public class XRControllerWithRumble : XRController, IXRRumble
     {
+        /// <summary>
+        /// True when the underlying system will be able to react to <cref="IXRRumble.SendImpulse"> commands.  In order to see what channels are available see ,<cref="IXRRumble.numChannels">
+        /// </summary>
         public bool supportsImpulse
         {
             get
@@ -114,6 +141,9 @@ namespace UnityEngine.InputSystem.XR
             }
         }
 
+        /// <summary>
+        /// The number of channels available to receive a <cref="IXRRumble.SendImpulse"> command to.  Each channel can represent a motor, or diffrent reaction from the underlying device.
+        /// </summary>
         public int numChannels
         {
             get
@@ -124,11 +154,25 @@ namespace UnityEngine.InputSystem.XR
             }
         }
 
+        /// <summary>
+        /// Sends an impulse of a set <cref="amplitude"> and <cref="duration">.
+        /// </summary>
+        /// <remarks>This will always send the impulse over channel 0, see <cref="bool SendImpulse(int, float, float)"> for information on channels</remarks>
+        /// <param name="amplitude">The overall power of the impulse from 0.0 to 1.0, where 1.0 is the strongest impulse available to the device.</param>
+        /// <param name="duration">How long, in seconds, the impulse should last for.</param>
+        /// <returns>True if the impulse was successfully received, false otherwise.</returns>
         public bool SendImpulse(float amplitude, float duration)
         {
             return SendImpulse(0, amplitude, duration);
         }
 
+        /// <summary>
+        /// Sends an impulse of a set <cref="amplitude"> and <cref="duration">.
+        /// </summary>
+        /// <param name="channel">A channel represents an individual motor or other output that can react to impulses being sent to the same source <cref="InputDevice"></param>
+        /// <param name="amplitude">The overall power of the impulse from 0.0 to 1.0, where 1.0 is the strongest impulse available to the device.</param>
+        /// <param name="duration">How long, in seconds, the impulse should last for.</param>
+        /// <returns>True if the impulse was successfully received, false otherwise.</returns>
         public bool SendImpulse(int channel, float amplitude, float duration)
         {
             var command = SendHapticImpulseCommand.Create(channel, amplitude, duration);
