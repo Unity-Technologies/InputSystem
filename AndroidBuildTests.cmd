33a52ff4167b02e6b102b1fb3511d7da09918e86
Tomas Dirvanauskas <tomas@unity3d.com>
2020-06-10 13:43:58 +0300

    switch to il2cpp

--- AndroidBuildTests.cmd
+++ AndroidBuildTests.cmd
@@ -13,5 +13,5 @@ if "%EDITOR_LOCATION%" == "Empty" (
 
 echo Editor location is %EDITOR_LOCATION%
 curl -s https://artifactory.internal.unity3d.com/core-automation/tools/utr-standalone/utr.bat --output utr.bat
-call ./utr.bat --suite=playmode --platform=Android --editor-location="%EDITOR_LOCATION%" --testproject=F:/Projects/InputSystem --player-save-path=build/players --artifacts_path=build/logs --scripting-backend=mono --build-only
+call ./utr.bat --suite=playmode --platform=Android --editor-location="%EDITOR_LOCATION%" --testproject=F:/Projects/InputSystem --player-save-path=build/players --artifacts_path=build/logs --scripting-backend=il2cpp --build-only
 pause
