740abf7eed2108f3be28373ec7d4767c3de1274b
Tomas Dirvanauskas <tomas@unity3d.com>
2020-06-18 15:54:43 +0300

    Convert AndroidBuildTests to ps1

--- AndroidBuildTests.cmd
+++ AndroidBuildTests.cmd
@@ -1,17 +1,2 @@
-echo off
-set EDITOR_LOCATION=Empty
-if "%EDITOR_LOCATION%" == "Empty" (
- echo Please specify Editor path, for ex., C:/Program Files/Unity/Hub/Editor/2019.3.13f1/Editor or modify this bat file
- set /p EDITOR_LOCATION=Editor Location:
-)
-
-if "%EDITOR_LOCATION%" == "Empty" (
- echo Invalid editor location, exiting
- pause
- GOTO :eof
-)
-
-echo Editor location is %EDITOR_LOCATION%
-curl -s https://artifactory.internal.unity3d.com/core-automation/tools/utr-standalone/utr.bat --output utr.bat
-call ./utr.bat --suite=playmode --platform=Android --editor-location="%EDITOR_LOCATION%" --testproject=F:/Projects/InputSystem --player-save-path=build/players --artifacts_path=build/logs --scripting-backend=il2cpp --build-only
-pause
+REM Use this script if you don't want to enable script executions on your system
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& .\AndroidBuildTests.ps1"
