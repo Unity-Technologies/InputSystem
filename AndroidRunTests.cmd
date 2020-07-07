24ad667b8a6655a96b5c5d9718fbce48b755c1dc
Tomas Dirvanauskas <tomas@unity3d.com>
2020-07-07 19:54:27 +0300

    Convert to PS script

--- AndroidRunTests.cmd
+++ AndroidRunTests.cmd
@@ -1,19 +1,2 @@
-REM echo off
-set ANDROID_SDK_ROOT=Empty
-if "%ANDROID_SDK_ROOT%" == "Empty" (
- echo 'Please specify Android SDK Root path, for ex., C:/Program Files/Unity/Hub/Editor/2019.3.13f1/Editor/Data/PlaybackEngines/AndroidPlayer/SDK or modify this bat file'
- 
- set /p ANDROID_SDK_ROOT=ANDROID_SDK_ROOT:
-)
-
-if "%ANDROID_SDK_ROOT%" == Empty (
- echo Invalid ANDROID_SDK_ROOT location, exiting
- pause
- GOTO :eof
-)
-
-echo ANDROID_SDK_ROOT is %ANDROID_SDK_ROOT%
-
-curl -s https://artifactory.internal.unity3d.com/core-automation/tools/utr-standalone/utr.bat --output utr.bat
-call ./utr --suite=playmode --platform=android --player-load-path=build/players --artifacts_path=build/test-results
-pause
+REM Use this script if you don't want to enable script executions on your system
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& .\AndroidRunTests.ps1"
