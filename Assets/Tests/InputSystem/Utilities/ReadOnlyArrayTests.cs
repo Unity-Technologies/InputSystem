66313566c431fd5189bfef05b927df64e9a2ba64
Benoit Alain <benoit.alain@unity3d.com>
2020-07-14 11:59:01 -0400

    Added ReadOnlyArrayTests unit tests to verify that foreach on read-only array doesn't allocate memory.

---
+++ Assets/Tests/InputSystem/Utilities/ReadOnlyArrayTests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using UnityEngine.InputSystem.Utilities;
+using UnityEngine.TestTools.Constraints;
+using Is = NUnit.Framework.Is;
+
+public class ReadOnlyArrayTests
+{
+    [Test]
+    public void WithForEach()
+    {
+        var array = new ReadOnlyArray<float>(new float[] { 1, 2, 3, 4 });
+        // Warm up.
+        var foo1 = 1.0f;
+        foreach (var element in array)
+            foo1 = element + 1;
+        Assert.That(() =>
+            {
+                var foo = 1.0f;
+                foreach (var element in array)
+                    foo = element + 1;
+            },
+            Is.Not.AllocatingGCMemory());
+    }
+    [Test]
+    public void WithoutForEach()
+    {
+        var array = new ReadOnlyArray<float>(new float[] { 1, 2, 3, 4 });
+        // Warm up.
+        var foo1 = 1.0f;
+        for (var i = 0; i < array.Count; ++i)
+            foo1 = array[i] + 1;
+        Assert.That(() =>
+            {
+                var foo = 1.0f;
+                for (var i = 0; i < array.Count; ++i)
+                    foo = array[i] + 1;
+            },
+            Is.Not.AllocatingGCMemory());
+    }
+}
