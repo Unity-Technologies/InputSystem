3f3017b65d9b114742daed7514ba377411aa336a
Tomas Dirvanauskas <tomas@unity3d.com>
2020-08-26 12:10:14 +0300

    Cleanup tests

--- Assets/Tests/InputSystem/ScreenKeyboardTests.cs
+++ Assets/Tests/InputSystem/ScreenKeyboardTests.cs
@@ -17,6 +17,32 @@ public class ScreenKeyboardTests : InputTestFixture
     static ScreenKeyboard s_TargetKeyboard;
     const int kFrameTimeout = 30;
 
+    public class CallbackInfo<T>
+    {
+        public T Data { private set; get; }
+        public int Frame { private set; get; }
+        public int ThreadId { private set; get; }
+
+        public CallbackInfo()
+        {
+            Frame = -1;
+            ThreadId = -1;
+        }
+
+        public CallbackInfo(T initialData)
+        {
+            Data = initialData;
+            Frame = -1;
+            ThreadId = -1;
+        }
+        public void CallbackInvoked(T data)
+        {
+            Data = data;
+            Frame = Time.frameCount;
+            ThreadId = Thread.CurrentThread.ManagedThreadId;
+        }
+    }
+
     // Workaround RangeInt not having ToString function
     private struct MyRangeInt
     {
@@ -65,115 +91,160 @@ public class ScreenKeyboardTests : InputTestFixture
         }
     }
 
-    private IEnumerator ResetKeyboard()
+    private IEnumerator HideKeyboard()
     {
         if (keyboard.status != ScreenKeyboardStatus.Done)
         {
             keyboard.Hide();
             for (int i = 0; i < kFrameTimeout && keyboard.status != ScreenKeyboardStatus.Done; i++)
                 yield return new WaitForFixedUpdate();
-            Assert.AreEqual(ScreenKeyboardStatus.Done, keyboard.status, "Couldn't reset keyboard");
+            Assert.AreEqual(ScreenKeyboardStatus.Done, keyboard.status, "Couldn't hide keyboard");
         }
     }
 
+    private IEnumerator ShowKeyboard()
+    {
+        return ShowKeyboard(new ScreenKeyboardShowParams());
+    }
+
+    private IEnumerator ShowKeyboard(ScreenKeyboardShowParams showParams)
+    {
+        Assert.IsTrue(keyboard.status != ScreenKeyboardStatus.Visible, "Expected keybard to be not visible");
+
+        keyboard.Show();
+        for (int i = 0; i < kFrameTimeout && keyboard.status != ScreenKeyboardStatus.Visible; i++)
+            yield return new WaitForFixedUpdate();
+        Assert.AreEqual(ScreenKeyboardStatus.Visible, keyboard.status, "Couldn't show keyboard");
+        
+    }
+
     // TODO:
-    // See if callbacks are received at the same frame, callback count
     // Disable selection callbacks, when no input field is present
-    // Return selection 0, 0, whhen inmput field is not present
     // See that callbacks are not called when keyboard is not shown
     [UnityTest]
     public IEnumerator CheckShowHideOperations()
     {
-        yield return ResetKeyboard();
-
-        Assert.AreEqual(ScreenKeyboardStatus.Done, keyboard.status);
-
-        keyboard.Show();
-
-        // Show, Hide are not immediate operations, it might take some time until keyboard becomes visible
-        for (int i = 0; i < kFrameTimeout && keyboard.status != ScreenKeyboardStatus.Visible; i++)
-            yield return new WaitForFixedUpdate();
-        Assert.AreEqual(ScreenKeyboardStatus.Visible, keyboard.status);
-
-        keyboard.Hide();
-        for (int i = 0; i < kFrameTimeout && keyboard.status != ScreenKeyboardStatus.Done; i++)
-            yield return new WaitForFixedUpdate();
-        Assert.AreEqual(ScreenKeyboardStatus.Done, keyboard.status);
+        yield return HideKeyboard();
+        yield return ShowKeyboard();
+        yield return HideKeyboard();
     }
 
     [UnityTest]
-    public IEnumerator CheckCallbacks()
+    public IEnumerator CheckStateCallback()
     {
-        yield return ResetKeyboard();
+        yield return HideKeyboard();
 
-        Assert.AreEqual(ScreenKeyboardStatus.Done, keyboard.status);
-        ScreenKeyboardStatus stateFromCallback = ScreenKeyboardStatus.Canceled;
-        int stateCallbackFrame = -1;
-        int statedCallbackThreadId = -1;
+        var stateCallbackInfo = new CallbackInfo<ScreenKeyboardStatus>(ScreenKeyboardStatus.Canceled);
         var stateCallback = new Action<ScreenKeyboardStatus>(
             (state) =>
             {
-                stateFromCallback = state;
-                stateCallbackFrame = Time.frameCount;
-                statedCallbackThreadId = Thread.CurrentThread.ManagedThreadId;
+                stateCallbackInfo.CallbackInvoked(state);
             });
         keyboard.stateChanged += stateCallback;
 
-        string textFromCallback = string.Empty;
-        int inputFieldTextChangedCallbackFrame = -1;
-        int inputFieldTextChangedCallbackThreadId = -1;
+        yield return ShowKeyboard();
 
+        Assert.AreEqual(ScreenKeyboardStatus.Visible, stateCallbackInfo.Data);
+        Assert.AreEqual(Thread.CurrentThread.ManagedThreadId, stateCallbackInfo.ThreadId);
+        // Don't check frame, since when you call Show the keyboard can appear only in next frame
+        
+        yield return HideKeyboard();
+
+        Assert.AreEqual(ScreenKeyboardStatus.Done, stateCallbackInfo.Data);
+        Assert.AreEqual(Thread.CurrentThread.ManagedThreadId, stateCallbackInfo.ThreadId);
+        keyboard.stateChanged -= stateCallback;
+    }
+
+    [UnityTest]
+    public IEnumerator CheckInputFieldTextCallback()
+    {
+        yield return HideKeyboard();
+
+        var inputFieldTextCallbackInfo = new CallbackInfo<string>(string.Empty);
         var inputFieldCallback = new Action<string>(
             (text) =>
             {
-                textFromCallback = text;
-                inputFieldTextChangedCallbackFrame = Time.frameCount;
-                inputFieldTextChangedCallbackThreadId = Thread.CurrentThread.ManagedThreadId;
+                inputFieldTextCallbackInfo.CallbackInvoked(text); ;
             });
-
-
         keyboard.inputFieldTextChanged += inputFieldCallback;
-        MyRangeInt selectionRange = new MyRangeInt(0, 0);
-        int selectionChangedCallbackFrame = -1;
-        int selectionChangedCallbackThreadId = -1;
 
-        var selectionCallback = new Action<RangeInt>((range) =>
-        {
-            selectionRange = range;
-            selectionChangedCallbackFrame = Time.frameCount;
-            selectionChangedCallbackThreadId = Thread.CurrentThread.ManagedThreadId;
-        });
+        yield return ShowKeyboard();
 
-        keyboard.selectionChanged += selectionCallback;
-
-        keyboard.Show();
-        for (int i = 0; i < kFrameTimeout && keyboard.status != ScreenKeyboardStatus.Visible; i++)
-            yield return new WaitForFixedUpdate();
-
-        Assert.AreEqual(ScreenKeyboardStatus.Visible, keyboard.status);
         Assert.AreEqual(string.Empty, keyboard.inputFieldText);
-        Assert.AreEqual(new MyRangeInt(0, 0), (MyRangeInt)keyboard.selection);
-        Assert.AreEqual(ScreenKeyboardStatus.Visible, stateFromCallback);
-        Assert.AreEqual(statedCallbackThreadId, Thread.CurrentThread.ManagedThreadId);
-
 
         var targetText = "Hello";
         keyboard.inputFieldText = targetText;
-        Assert.AreEqual(textFromCallback, targetText);
-        Assert.AreEqual(inputFieldTextChangedCallbackFrame, Time.frameCount);
-        Assert.AreEqual(inputFieldTextChangedCallbackThreadId, Thread.CurrentThread.ManagedThreadId);
 
-        Assert.AreEqual(new MyRangeInt(targetText.Length, 0), selectionRange);
-        Assert.AreEqual(selectionChangedCallbackFrame, Time.frameCount);
-        Assert.AreEqual(selectionChangedCallbackThreadId, Thread.CurrentThread.ManagedThreadId);
+        Assert.AreEqual(targetText, inputFieldTextCallbackInfo.Data);
+        Assert.AreEqual(Time.frameCount, inputFieldTextCallbackInfo.Frame);
+        Assert.AreEqual(Thread.CurrentThread.ManagedThreadId, inputFieldTextCallbackInfo.ThreadId);
 
-        keyboard.stateChanged -= stateCallback;
         keyboard.inputFieldTextChanged -= inputFieldCallback;
+
+        yield return HideKeyboard();
+    }
+
+    [UnityTest]
+    public IEnumerator CheckInputFieldText()
+    {
+        yield return HideKeyboard();
+        var initiaText = "Placeholder";
+        var targetText = "Hello";
+        yield return ShowKeyboard(new ScreenKeyboardShowParams{initialText = initiaText });
+
+        Assert.AreEqual(initiaText, keyboard.inputFieldText);
+        keyboard.inputFieldText = targetText;
+        Assert.AreEqual(targetText, keyboard.inputFieldText);
+
+        yield return HideKeyboard();
+
+        Assert.AreEqual(targetText, keyboard.inputFieldText);
+    }
+
+
+    [UnityTest]
+    public IEnumerator CheckSelectionCallbacks()
+    {
+        yield return HideKeyboard();
+
+        var selectionCallbackInfo = new CallbackInfo<MyRangeInt>(new MyRangeInt(0, 0));
+        var selectionCallback = new Action<RangeInt>((range) => { selectionCallbackInfo.CallbackInvoked(range); });
+
+        keyboard.selectionChanged += selectionCallback;
+
+        yield return ShowKeyboard();
+
+        Assert.AreEqual(new MyRangeInt(0, 0), (MyRangeInt)keyboard.selection);
+
+        var targetText = "Hello";
+        keyboard.inputFieldText = targetText;
+
+        Assert.AreEqual(new MyRangeInt(targetText.Length, 0), selectionCallbackInfo.Data);
+        Assert.AreEqual(Time.frameCount, selectionCallbackInfo.Frame);
+        Assert.AreEqual(Thread.CurrentThread.ManagedThreadId, selectionCallbackInfo.ThreadId);
+
+        //TODO: call selection directly, see that text callback is not called
+        var inputFieldTextCallbackInfo = new CallbackInfo<string>(string.Empty);
+        var inputFieldCallback = new Action<string>(
+            (text) =>
+            {
+                inputFieldTextCallbackInfo.CallbackInvoked(text); ;
+            });
+        keyboard.inputFieldTextChanged += inputFieldCallback;
+
+        keyboard.selection = new RangeInt(1, 0);
+        Assert.AreEqual(new MyRangeInt(1, 0), selectionCallbackInfo.Data);
+
+        // Calling selection shouldn't trigger inputFieldText callback
+        Assert.AreEqual(-1, inputFieldTextCallbackInfo.Frame);
+        keyboard.inputFieldTextChanged -= inputFieldCallback;
+
         keyboard.selectionChanged -= selectionCallback;
 
-        keyboard.Hide();
-        for (int i = 0; i < kFrameTimeout && keyboard.status != ScreenKeyboardStatus.Done; i++)
-            yield return new WaitForFixedUpdate();
-        Assert.AreEqual(ScreenKeyboardStatus.Done, keyboard.status);
+        keyboard.selection = new RangeInt(targetText.Length, 0);
+
+        yield return HideKeyboard();
+
+        Assert.AreEqual(new MyRangeInt(targetText.Length, 0), (MyRangeInt)keyboard.selection);
     }
 }
