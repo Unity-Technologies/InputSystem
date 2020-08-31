eba26b0421f74f0bc0ecdf2638e30d395e213cf2
Tomas Dirvanauskas <tomas@unity3d.com>
2020-08-31 12:02:20 +0300

    Remove IScreenKeyboardCallbackReceiver

--- Packages/com.unity.inputsystem/InputSystem/Devices/IScreenKeyboardCallbackReceiver.cs
+++
@@ -1,9 +0,0 @@
-/*namespace UnityEngine.InputSystem.LowLevel
-{
-    // TODO: is this needed
-    public interface IScreenKeyboardCallbackReceiver
-    {
-        void OnScreenKeyboardPropertiesChanged(ScreenKeyboardProperties keyboardProperties);
-    }
-}
-*/
