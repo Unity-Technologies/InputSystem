615684948fafa6d73f7b2ffb949e68c7e4d7e303
Dmytro Ivanov <dmytro@unity3d.com>
2020-11-20 15:55:32 +0100

    Nuking local doctools for now

--- Packages/com.unity.package-manager-doctools/Editor/Sources/Services/Doc/GlobalSettings.cs
+++
@@ -1,7 +0,0 @@
-namespace UnityEditor.PackageManager.DocumentationTools.UI
-{
-    internal static class GlobalSettings
-    {
-        public static bool Verbose { get; set; }
-    }
-}
--- Packages/com.unity.package-manager-doctools/README.md
+++
@@ -1,3 +0,0 @@
-# Unity Package Manager Documentation Tools
-
-Unity Package Manager Documentation Generation Tools as a Unity Package
