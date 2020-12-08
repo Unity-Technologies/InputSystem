9538a2fcdaf2e8364754a744c404cff71bf444a4
Tomas Dirvanauskas <tomas@unity3d.com>
2020-12-08 12:24:52 +0200

    Forgot to commit

--- Packages/com.unity.inputsystem/InputSystem/Devices/DefaultScreenKeyboardFactory.cs
+++ Packages/com.unity.inputsystem/InputSystem/Devices/DefaultScreenKeyboardFactory.cs
@@ -21,4 +21,24 @@ namespace UnityEngine.InputSystem
             }
         }
     }
+
+    [Preserve]
+    public class DefaultScreenKeyboardFactory2 : IScreenKeyboardFactory
+    {
+        public ScreenKeyboard Create()
+        {
+            // Note: Not using defines, since it hides compile errors
+            switch (Application.platform)
+            {
+#if !DISABLE_SCREEN_KEYBOARD
+                case RuntimePlatform.Android:
+                    return new Android.AndroidScreenKeyboard();
+                case RuntimePlatform.IPhonePlayer:
+                    return new iOS.iOSScreenKeyboard();
+#endif
+                default:
+                    return new EmulatedScreenKeyboard();
+            }
+        }
+    }
 }
