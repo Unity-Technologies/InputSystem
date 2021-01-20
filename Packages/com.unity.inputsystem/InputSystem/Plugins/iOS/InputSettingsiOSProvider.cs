bea731cb51c0d3669963ff308978f816db73d5c3
Tomas Dirvanauskas <tomas@unity3d.com>
2021-01-20 16:57:13 +0200

    Fix tooltip

--- Packages/com.unity.inputsystem/InputSystem/Plugins/iOS/InputSettingsiOSProvider.cs
+++ Packages/com.unity.inputsystem/InputSystem/Plugins/iOS/InputSettingsiOSProvider.cs
@@ -19,7 +19,7 @@ namespace UnityEngine.InputSystem
             m_MotionUsageDescription = parent.FindProperty(prefix + ".m_Description");
 
             m_MotionUsageContent = new GUIContent("Motion Usage", "Enables Motion Usage for the app, required for sensors like Step Counter. This also adds 'Privacy - Motion Usage Description' entry to Info.plist");
-            m_MotionUsageDescriptionContent = new GUIContent("  Description", "Describe why the app wants to access the device's accelerometer.");
+            m_MotionUsageDescriptionContent = new GUIContent("  Description", "Describe why the app wants to access the device's Motion Usage sensor.");
         }
 
         public void OnGUI()
