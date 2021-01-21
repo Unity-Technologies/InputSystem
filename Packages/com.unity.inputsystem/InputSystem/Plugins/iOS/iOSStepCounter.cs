e78c4d035fcae9fd1b6efd353849fb4dcd396cfa
Tomas Dirvanauskas <tomas@unity3d.com>
2021-01-21 12:33:00 +0200

    Fix docs

--- Packages/com.unity.inputsystem/InputSystem/Plugins/iOS/iOSStepCounter.cs
+++ Packages/com.unity.inputsystem/InputSystem/Plugins/iOS/iOSStepCounter.cs
@@ -1,4 +1,4 @@
-#if UNITY_EDITOR || UNITY_IOS || UNITY_TVOS
+#if UNITY_EDITOR || UNITY_IOS || UNITY_TVOS || PACKAGE_DOCS_GENERATION
 using System.Runtime.InteropServices;
 using AOT;
 using Unity.Collections.LowLevel.Unsafe;
