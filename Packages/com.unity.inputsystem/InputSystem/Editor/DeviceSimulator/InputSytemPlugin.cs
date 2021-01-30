293aa33aebbf48002cb3a05d264a5929ab1991d4
Roman Osipov <romano@unity3d.com>
2021-01-30 12:04:13 +0200

    Moved plugin into UnityEngine.InputSystem.Editor namespace and made it internal

--- Packages/com.unity.inputsystem/InputSystem/Editor/DeviceSimulator/InputSytemPlugin.cs
+++ Packages/com.unity.inputsystem/InputSystem/Editor/DeviceSimulator/InputSytemPlugin.cs
@@ -8,82 +8,86 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.LowLevel;
 
-public class InputSystemPlugin : DeviceSimulatorPlugin
+namespace UnityEngine.InputSystem.Editor
 {
-    private bool m_InputSystemEnabled;
-    private Touchscreen m_SimulatorTouchscreen;
-    private List<InputDevice> m_DisabledDevices;
-
-    public override string title => "Input System";
-
-    public override void OnCreate()
+    internal class InputSystemPlugin : DeviceSimulatorPlugin
     {
-        var playerSettings = Resources.FindObjectsOfTypeAll<PlayerSettings>()[0];
-        SerializedObject playerSettingsSerialized = new SerializedObject(playerSettings);
+        private bool m_InputSystemEnabled;
+        private Touchscreen m_SimulatorTouchscreen;
+        private List<InputDevice> m_DisabledDevices;
 
-        var activeInputHandler = playerSettingsSerialized.FindProperty("activeInputHandler").intValue;
-        m_InputSystemEnabled = activeInputHandler == 1 || activeInputHandler == 2;
-        if (m_InputSystemEnabled)
+        public override string title => "Input System";
+
+        public override void OnCreate()
         {
-            deviceSimulator.touchScreenInput += OnTouchEvent;
+            var playerSettings = Resources.FindObjectsOfTypeAll<PlayerSettings>()[0];
+            SerializedObject playerSettingsSerialized = new SerializedObject(playerSettings);
 
-            // UGUI elements like a button don't get pressed when multiple pointers for example mouse and touchscreen are sending data at the same time
-            m_DisabledDevices = new List<InputDevice>();
-            foreach (var device in InputSystem.devices)
+            var activeInputHandler = playerSettingsSerialized.FindProperty("activeInputHandler").intValue;
+            m_InputSystemEnabled = activeInputHandler == 1 || activeInputHandler == 2;
+            if (m_InputSystemEnabled)
             {
-                if (device.native && device is Mouse && device.enabled)
+                deviceSimulator.touchScreenInput += OnTouchEvent;
+
+                // UGUI elements like a button don't get pressed when multiple pointers for example mouse and touchscreen are sending data at the same time
+                m_DisabledDevices = new List<InputDevice>();
+                foreach (var device in InputSystem.devices)
                 {
-                    InputSystem.DisableDevice(device);
-                    m_DisabledDevices.Add(device);
+                    if (device.native && device is Mouse && device.enabled)
+                    {
+                        InputSystem.DisableDevice(device);
+                        m_DisabledDevices.Add(device);
+                    }
                 }
+
+                m_SimulatorTouchscreen = InputSystem.AddDevice<Touchscreen>("Device Simulator Touchscreen");
             }
-            m_SimulatorTouchscreen = InputSystem.AddDevice<Touchscreen>("Device Simulator Touchscreen");
         }
-    }
 
-    private void OnTouchEvent(TouchEvent touchEvent)
-    {
-        // Input System does not accept 0 as id
-        var id = touchEvent.touchId + 1;
-
-        InputSystem.QueueStateEvent(m_SimulatorTouchscreen,
-            new TouchState
-            {
-                touchId = id,
-                phase = ToInputSystem(touchEvent.phase),
-                position = touchEvent.position
-            });
-    }
-
-    private static UnityEngine.InputSystem.TouchPhase ToInputSystem(UnityEditor.DeviceSimulation.TouchPhase original)
-    {
-        switch (original)
+        private void OnTouchEvent(TouchEvent touchEvent)
         {
-            case UnityEditor.DeviceSimulation.TouchPhase.Began:
-                return UnityEngine.InputSystem.TouchPhase.Began;
-            case UnityEditor.DeviceSimulation.TouchPhase.Moved:
-                return UnityEngine.InputSystem.TouchPhase.Moved;
-            case UnityEditor.DeviceSimulation.TouchPhase.Ended:
-                return UnityEngine.InputSystem.TouchPhase.Ended;
-            case UnityEditor.DeviceSimulation.TouchPhase.Canceled:
-                return UnityEngine.InputSystem.TouchPhase.Canceled;
-            case UnityEditor.DeviceSimulation.TouchPhase.Stationary:
-                return UnityEngine.InputSystem.TouchPhase.Stationary;
-            default:
-                throw new ArgumentOutOfRangeException(nameof(original), original, "Unexpected value");
+            // Input System does not accept 0 as id
+            var id = touchEvent.touchId + 1;
+
+            InputSystem.QueueStateEvent(m_SimulatorTouchscreen,
+                new TouchState
+                {
+                    touchId = id,
+                    phase = ToInputSystem(touchEvent.phase),
+                    position = touchEvent.position
+                });
         }
-    }
 
-    public override void OnDestroy()
-    {
-        if (m_InputSystemEnabled)
+        private static UnityEngine.InputSystem.TouchPhase ToInputSystem(UnityEditor.DeviceSimulation.TouchPhase original)
         {
-            deviceSimulator.touchScreenInput -= OnTouchEvent;
-            if (m_SimulatorTouchscreen != null)
-                InputSystem.RemoveDevice(m_SimulatorTouchscreen);
-            foreach (var device in m_DisabledDevices)
+            switch (original)
             {
-                InputSystem.EnableDevice(device);
+                case UnityEditor.DeviceSimulation.TouchPhase.Began:
+                    return UnityEngine.InputSystem.TouchPhase.Began;
+                case UnityEditor.DeviceSimulation.TouchPhase.Moved:
+                    return UnityEngine.InputSystem.TouchPhase.Moved;
+                case UnityEditor.DeviceSimulation.TouchPhase.Ended:
+                    return UnityEngine.InputSystem.TouchPhase.Ended;
+                case UnityEditor.DeviceSimulation.TouchPhase.Canceled:
+                    return UnityEngine.InputSystem.TouchPhase.Canceled;
+                case UnityEditor.DeviceSimulation.TouchPhase.Stationary:
+                    return UnityEngine.InputSystem.TouchPhase.Stationary;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(original), original, "Unexpected value");
+            }
+        }
+
+        public override void OnDestroy()
+        {
+            if (m_InputSystemEnabled)
+            {
+                deviceSimulator.touchScreenInput -= OnTouchEvent;
+                if (m_SimulatorTouchscreen != null)
+                    InputSystem.RemoveDevice(m_SimulatorTouchscreen);
+                foreach (var device in m_DisabledDevices)
+                {
+                    InputSystem.EnableDevice(device);
+                }
             }
         }
     }
