963fa6af1f901eb1d6286d8ebc05eae4c7ce803e
StayTalm <tomb@unity3d.com>
2021-03-01 19:02:35 -0800

    Add Lumin and Extra define to force enable XR

--- Packages/com.unity.inputsystem/InputSystem/InputSystem.cs
+++ Packages/com.unity.inputsystem/InputSystem/InputSystem.cs
@@ -3166,7 +3166,7 @@ namespace UnityEngine.InputSystem
             Switch.SwitchSupportHID.Initialize();
             #endif
 
-            #if (UNITY_EDITOR || UNITY_STANDALONE || UNITY_ANDROID || UNITY_IOS || UNITY_WSA || UNITY_SWITCH) && UNITY_INPUT_SYSTEM_ENABLE_XR && ENABLE_VR
+            #if (UNITY_EDITOR || UNITY_STANDALONE || UNITY_ANDROID || UNITY_IOS || UNITY_WSA || UNITY_SWITCH || UNITY_LUMIN || UNITY_INPUT_FORCE_XR_PLUGIN) && UNITY_INPUT_SYSTEM_ENABLE_XR && ENABLE_VR
             XR.XRSupport.Initialize();
             #endif
 
