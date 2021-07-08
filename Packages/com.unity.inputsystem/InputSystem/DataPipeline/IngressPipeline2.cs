4db06205d804195301482189c3cce2a9429a18d5
Dmytro Ivanov <dmytro@unity3d.com>
2021-07-08 15:52:46 +0200

    wip

--- Packages/com.unity.inputsystem/InputSystem/DataPipeline/IngressPipeline2.cs
+++ Packages/com.unity.inputsystem/InputSystem/DataPipeline/IngressPipeline2.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Unity.Collections.LowLevel.Unsafe;
 using UnityEngine.InputSystem.LowLevel;
 
 namespace UnityEngine.InputSystem.DataPipeline
@@ -9,8 +10,37 @@ namespace UnityEngine.InputSystem.DataPipeline
         {
         }
 
-        public void ProcessEvents(InputUpdateType updateType, double processUntilTimestamp, ref InputEventBuffer events)
+        public unsafe void ProcessEvents(InputUpdateType updateType, double processUntilTimestamp, ref InputEventBuffer events)
         {
+            if (events.sizeInBytes == InputEventBuffer.BufferSizeUnknown)
+                return;
+
+            if (events.sizeInBytes == 0)
+                return;
+
+            InputEvent* current = events.bufferPtr; 
+            for(var i = 0; i < events.eventCount; ++i)
+            {
+                var next = InputEvent.GetNextInMemory(current);
+
+                if (current->type == StateEvent.Type)
+                {
+                    var stateEvent = StateEvent.FromUnchecked(current);
+
+                    if (stateEvent->stateFormat == MouseState.Format)
+                    {
+                    }
+                }
+                else if (current->type == DeltaStateEvent.Type)
+                {
+                    
+                }
+                
+                if (!events.Contains(next))
+                    break;
+
+                current = next;
+            }
         }
         
         public void Dispose()
