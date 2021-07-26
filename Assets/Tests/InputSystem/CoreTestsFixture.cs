903f5ff514158bb2884958b7e96cfb2ee23b6191
Rene Damm <rene@unity3d.com>
2021-07-26 12:26:13 +0200

    FIX: CoreTestsFixture not cleaning up additional scenes.

--- Assets/Tests/InputSystem/CoreTestsFixture.cs
+++ Assets/Tests/InputSystem/CoreTestsFixture.cs
@@ -5,15 +5,21 @@ public class CoreTestsFixture : InputTestFixture
 {
     public override void TearDown()
     {
-        // Destroy any GameObject in the current scene that isn't hidden and isn't the
+        // Destroy any GameObject in the first scene that isn't hidden and isn't the
         // test runner object. Do this first so that any cleanup finds the system in the
         // state it expects.
-        var scene = SceneManager.GetActiveScene();
-        foreach (var go in scene.GetRootGameObjects())
+        foreach (var go in SceneManager.GetSceneAt(0).GetRootGameObjects())
         {
             if (go.hideFlags != 0 || go.name.Contains("tests runner"))
                 continue;
             UnityEngine.Object.DestroyImmediate(go);
+        }
+
+        // Unload any additional scenes.
+        for (var i = 1; i < SceneManager.sceneCount; ++i)
+        {
+            var scene = SceneManager.GetSceneAt(i);
+            SceneManager.UnloadSceneAsync(scene);
         }
 
         base.TearDown();
