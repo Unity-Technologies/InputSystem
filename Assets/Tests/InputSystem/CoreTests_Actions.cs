356b3ed4a8beb60eed18a5145cebdfef4967f3bd
Håkan Sidenvall <hakan.sidenvall@unity3d.com>
2021-08-18 10:02:51 +0200

    Temporarily ignoring test to find root cause of failing CI

--- Assets/Tests/InputSystem/CoreTests_Actions.cs
+++ Assets/Tests/InputSystem/CoreTests_Actions.cs
@@ -511,6 +511,7 @@ partial class CoreTests
 
     [Test]
     [Category("Actions")]
+    [Ignore("Temporarily disabled to bisect CI timeout problems")] // TODO Reenable
     public void Actions_ButtonAndPassThroughActions_CanTurnOnInitialStateCheck()
     {
         var gamepad = InputSystem.AddDevice<Gamepad>();
