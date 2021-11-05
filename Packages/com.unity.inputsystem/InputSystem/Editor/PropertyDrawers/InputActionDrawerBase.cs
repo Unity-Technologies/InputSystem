4d195ab2699ce70f6b2318659c198a506004c998
Håkan Sidenvall <hakan.sidenvall@unity3d.com>
2021-11-05 10:50:03 +0100

    Fixed incorrectly formatted display names for InputAction when serialized

--- Packages/com.unity.inputsystem/InputSystem/Editor/PropertyDrawers/InputActionDrawerBase.cs
+++ Packages/com.unity.inputsystem/InputSystem/Editor/PropertyDrawers/InputActionDrawerBase.cs
@@ -102,13 +102,15 @@ namespace UnityEngine.InputSystem.Editor
 
         private static string GetPropertyTitle(SerializedProperty property)
         {
-            var propertyTitleNumeral = string.Empty;
+            if (property.GetParentProperty() == null && property.displayName != null &&
+                property.displayName.Length > 0 && property.type == nameof(InputAction))
+            {
+                return $"{property.displayName}";
+            }
 
+            var propertyTitleNumeral = string.Empty;
             if (property.GetParentProperty() != null && property.GetParentProperty().isArray)
                 propertyTitleNumeral = $" {property.GetIndexOfArrayElement()}";
-            if (property.type == nameof(InputAction) && property.displayName != null && property.displayName.Length > 0)
-                return $"{property.displayName}";
-
             return property.type == nameof(InputActionMap) ?
                 $"Input Action Map{propertyTitleNumeral}" :
                 $"Input Action{propertyTitleNumeral}";
