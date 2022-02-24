a1a9b7a15377e567167f1be5bd18d6b8a35c75ab
Andrew O'Connor <andrew.oconnor@unity3d.com>
2022-02-24 14:07:54 +0100

    Implemented a view architecture for views to only update when relevant view state has changed
    * Fixed some styling across light and dark themes, and other styling issues
    * Show correct icons on actions and bindings

--- Packages/com.unity.inputsystem/InputSystem/Editor/GlobalAssetEditor/Views/BindingPropertiesView.cs
+++ Packages/com.unity.inputsystem/InputSystem/Editor/GlobalAssetEditor/Views/BindingPropertiesView.cs
@@ -1,24 +1,25 @@
-﻿using System.Linq;
-using UnityEditor;
-using UnityEngine.InputSystem.Editor.Lists;
-using UnityEngine.InputSystem.Utilities;
-using UnityEngine.UIElements;
+﻿using UnityEngine.UIElements;
 
 namespace UnityEngine.InputSystem.Editor
 {
-	internal class BindingPropertiesView : UIToolkitView
+	internal class BindingPropertiesView : UIToolkitView<GlobalInputActionsEditorState>
 	{
 		private readonly VisualElement m_Root;
 		private readonly Foldout m_ParentFoldout;
+		private CompositeBindingPropertiesView m_CompositeBindingPropertiesView;
+		private CompositePartBindingPropertiesView m_CompositePartBindingPropertiesView;
 
 		public BindingPropertiesView(VisualElement root, Foldout foldout, StateContainer stateContainer)
 			: base(stateContainer)
 		{
 			m_Root = root;
 			m_ParentFoldout = foldout;
+
+			CreateSelector(state => state.selectedBindingIndex.value,
+				(_, state) => state);
 		}
 
-		public override void CreateUI(GlobalInputActionsEditorState state)
+		public override void RedrawUI(GlobalInputActionsEditorState state)
 		{
 			var selectedBinding = state.selectedBindingIndex.value;
 			if (selectedBinding == -1)
@@ -29,71 +30,12 @@ namespace UnityEngine.InputSystem.Editor
 			var binding = Selectors.GetSelectedBinding(state);
 			if (binding.isComposite)
 			{
-				var inputAction = Selectors.GetSelectedAction(state);
-				var compositeTypes = Selectors.GetCompositeTypes(binding.path, inputAction.expectedControlType).ToList();
-
-				var compositeNameAndParameters = NameAndParameters.Parse(binding.path);
-				var compositeName = compositeNameAndParameters.name;
-				var compositeType = InputBindingComposite.s_Composites.LookupTypeRegistration(compositeName);
-
-				var compositeParameters = new ParameterListView();
-				compositeParameters.onChange = () => Dispatch(
-					Commands.UpdatePathNameAndValues(compositeParameters.GetParameters(), Selectors.GetSelectedBindingPath(state)));
-
-				if (compositeType != null)
-					compositeParameters.Initialize(compositeType, compositeNameAndParameters.parameters);
-
-				var compositeTypeField = new DropdownField("Composite Type")
-				{
-					tooltip = GlobalInputActionsConstants.CompositeTypeTooltip
-				};
-				compositeTypeField.choices.AddRange(compositeTypes.Select(ObjectNames.NicifyVariableName));
-				compositeTypeField.index = compositeTypes.FindIndex(str =>
-					InputBindingComposite.s_Composites.LookupTypeRegistration(str) == compositeType);
-			    
-				compositeTypeField.RegisterValueChangedCallback(evt =>
-					Dispatch(
-						Commands.SetCompositeBindingType(
-							binding,
-							new NameAndParameters
-							{
-								name = compositeTypes[compositeTypeField.index],
-								parameters = compositeParameters.GetParameters()
-							})));
-
 				m_ParentFoldout.text = "Composite";
-				m_Root.Add(compositeTypeField);
-
-				compositeParameters.OnDrawVisualElements(m_Root);
+				m_CompositeBindingPropertiesView = CreateChildView(new CompositeBindingPropertiesView(m_Root, stateContainer));
 			}
 			else if (binding.isPartOfComposite)
 			{
-				// TODO: Persist control picker state
-				var controlPathEditor = new InputControlPathEditor(Selectors.GetSelectedBindingPath(state), new InputControlPickerState(),
-					() => { /* the InputControlPathEditor already updates the serialized object internally, so we have nothing to do here */ });
-
-				var partName = binding.name;
-				var compositeName = binding.compositePath;
-				var layoutName = InputBindingComposite.GetExpectedControlLayoutName(compositeName, partName);
-
-				controlPathEditor.SetExpectedControlLayout(layoutName ?? "");
-
-				var controlPathContainer = new IMGUIContainer(controlPathEditor.OnGUI);
-				m_Root.Add(controlPathContainer);
-
-
-				var compositeParts = Selectors.GetCompositePartOptions(binding.name, binding.compositePath).ToList();
-				var compositePartField = new DropdownField("Composite Part", 
-					compositeParts.Select(ObjectNames.NicifyVariableName).ToList(),
-					compositeParts.FindIndex(str => str == binding.name))
-				{
-					tooltip = GlobalInputActionsConstants.CompositePartAssignmentTooltip
-				};
-				compositePartField.RegisterValueChangedCallback(evt =>
-				{
-					Dispatch(Commands.SetCompositeBindingPartName(Selectors.GetSelectedBinding(state), evt.newValue));
-				});
-				m_Root.Add(compositePartField);
+				m_CompositePartBindingPropertiesView = CreateChildView(new CompositePartBindingPropertiesView(m_Root, stateContainer));
 			}
 			else
 			{
@@ -108,6 +50,12 @@ namespace UnityEngine.InputSystem.Editor
 				var controlPathContainer = new IMGUIContainer(controlPathEditor.OnGUI);
 				m_Root.Add(controlPathContainer);
 			}
+		}
+
+		public override void DestroyView()
+		{
+			m_CompositeBindingPropertiesView?.DestroyView();
+			m_CompositePartBindingPropertiesView?.DestroyView();
 		}
 	}
 }
