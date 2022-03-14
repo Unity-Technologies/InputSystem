4c3164725f35270499ddff6709737ee6e9489b0c
Dmytro Ivanov <jimon.j1m0n@gmail.com>
2022-03-14 13:37:14 +0100

    NEW: Added a setting to enable Windows.Gaming.Input on relevant platforms (#1512)

--- Packages/com.unity.inputsystem/InputSystem/InputFeatureNames.cs
+++ Packages/com.unity.inputsystem/InputSystem/InputFeatureNames.cs
@@ -4,5 +4,6 @@ namespace UnityEngine.InputSystem
     {
         public const string kRunPlayerUpdatesInEditMode = "RUN_PLAYER_UPDATES_IN_EDIT_MODE";
         public const string kDisableUnityRemoteSupport = "DISABLE_UNITY_REMOTE_SUPPORT";
+        public const string kUseWindowsGamingInputBackend = "USE_WINDOWS_GAMING_INPUT_BACKEND";
     }
 }
