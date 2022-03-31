c165226a4d8d797bd05ab3025f48d0534bc86ee4
Andrew O'Connor <andrew.oconnor@unity3d.com>
2022-03-31 20:37:10 +0200

    Removed test editor window and made the new editor the default for opening input assets

---
+++ Packages/com.unity.inputsystem/InputSystem/Editor/UITKAssetEditor/InputActionsEditorWindow.cs
@@ -0,0 +1,51 @@
+using System;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEngine.UIElements;
+
+namespace UnityEngine.InputSystem.Editor
+{
+	internal class InputActionsEditorWindow : EditorWindow
+	{
+		private static readonly string k_FileExtension = "." + InputActionAsset.Extension;
+
+		[OnOpenAsset]
+		public static bool OpenAsset(int instanceId, int line)
+		{
+			var path = AssetDatabase.GetAssetPath(instanceId);
+			if (!path.EndsWith(k_FileExtension, StringComparison.InvariantCultureIgnoreCase))
+				return false;
+
+			// Grab InputActionAsset.
+			// NOTE: We defer checking out an asset until we save it. This allows a user to open an .inputactions asset and look at it
+			//       without forcing a checkout.
+			var obj = EditorUtility.InstanceIDToObject(instanceId);
+			var asset = obj as InputActionAsset;
+			if (asset == null)
+				return false;
+			
+			var window = GetWindow<InputActionsEditorWindow>();
+			window.titleContent = new GUIContent("Input Actions Editor");
+			window.SetAsset(asset);
+			window.Show();
+
+			return true;
+		}
+
+		private void SetAsset(InputActionAsset asset)
+		{
+			// var asset = AssetDatabase.LoadAssetAtPath<InputActionAsset>("Assets/GlobalInputActionsTest.inputactions");
+			var serializedAsset = new SerializedObject(asset);
+			var stateContainer = new StateContainer(rootVisualElement, new InputActionsEditorState(serializedAsset));
+		
+			var theme = EditorGUIUtility.isProSkin
+				? AssetDatabase.LoadAssetAtPath<StyleSheet>(InputActionsEditorConstants.PackagePath + InputActionsEditorConstants.ResourcesPath + "/InputAssetEditorDark.uss")
+				: AssetDatabase.LoadAssetAtPath<StyleSheet>(InputActionsEditorConstants.PackagePath + InputActionsEditorConstants.ResourcesPath + "/InputAssetEditorLight.uss");
+			
+			rootVisualElement.styleSheets.Add(theme);
+
+			var view = new InputActionsEditorView(rootVisualElement, stateContainer);
+			stateContainer.Initialize();
+		}
+	}
+}
