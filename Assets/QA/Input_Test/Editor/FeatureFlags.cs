d593e284bf02b2f961853dae1e8b58cf6db06b70
Håkan Sidenvall <hakan.sidenvall@unity3d.com>
2022-07-05 17:44:30 +0200

    Updated FeatureFlags to a PlayerSettings based implementation.

--- Assets/QA/Input_Test/Editor/FeatureFlags.cs
+++ Assets/QA/Input_Test/Editor/FeatureFlags.cs
@@ -1,25 +1,48 @@
 using UnityEditor;
+using System.IO;
 
 namespace UnityEngine.InputSystem
 {
     [InitializeOnLoad]
     public class FeatureFlags
     {
-        private static readonly string useWindowsGamingInputBackendName = "USE_WINDOWS_GAMING_INPUT_BACKEND";
-        private static bool useWindowsGamingInputBackend = false;
-
-        private static void SetInternalFeatureFlag(string setting, bool enabled)
+#if UNITY_EDITOR_WIN
+        private static bool ChangePlayerSetting(WindowsGamepadBackendHint hint)
         {
-            Debug.Log($"Feature flag \"{setting}\" set to: {(enabled ? "On" : "Off")}");
-            InputSystem.settings.SetInternalFeatureFlag(setting, enabled);
-        }
+            if (hint != PlayerSettings.windowsGamepadBackendHint)
+            {
+                if (EditorUtility.DisplayDialog(
+                    "Change Windows Gamepad Backend Hint Setting in PlayerSettings?",
+                    $"Are you sure you want to change PlayerSettings.windowsGamepadBackendHint to {hint}? This requires Unity to be restarted to have effect.",
+                    "Yes (Restart Now)",
+                    "No"))
+                {
+                    PlayerSettings.windowsGamepadBackendHint = hint;
+                    EditorApplication.OpenProject(Directory.GetCurrentDirectory());
+                }
+            }
 
-        [MenuItem("QA Tools/Input Features/Toggle USE_WINDOWS_GAMING_INPUT_BACKEND", false, 0)]
-        static bool SwitchGamepadBackend()
-        {
-            useWindowsGamingInputBackend = !useWindowsGamingInputBackend;
-            SetInternalFeatureFlag(useWindowsGamingInputBackendName, useWindowsGamingInputBackend);
             return true;
         }
+
+        [MenuItem("QA Tools/Input Features/Windows Gamepad Backend Hint/Set to \"Default\"", false, 0)]
+        static bool UseDefault()
+        {
+            return ChangePlayerSetting(WindowsGamepadBackendHint.kWindowsGamepadBackendHintDefault);
+        }
+
+        [MenuItem("QA Tools/Input Features/Windows Gamepad Backend Hint/Set to \"XInput\"", false, 0)]
+        static bool UseXInput()
+        {
+            return ChangePlayerSetting(WindowsGamepadBackendHint.kWindowsGamepadBackendHintXInput);
+        }
+
+        [MenuItem("QA Tools/Input Features/Windows Gamepad Backend Hint/Set to \"Windows.Gaming.Input\"", false, 0)]
+        static bool UseWgi()
+        {
+            return ChangePlayerSetting(WindowsGamepadBackendHint.kWindowsGamepadBackendHintWindowsGamingInput);
+        }
+
+#endif // UNITY_EDITOR_WIN
     }
 }
