0c2d01a90acadffd2e182795a39569ff459c5023
Dmytro Ivanov <dmytro@unity3d.com>
2022-09-06 11:31:06 +0200

    Revert "Fixed behavior field repaint when switching input actions"
    This reverts commit 0b65541fe00bef9120d27754fa7e1f6ae4c285f2.

--- Packages/com.unity.inputsystem/InputSystem/Plugins/PlayerInput/PlayerInputEditor.cs
+++ Packages/com.unity.inputsystem/InputSystem/Plugins/PlayerInput/PlayerInputEditor.cs
@@ -73,13 +73,8 @@ namespace UnityEngine.InputSystem.Editor
             // Action config section.
             EditorGUI.BeginChangeCheck();
             EditorGUILayout.PropertyField(m_ActionsProperty);
-            var actionsWhereChanged = false;
             if (EditorGUI.EndChangeCheck() || !m_ActionAssetInitialized)
-            {
                 OnActionAssetChange();
-                actionsWhereChanged = true;
-            }
-
             ++EditorGUI.indentLevel;
             if (m_ControlSchemeOptions != null && m_ControlSchemeOptions.Length > 1) // Don't show if <Any> is the only option.
             {
@@ -167,7 +162,7 @@ namespace UnityEngine.InputSystem.Editor
             // Notifications/event section.
             EditorGUI.BeginChangeCheck();
             EditorGUILayout.PropertyField(m_NotificationBehaviorProperty, m_NotificationBehaviorText);
-            if (EditorGUI.EndChangeCheck() || actionsWhereChanged || !m_NotificationBehaviorInitialized)
+            if (EditorGUI.EndChangeCheck() || !m_NotificationBehaviorInitialized)
                 OnNotificationBehaviorChange();
             switch ((PlayerNotifications)m_NotificationBehaviorProperty.intValue)
             {
