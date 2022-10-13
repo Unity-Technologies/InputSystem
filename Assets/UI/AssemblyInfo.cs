3a3d2fade1a88413d499d4a56097f210a4bf3c40
Dmytro Ivanov <dmytro@unity3d.com>
2022-10-13 14:24:39 +0200

    Moving code to ugui

--- Assets/UI/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-using System.Reflection;
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Unity.InputSystem.Tests.Editor")]
-[assembly: InternalsVisibleTo("Unity.InputSystem.Tests")]
-[assembly: InternalsVisibleTo("Unity.InputSystem.IntegrationTests")]
-
