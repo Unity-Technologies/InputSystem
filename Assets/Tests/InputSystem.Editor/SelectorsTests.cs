f3491847ab500f8fb4f534cbe34daf0448bfa9dc
James McGill <jamesmcgill@users.noreply.github.com>
2023-03-19 18:36:24 +0100

    NEW: UITK Editor separate action maps and actions views (#1661)
    * Replaced action map drop down with ListView and bindings view with tree view showing actions and bindings
    
    * Formatter
    
    * Disabled code for renaming items in action maps and actions view
    
    * Formatter
    
    * Replaced action map drop down with ListView and bindings view with tree view showing actions and bindings
    
    ---------
    
    Co-authored-by: Andrew O'Connor <andrew.oconnor@unity3d.com>

---
+++ Assets/Tests/InputSystem.Editor/SelectorsTests.cs
@@ -0,0 +1,41 @@
+using System.Linq;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Editor;
+
+internal class SelectorsTests
+{
+    [Test]
+    [Category("AssetEditor")]
+    public void GetActionsAsTreeViewData_ReturnsActionsAndBindingsAsTreeViewData()
+    {
+        var asset = ScriptableObject.CreateInstance<InputActionAsset>();
+        var actionMap = asset.AddActionMap("ActionMap");
+        var actionOne = actionMap.AddAction("Action1", binding: "<Keyboard>/a");
+        actionOne.AddCompositeBinding("2DVector")
+            .With("left", "<Gamepad>/rightStick/x")
+            .With("right", "<Gamepad>/rightStick/y");
+        var actionTwo = actionMap.AddAction("Action2", binding: "<Keyboard>/d");
+
+
+        var treeViewData = Selectors.GetActionsAsTreeViewData(TestData.EditorStateWithAsset(asset).Generate());
+
+
+        Assert.That(treeViewData.Count, Is.EqualTo(2));
+        Assert.That(treeViewData[0].data.name, Is.EqualTo("Action1"));
+        Assert.That(treeViewData[1].data.name, Is.EqualTo("Action2"));
+
+        Assert.That(treeViewData[0].hasChildren, Is.True);
+        Assert.That(treeViewData[0].children.ElementAt(0).data.name, Is.EqualTo(InputControlPath.ToHumanReadableString(actionOne.bindings[0].path)));
+        Assert.That(treeViewData[0].children.ElementAt(1).data.name, Is.EqualTo("2DVector"));
+
+        Assert.That(treeViewData[0].children.ElementAt(1).hasChildren, Is.True);
+        Assert.That(treeViewData[0].children.ElementAt(1).children.ElementAt(0).data.name, Is.EqualTo("Left: " + InputControlPath.ToHumanReadableString("<Gamepad>/rightStick/x")));
+        Assert.That(treeViewData[0].children.ElementAt(1).children.ElementAt(1).data.name, Is.EqualTo("Right: " + InputControlPath.ToHumanReadableString("<Gamepad>/rightStick/y")));
+
+
+        Assert.That(treeViewData[1].hasChildren, Is.True);
+        Assert.That(treeViewData[1].children.ElementAt(0).data.name, Is.EqualTo(InputControlPath.ToHumanReadableString(actionTwo.bindings[0].path)));
+    }
+}
---
+++ Packages/com.unity.inputsystem/InputSystem/Editor/UITKAssetEditor/Views/InputActionsTreeViewItem.cs
@@ -0,0 +1,108 @@
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.InputSystem.Editor;
+using UnityEngine.UIElements;
+
+/// <summary>
+/// A visual element that supports renaming of items.
+/// </summary>
+public class InputActionsTreeViewItem : VisualElement
+{
+    private const string kRenameTextField = "rename-text-field";
+    public event EventCallback<string> EditTextFinished;
+
+    public InputActionsTreeViewItem()
+    {
+        var template = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>(
+            InputActionsEditorConstants.PackagePath +
+            InputActionsEditorConstants.ResourcesPath +
+            InputActionsEditorConstants.InputActionsTreeViewItemUxml);
+        template.CloneTree(this);
+
+        focusable = true;
+        delegatesFocus = false;
+
+        renameTextfield.selectAllOnFocus = true;
+        renameTextfield.selectAllOnMouseUp = false;
+
+        // TODO: The rename functionality is currently disabled due to focus issues. The text field doesn't
+        // focus correctly when calling Focus() on it (it needs to be subsequently clicked) and on losing focus,
+        // we're getting two focus out events, one for the TextField and one for the contained TextElement.
+        // RegisterCallback<MouseDownEvent>(OnMouseDownEventForRename);
+        // RegisterCallback<KeyDownEvent>(OnKeyDownEventForRename);
+        //
+        // renameTextfield.RegisterCallback<KeyUpEvent>(e =>
+        // {
+        //  if (e.keyCode == KeyCode.Return || e.keyCode == KeyCode.KeypadEnter || e.keyCode == KeyCode.Escape)
+        //  {
+        //   (e.currentTarget as VisualElement)?.Blur();
+        //   return;
+        //  }
+        //
+        //  e.StopImmediatePropagation();
+        // });
+        //
+        // renameTextfield.RegisterCallback<FocusOutEvent>(e =>
+        // {
+        //  OnEditTextFinished(renameTextfield);
+        // });
+    }
+
+    public Label label => this.Q<Label>();
+    public TextField renameTextfield => this.Q<TextField>(kRenameTextField);
+
+    private void OnKeyDownEventForRename(KeyDownEvent e)
+    {
+        if (e.keyCode != KeyCode.F2)
+            return;
+
+        FocusOnRenameTextField();
+        e.PreventDefault();
+    }
+
+    private void OnMouseDownEventForRename(MouseDownEvent e)
+    {
+        if (e.clickCount != 2 || e.button != (int)MouseButton.LeftMouse || e.target == null)
+            return;
+
+        FocusOnRenameTextField();
+
+        e.StopPropagation();
+    }
+
+    private void FocusOnRenameTextField()
+    {
+        delegatesFocus = true;
+
+        renameTextfield.SetValueWithoutNotify(label.text);
+        renameTextfield.RemoveFromClassList(InputActionsEditorConstants.HiddenStyleClassName);
+        label?.AddToClassList(InputActionsEditorConstants.HiddenStyleClassName);
+
+        renameTextfield.Q<TextElement>().Focus();
+        renameTextfield.SelectAll();
+    }
+
+    public void OnEditTextFinished(TextField renameTextField)
+    {
+        delegatesFocus = false;
+
+        var text = renameTextField.value?.Trim();
+        if (string.IsNullOrEmpty(text))
+        {
+            renameTextField.schedule.Execute(() =>
+            {
+                FocusOnRenameTextField();
+                renameTextField.SetValueWithoutNotify(text);
+            });
+            return;
+        }
+
+        renameTextField.AddToClassList(InputActionsEditorConstants.HiddenStyleClassName);
+        label.RemoveFromClassList(InputActionsEditorConstants.HiddenStyleClassName);
+        label.text = renameTextField.text;
+
+        EditTextFinished?.Invoke(text);
+    }
+}
+#endif
