7e6a0290306cffbe6e38b24a2aaad69d3ad9e336
James McGill <jamesmcgill@users.noreply.github.com>
2023-04-11 22:39:13 +0200

    NEW: View-only UIToolkit version of Input Actions Asset Editor (#1668)
    * Change yamato config to build develop-v2 branch, but only build against trunk
    
    * Basic UITK version of input asset editor
    
    Mostly read only for now. Doesn't contain things like drag and drop for list views, adding/delete/editing action maps, action, or bindings.
    
    * Implemented a view architecture for views to only update when relevant view state has changed
    
    * Fixed some styling across light and dark themes, and other styling issues
    * Show correct icons on actions and bindings
    
    * Fix InputControlPicker changes not being saved
    
    * Removing meta file that should have been removed before
    
    * State is now closer to properly immutable and removed usages of ReactiveProperty
    
    * Removed ReactiveProperty properties from state. All changes now go through the state container, and that should be the only way to get state change events.
    * Added the With method to GlobalInputActionsEditorState to make it easier to mutate into new instances.
    * Mutating state actions now return new instance of the state and the state container ensures that the latest version is used everywhere.
    
    * Remove Global from all names and moved everything to UITKAssetEditor folder
    
    * Add control scheme editor
    
    Also renamed UIToolkitView to ViewBase
    
    * Remove temporary test assets
    
    * Removed test editor window and made the new editor the default for opening input assets
    
    * Added project context menu to open legacy editor from input asset
    
    * Added ability to assign bindings to control schemes
    
    * Formatting
    
    * Fix issue in CompositeBindingPropertoesView where parameter values would not be updated correctly
    
    * Review changes
    
    * Added missing UNITY_EDITOR ifdefs
    
    * Change dry run CI config to use 2022.2.0a10
    
    * Only show the UITK editor when the feature flag is set, and only on 2022.1
    
    * Some changes to get things compiling on versions of Unity less than 2022.1
    
    * More changes to fix build on 2019.4
    
    * Update package to 1.5
    
    The InputParameterEditor class had OnDrawVisualElements public method added to it.
    
    * Fix incorrect UIToolkit uss reference
    
    * Revert "Change yamato config to build develop-v2 branch, but only build against trunk"
    
    This reverts commit aaa54c18b25a156aab3badd85bc7bfc4813d36dc.
    
    * Revert "Update package to 1.5"
    
    This reverts commit 8657d1e6521243395c58e9c0c2fecabd385e1979.
    
    * Moved some UITK editor specific code behind 2022.1 conditionals
    
    The UITK asset editor is only usable on those versions of Unity
    
    * NEW: Add save and auto-save buttons
    
    * NEW: Add save and auto-save buttons
    
    * NEW: Recreate editor UI after domain reload
    
    * Review changes: restore SerializeObject in InputActionEditorState
    
    * Review changes: Move save logic out of the View
    
    * Review changes: Move save logic into the Window
    
    * NEW: UITK Editor separate action maps and actions views (#1661)
    
    * Replaced action map drop down with ListView and bindings view with tree view showing actions and bindings
    
    * Formatter
    
    * Disabled code for renaming items in action maps and actions view
    
    * Formatter
    
    * Replaced action map drop down with ListView and bindings view with tree view showing actions and bindings
    
    ---------
    
    Co-authored-by: Andrew O'Connor <andrew.oconnor@unity3d.com>
    
    * fix merge error (again)
    
    * fix formatting
    
    * fix compilation issues on 2019
    
    * fix exception during API validation tests
    
    * fix missing public docs tests
    
    * fix Publish to Internal tests by restoring editor version
    
    * remove new menu item for legacy asset editor
    
    * replace removed code that seems necessary
    
    ---------
    
    Co-authored-by: Andrew O'Connor <andrew.oconnor@unity3d.com>

---
+++ Packages/com.unity.inputsystem/InputSystem/Editor/UITKAssetEditor/Views/Selectors.cs
@@ -0,0 +1,230 @@
+#if UNITY_EDITOR && UNITY_2022_1_OR_NEWER
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine.InputSystem.Controls;
+using UnityEngine.InputSystem.Editor.Lists;
+using UnityEngine.InputSystem.Layouts;
+using UnityEngine.InputSystem.Utilities;
+
+namespace UnityEngine.InputSystem.Editor
+{
+    internal static partial class Selectors
+    {
+        public static IEnumerable<string> GetActionMapNames(InputActionsEditorState state)
+        {
+            return state.serializedObject
+                ?.FindProperty(nameof(InputActionAsset.m_ActionMaps))
+                ?.Select(m => m.FindPropertyRelative(nameof(InputActionMap.m_Name))?.stringValue)
+                ?? Enumerable.Empty<string>();
+        }
+
+        public static IEnumerable<SerializedInputAction> GetActionsForSelectedActionMap(InputActionsEditorState state)
+        {
+            var actionMapIndex = state.selectedActionMapIndex;
+            var actionMaps = state.serializedObject.FindProperty(nameof(InputActionAsset.m_ActionMaps));
+
+            var actionMap = actionMapIndex == -1 ?
+                actionMaps.GetArrayElementAtIndex(0) :
+                actionMaps.GetArrayElementAtIndex(actionMapIndex);
+
+            if (actionMap == null)
+                return Enumerable.Empty<SerializedInputAction>();
+
+            return actionMap
+                .FindPropertyRelative(nameof(InputActionMap.m_Actions))
+                .Select(serializedProperty => new SerializedInputAction(serializedProperty));
+        }
+
+        public static SerializedInputActionMap GetSelectedActionMap(InputActionsEditorState state)
+        {
+            return new SerializedInputActionMap(state.serializedObject
+                ?.FindProperty(nameof(InputActionAsset.m_ActionMaps))
+                ?.GetArrayElementAtIndex(state.selectedActionMapIndex));
+        }
+
+        public static SerializedProperty GetSelectedBindingPath(InputActionsEditorState state)
+        {
+            var actionMapSO = state.serializedObject
+                ?.FindProperty(nameof(InputActionAsset.m_ActionMaps))
+                ?.GetArrayElementAtIndex(state.selectedActionMapIndex);
+
+            return actionMapSO?.FindPropertyRelative(nameof(InputActionMap.m_Bindings))
+                ?.GetArrayElementAtIndex(state.selectedBindingIndex)
+                ?.FindPropertyRelative("m_Path");
+        }
+
+        public static SerializedInputBinding GetSelectedBinding(InputActionsEditorState state)
+        {
+            var actionMapSO = state.serializedObject
+                ?.FindProperty(nameof(InputActionAsset.m_ActionMaps))
+                ?.GetArrayElementAtIndex(state.selectedActionMapIndex);
+            return new SerializedInputBinding(actionMapSO?.FindPropertyRelative(nameof(InputActionMap.m_Bindings))
+                ?.GetArrayElementAtIndex(state.selectedBindingIndex));
+        }
+
+        public static IEnumerable<string> GetCompositeTypes(string path, string expectedControlLayout)
+        {
+            // Find name of current composite.
+            var compositeNameAndParameters = NameAndParameters.Parse(path);
+            var compositeName = compositeNameAndParameters.name;
+            var compositeType = InputBindingComposite.s_Composites.LookupTypeRegistration(compositeName);
+
+            // Collect all possible composite types.
+            var selectedCompositeIndex = -1;
+            var currentIndex = 0;
+            foreach (var composite in InputBindingComposite.s_Composites.internedNames.Where(x =>
+                !InputBindingComposite.s_Composites.aliases.Contains(x)).OrderBy(x => x))
+            {
+                if (!string.IsNullOrEmpty(expectedControlLayout))
+                {
+                    var valueType = InputBindingComposite.GetValueType(composite);
+                    if (valueType != null &&
+                        !InputControlLayout.s_Layouts.ValueTypeIsAssignableFrom(
+                            new InternedString(expectedControlLayout), valueType))
+                        continue;
+                }
+
+                if (InputBindingComposite.s_Composites.LookupTypeRegistration(composite) == compositeType)
+                    selectedCompositeIndex = currentIndex;
+
+                yield return composite;
+                ++currentIndex;
+            }
+
+            // If the current composite type isn't a registered type, add it to the list as
+            // an extra option.
+            if (selectedCompositeIndex == -1)
+                yield return compositeName;
+        }
+
+        public static IEnumerable<string> GetCompositePartOptions(string bindingName, string compositeName)
+        {
+            var currentIndex = 0;
+            var selectedPartNameIndex = -1;
+            foreach (var partName in InputBindingComposite.GetPartNames(compositeName))
+            {
+                if (partName.Equals(bindingName, StringComparison.OrdinalIgnoreCase))
+                    selectedPartNameIndex = currentIndex;
+                yield return partName;
+                ++currentIndex;
+            }
+
+            // If currently selected part is not in list, add it as an option.
+            if (selectedPartNameIndex == -1)
+                yield return bindingName;
+        }
+
+        public static SerializedInputAction GetSelectedAction(InputActionsEditorState state)
+        {
+            return new SerializedInputAction(state.serializedObject
+                ?.FindProperty(nameof(InputActionAsset.m_ActionMaps))
+                ?.GetArrayElementAtIndex(state.selectedActionMapIndex)
+                ?.FindPropertyRelative(nameof(InputActionMap.m_Actions))
+                ?.GetArrayElementAtIndex(state.selectedActionIndex));
+        }
+
+        public static IEnumerable<string> BuildSortedControlList(InputActionType selectedActionType)
+        {
+            return BuildControlTypeList(selectedActionType)
+                .OrderBy(typeName => typeName, StringComparer.OrdinalIgnoreCase);
+        }
+
+        public static IEnumerable<string> BuildControlTypeList(InputActionType selectedActionType)
+        {
+            var allLayouts = InputSystem.s_Manager.m_Layouts;
+
+            yield return "Any";
+            foreach (var layoutName in allLayouts.layoutTypes.Keys)
+            {
+                if (EditorInputControlLayoutCache.TryGetLayout(layoutName).hideInUI)
+                    continue;
+
+                // If the action type is InputActionType.Value, skip button controls.
+                var type = allLayouts.layoutTypes[layoutName];
+                if (selectedActionType == InputActionType.Value && typeof(ButtonControl).IsAssignableFrom(type))
+                    continue;
+
+                ////TODO: skip aliases
+
+                if (typeof(InputControl).IsAssignableFrom(type) && !typeof(InputDevice).IsAssignableFrom(type))
+                    yield return layoutName;
+            }
+        }
+
+        public static IEnumerable<ParameterListView> GetInteractionsAsParameterListViews(InputActionsEditorState state)
+        {
+            var inputAction = GetSelectedAction(state);
+
+            Type expectedValueType = null;
+            if (!string.IsNullOrEmpty(inputAction.expectedControlType))
+                expectedValueType = EditorInputControlLayoutCache.GetValueType(inputAction.expectedControlType);
+
+            var interactions = string.Empty;
+            if (state.selectionType == SelectionType.Action)
+                interactions = inputAction.interactions;
+            else if (state.selectionType == SelectionType.Binding)
+                interactions = GetSelectedBinding(state).interactions;
+
+            return CreateParameterListViews(
+                interactions,
+                expectedValueType,
+                InputInteraction.s_Interactions.LookupTypeRegistration,
+                InputInteraction.GetValueType);
+        }
+
+        public static IEnumerable<ParameterListView> GetProcessorsAsParameterListViews(InputActionsEditorState state)
+        {
+            var processors = string.Empty;
+            Type expectedValueType = null;
+
+            var inputAction = GetSelectedAction(state);
+
+            if (!string.IsNullOrEmpty(inputAction.expectedControlType))
+                expectedValueType = EditorInputControlLayoutCache.GetValueType(inputAction.expectedControlType);
+
+            if (state.selectionType == SelectionType.Action)
+                processors = inputAction.processors;
+            else if (state.selectionType == SelectionType.Binding)
+                processors = GetSelectedBinding(state).processors;
+
+            return CreateParameterListViews(
+                processors,
+                expectedValueType,
+                InputProcessor.s_Processors.LookupTypeRegistration,
+                InputProcessor.GetValueTypeFromType);
+        }
+
+        private static IEnumerable<ParameterListView> CreateParameterListViews(string interactions, Type expectedValueType,
+            Func<string, Type> typeLookup, Func<Type, Type> getGenericArgumentType)
+        {
+            return NameAndParameters.ParseMultiple(interactions)
+                .Select(p => (interaction: p, rowType: typeLookup(p.name)))
+                .Select(t =>
+                {
+                    var(parameter, rowType) = t;
+
+                    var parameterListView = new ParameterListView();
+                    parameterListView.Initialize(rowType, parameter.parameters);
+
+                    parameterListView.name = ObjectNames.NicifyVariableName(parameter.name);
+
+                    if (rowType == null)
+                    {
+                        parameterListView.name += " (Obsolete)";
+                    }
+                    else if (expectedValueType != null)
+                    {
+                        var valueType = getGenericArgumentType(rowType);
+                        if (valueType != null && !expectedValueType.IsAssignableFrom(valueType))
+                            parameterListView.name += " (Incompatible Value Type)";
+                    }
+
+                    return parameterListView;
+                });
+        }
+    }
+}
+
+#endif
