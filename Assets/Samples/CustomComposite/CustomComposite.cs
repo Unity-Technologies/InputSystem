3c99e45f7efdcfb4ce1362fdf6d80c2adb5ef115
Rita Merkl <rita.merkl@unity3d.com>
2023-06-12 14:28:28 +0200

    pragma fix for example script

--- Assets/Samples/CustomComposite/CustomComposite.cs
+++ Assets/Samples/CustomComposite/CustomComposite.cs
@@ -177,9 +177,9 @@ public class CustomCompositeEditor : InputParameterEditor<CustomComposite>
         target.scaleFactor = EditorGUILayout.Slider(m_ScaleFactorLabel, currentValue, 0, 2);
     }
 
-#if UNITY_INPUT_SYSTEM_UI_TK_ASSET_EDITOR
     public override void OnDrawVisualElements(VisualElement root, Action onChangedCallback)
     {
+#if UNITY_INPUT_SYSTEM_UI_TK_ASSET_EDITOR
         var slider = new Slider(m_ScaleFactorLabel.text, 0, 2)
         {
             value = target.scaleFactor,
@@ -197,9 +197,8 @@ public class CustomCompositeEditor : InputParameterEditor<CustomComposite>
         });
 
         root.Add(slider);
-    }
-
 #endif
+    }
 
     private GUIContent m_ScaleFactorLabel = new GUIContent("Scale Factor");
 }
