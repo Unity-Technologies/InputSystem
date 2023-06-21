c8954e7708cafd30681db4cb9f26c2aba6ef10de
João Freire <joao.freire@unity3d.com>
2023-06-21 11:35:35 +0200

    Disable tests failing on 2022.3.3f1

--- Assets/Tests/InputSystem/Plugins/UITests.cs
+++ Assets/Tests/InputSystem/Plugins/UITests.cs
@@ -3886,6 +3886,7 @@ internal class UITests : CoreTestsFixture
 #if UNITY_TVOS
     [Ignore("Failing on tvOS https://jira.unity3d.com/browse/ISX-448")]
 #endif
+    [Ignore("Failing on 2023.3.3f1 https://jira.unity3d.com/browse/ISX-1462")]
     public IEnumerator UI_DisplayIndexMatchesDisplayWithTouchscreenOnOverlayCanvas()
     {
         // Setup the Test Scene
@@ -3987,6 +3988,7 @@ internal class UITests : CoreTestsFixture
     }
 
     [UnityTest]
+    [Ignore("Failing on 2023.3.3f1 https://jira.unity3d.com/browse/ISX-1462")]
     public IEnumerator UI_DisplayIndexMatchesDisplayWithMouseOnOverlayCanvas()
     {
         // Setup the Test Scene
