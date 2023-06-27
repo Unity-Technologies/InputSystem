ce5edd21fa271002cceb8edb1c3cc60ddbe76398
roger.liu <roger.liu@unity3d.com>
2023-06-27 15:36:36 -0700

    Fixed to 'Derived Bindings'

--- Packages/com.unity.inputsystem/InputSystem/Editor/AssetEditor/InputBindingPropertiesView.cs
+++ Packages/com.unity.inputsystem/InputSystem/Editor/AssetEditor/InputBindingPropertiesView.cs
@@ -150,7 +150,7 @@ namespace UnityEngine.InputSystem.Editor
                 // Otherwise, we will show either all controls that match the current binding (if control usages are used)
                 // or all controls in derived device layouts (if a no control usages are used).
                 EditorGUILayout.BeginVertical();
-                showMatchingLayouts = EditorGUILayout.Foldout(showMatchingLayouts, "Derived Controls");
+                showMatchingLayouts = EditorGUILayout.Foldout(showMatchingLayouts, "Derived Bindings");
 
                 if (showMatchingLayouts)
                 {
