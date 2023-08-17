2c19d07f51515ea3f3dbfa6225e682719ca2a52d
Ben Pitt <benp@unity3d.com>
2023-08-17 13:21:48 +0100

    Added stub doc to InputSystem.actions

--- Packages/com.unity.inputsystem/InputSystem/InputSystem.cs
+++ Packages/com.unity.inputsystem/InputSystem/InputSystem.cs
@@ -3014,6 +3014,10 @@ namespace UnityEngine.InputSystem
         /// The set of project-wide available input actions.
         /// </summary>
         private static InputActionAsset s_projectWideActions;
+
+        /// <summary>
+        /// The set of project-wide available input actions.
+        /// </summary>
         public static InputActionAsset actions
         {
             get
