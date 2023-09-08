4a774a6ed585c6d2f4dd423fca5369c27ace0c67
James McGill <james.mcgill@unity3d.com>
2023-09-08 22:10:15 +0200

    Change ProjectWide Actions sample to use only typesafe API

--- Assets/Samples/ProjectWideActionsTest/ProjectWideActionsTest.cs
+++ Assets/Samples/ProjectWideActionsTest/ProjectWideActionsTest.cs
@@ -1,50 +1,29 @@
 #if UNITY_INPUT_SYSTEM_PROJECT_WIDE_ACTIONS
 
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class NewBehaviourScript : MonoBehaviour
 {
     [SerializeField] public GameObject cube;
 
-    InputAction move;
-    InputAction look;
-    InputAction attack;
-    InputAction jump;
-    InputAction interact;
-    InputAction next;
-    InputAction previous;
-    InputAction sprint;
-    InputAction crouch;
-
     // Start is called before the first frame update
     void Start()
     {
-        // Project-Wide Actions
-        move = InputSystem.actions.FindAction("Player/Move");
-        look = InputSystem.actions.FindAction("Player/Look");
-        attack = InputSystem.actions.FindAction("Player/Attack");
-        jump = InputSystem.actions.FindAction("Player/Jump");
-        interact = InputSystem.actions.FindAction("Player/Interact");
-        next = InputSystem.actions.FindAction("Player/Next");
-        previous = InputSystem.actions.FindAction("Player/Previous");
-        sprint = InputSystem.actions.FindAction("Player/Sprint");
-        crouch = InputSystem.actions.FindAction("Player/Crouch");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (attack.WasPressedThisFrame())
+        // Project-Wide Actions via Source Generated type-safe API
+        if (InputActions.player.attack.wasPressedThisFrame)
         {
             cube.GetComponent<Renderer>().material.color = Color.red;
         }
-        else if (attack.WasReleasedThisFrame())
+        else if (InputActions.player.attack.wasReleasedThisFrame)
         {
             cube.GetComponent<Renderer>().material.color = Color.green;
         }
 
-        // Project-Wide Actions via Source Generated type-safe API
         if (InputActions.player.move.value.x < 0.0f)
         {
             cube.transform.Translate(new Vector3(-10 * Time.deltaTime, 0, 0));
