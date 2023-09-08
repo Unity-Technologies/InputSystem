482e160d428b6b9868b52ae0fb34d212731db509
James McGill <james.mcgill@unity3d.com>
2023-09-08 21:50:57 +0200

    NEW: Added Rosyln Source Generator for typesafe API

---
+++ Packages/com.unity.inputsystem/InputSystem/SourceGenerators/Source~/SourceGenerators/InputActionsSyntaxReceiver.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Unity.InputSystem.SourceGenerators;
+
+public class InputActionsSyntaxReceiver : ISyntaxReceiver
+{
+    public List<MemberAccessExpressionSyntax> InputActionsReferences { get; }
+
+    public InputActionsSyntaxReceiver()
+    {
+        InputActionsReferences = new List<MemberAccessExpressionSyntax>();
+    }
+
+    public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
+    {
+        if (syntaxNode is MemberAccessExpressionSyntax memberAccessSyntaxNode &&
+            memberAccessSyntaxNode.Expression is IdentifierNameSyntax identifierNameSyntax)
+        {
+            if (identifierNameSyntax.Identifier.ValueText == "InputActions")
+                InputActionsReferences.Add(memberAccessSyntaxNode);
+        }
+    }
+}
