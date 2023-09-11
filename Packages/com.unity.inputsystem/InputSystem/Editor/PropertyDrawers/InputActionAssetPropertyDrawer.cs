38153a2f2ebcaa785787d3c234f53bfc2a659648
João Freire <joao.freire@unity3d.com>
2023-09-11 16:56:16 +0200

    Cache properties and factor out option selection

--- Packages/com.unity.inputsystem/InputSystem/Editor/PropertyDrawers/InputActionAssetPropertyDrawer.cs
+++ Packages/com.unity.inputsystem/InputSystem/Editor/PropertyDrawers/InputActionAssetPropertyDrawer.cs
@@ -10,45 +10,49 @@ namespace UnityEngine.InputSystem.Editor
     [CustomPropertyDrawer(typeof(InputActionAssetProperty))]
     public class InputActionAssetPropertyDrawer : PropertyDrawer
     {
-        //TODO: refactor into enum
         static readonly string[] k_ActionsTypeOptions = new[] { "Project-Wide Actions", "Actions Asset" };
         [SerializeField] int m_SelectedActionsTypeIndex;
+        SerializedProperty m_IsAssetProjectWideActionsProperty;
+        SerializedProperty m_ActionsAssetProperty;
 
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
             EditorGUI.BeginProperty(position, label, property);
-
-            var isAssetProjectWideActions = property.FindPropertyRelative("m_IsAssetProjectWideActions").boolValue;
-            m_SelectedActionsTypeIndex = isAssetProjectWideActions ? 0 : 1;
+            m_IsAssetProjectWideActionsProperty = property.FindPropertyRelative("m_IsAssetProjectWideActions");
+            m_ActionsAssetProperty = property.FindPropertyRelative("m_ActionsAsset");
+            m_SelectedActionsTypeIndex = m_IsAssetProjectWideActionsProperty.boolValue ? 0 : 1;
 
             var selected = EditorGUILayout.Popup(new GUIContent("Actions"), m_SelectedActionsTypeIndex, k_ActionsTypeOptions);
 
             // Update property in case there's a change in the dropdown popup
             if (m_SelectedActionsTypeIndex != selected)
             {
-                if (selected == 0)
-                {
-                    //TODO: check if we can cache this
-                    property.FindPropertyRelative("m_IsAssetProjectWideActions").boolValue = true;
-                    property.FindPropertyRelative("m_ActionsAsset").objectReferenceValue = ProjectWideActionsAsset.GetOrCreate();
-                }
-                else
-                {
-                    property.FindPropertyRelative("m_IsAssetProjectWideActions").boolValue = false;
-                    // Reset the actions asset to null if the first time user selects the "Actions Asset" option
-                    property.FindPropertyRelative("m_ActionsAsset").objectReferenceValue = null;
-                }
-
-                m_SelectedActionsTypeIndex = selected;
+                UpdatePropertiesWithSelectedOption(selected);
             }
 
             // Show UI elements depending on the option selected
             if (m_SelectedActionsTypeIndex == 1)
             {
-                EditorGUILayout.PropertyField(property.FindPropertyRelative("m_ActionsAsset"), label, true);
+                EditorGUILayout.PropertyField(m_ActionsAssetProperty, label, true);
             }
 
             EditorGUI.EndProperty();
+        }
+
+        void UpdatePropertiesWithSelectedOption(int selected)
+        {
+            if (selected == 0)
+            {
+                m_IsAssetProjectWideActionsProperty.boolValue = true;
+                m_ActionsAssetProperty.objectReferenceValue = ProjectWideActionsAsset.GetOrCreate();
+            }
+            else
+            {
+                m_IsAssetProjectWideActionsProperty.boolValue = false;
+                // Reset the actions asset to null if the first time user selects the "Actions Asset" option
+                m_ActionsAssetProperty.objectReferenceValue = null;
+            }
+            m_SelectedActionsTypeIndex = selected;
         }
     }
 }
