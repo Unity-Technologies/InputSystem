9b045c31a98c9f2cde42853965fd825b8825e0bc
James McGill <james.mcgill@unity3d.com>
2023-09-14 14:03:12 +0200

    Add namespace to projectwide sample

--- Assets/Samples/ProjectWideActionsTest/ProjectWideActionsTest.cs
+++ Assets/Samples/ProjectWideActionsTest/ProjectWideActionsTest.cs
@@ -1,44 +1,45 @@
 #if UNITY_INPUT_SYSTEM_PROJECT_WIDE_ACTIONS
 
-using UnityEngine;
-
-public class NewBehaviourScript : MonoBehaviour
+namespace UnityEngine.InputSystem.Samples.ProjectWideActionsTest
 {
-    [SerializeField] public GameObject cube;
-
-    // Start is called before the first frame update
-    void Start()
+    public class ProjectWideActionsTest : MonoBehaviour
     {
-    }
+        [SerializeField] public GameObject cube;
 
-    // Update is called once per frame
-    void Update()
-    {
-        // Project-Wide Actions via Source Generated type-safe API
-        if (InputActions.player.attack.wasPressedThisFrame)
+        // Start is called before the first frame update
+        void Start()
         {
-            cube.GetComponent<Renderer>().material.color = Color.red;
-        }
-        else if (InputActions.player.attack.wasReleasedThisFrame)
-        {
-            cube.GetComponent<Renderer>().material.color = Color.green;
         }
 
-        if (InputActions.player.move.value.x < 0.0f)
+        // Update is called once per frame
+        void Update()
         {
-            cube.transform.Translate(new Vector3(-10 * Time.deltaTime, 0, 0));
-        }
-        else if (InputActions.player.move.value.x > 0.0f)
-        {
-            cube.transform.Translate(new Vector3(10 * Time.deltaTime, 0, 0));
-        }
-        if (InputActions.player.move.value.y < 0.0f)
-        {
-            cube.transform.Translate(new Vector3(0, -10 * Time.deltaTime, 0));
-        }
-        else if (InputActions.player.move.value.y > 0.0f)
-        {
-            cube.transform.Translate(new Vector3(0, 10 * Time.deltaTime, 0));
+            // Project-Wide Actions via Source Generated type-safe API
+            if (InputActions.player.attack.wasPressedThisFrame)
+            {
+                cube.GetComponent<Renderer>().material.color = Color.red;
+            }
+            else if (InputActions.player.attack.wasReleasedThisFrame)
+            {
+                cube.GetComponent<Renderer>().material.color = Color.green;
+            }
+            
+            if (InputActions.player.move.value.x < 0.0f)
+            {
+                cube.transform.Translate(new Vector3(-10 * Time.deltaTime, 0, 0));
+            }
+            else if (InputActions.player.move.value.x > 0.0f)
+            {
+                cube.transform.Translate(new Vector3(10 * Time.deltaTime, 0, 0));
+            }
+            if (InputActions.player.move.value.y < 0.0f)
+            {
+                cube.transform.Translate(new Vector3(0, -10 * Time.deltaTime, 0));
+            }
+            else if (InputActions.player.move.value.y > 0.0f)
+            {
+                cube.transform.Translate(new Vector3(0, 10 * Time.deltaTime, 0));
+            }
         }
     }
 }
