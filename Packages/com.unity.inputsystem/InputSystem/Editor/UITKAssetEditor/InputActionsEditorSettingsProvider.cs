e4ea44a55b372a6699f4126df9ef8654aab505ac
João Freire <joao.freire@unity3d.com>
2023-09-27 16:52:48 +0200

    WIP Auto-save when out of focus and asset editor disabled

--- Packages/com.unity.inputsystem/InputSystem/Editor/UITKAssetEditor/InputActionsEditorSettingsProvider.cs
+++ Packages/com.unity.inputsystem/InputSystem/Editor/UITKAssetEditor/InputActionsEditorSettingsProvider.cs
@@ -27,19 +27,38 @@ namespace UnityEngine.InputSystem.Editor
             BuildUI();
         }
 
+        public override void OnDeactivate()
+        {
+            // Debug.Log("Project settings Editor destroyed, trying to save asset");
+            TrySaveAsset();
+        }
+
         private void OnStateChanged(InputActionsEditorState newState)
         {
             // Project wide input actions always auto save - don't check the asset auto save status
             InputActionsEditorWindowUtils.SaveAsset(m_State.serializedObject);
         }
 
+        private void TrySaveAsset()
+        {
+            if (m_State.serializedObject != null)
+            {
+                InputActionsEditorWindowUtils.SaveAsset(m_State.serializedObject);
+            }
+        }
+
         private void BuildUI()
         {
             m_StateContainer = new StateContainer(m_RootVisualElement, m_State);
-            m_StateContainer.StateChanged += OnStateChanged;
+            //TODO: remove this
+            // m_StateContainer.StateChanged += OnStateChanged;
             m_RootVisualElement.styleSheets.Add(InputActionsEditorWindowUtils.theme);
             new InputActionsEditorView(m_RootVisualElement, m_StateContainer, null);
             m_StateContainer.Initialize();
+
+            var mainElement = m_RootVisualElement.Q("actions-split-view");
+            mainElement.focusable = true;
+            mainElement.RegisterCallback<FocusOutEvent>(OnFocusLost);
 
             // Hide the save / auto save buttons in the project wide input actions
             // Project wide input actions always auto save
@@ -48,6 +67,21 @@ namespace UnityEngine.InputSystem.Editor
             {
                 element.style.visibility = Visibility.Hidden;
                 element.style.display = DisplayStyle.None;
+            }
+        }
+
+        void OnFocusLost(FocusOutEvent evt)
+        {
+            // Workaround to find if the "main window" is out of focus.
+            // `relatedTarget` contains the element that gains focus, which seems to be null if we select
+            // elements outside of project settings Editor Window.
+            var element = (VisualElement)evt.relatedTarget;
+            if (element == null)
+            {
+                // Debug.Log("Focus lost, saving asset");
+                //TODO Validate if something else needs to happen
+                //TODO I think the state is not in a good condition if we just save here
+                TrySaveAsset();
             }
         }
 
