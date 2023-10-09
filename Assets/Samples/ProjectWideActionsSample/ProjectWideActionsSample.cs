c7b4a32e6d4a2bf2c240fabff605e2cf39f27261
James McGill <james.mcgill@unity3d.com>
2023-10-09 17:57:29 +0200

    Remove typesafe api

--- Assets/Samples/ProjectWideActionsSample/ProjectWideActionsSample.cs
+++ Assets/Samples/ProjectWideActionsSample/ProjectWideActionsSample.cs
@@ -1,42 +1,64 @@
 #if UNITY_INPUT_SYSTEM_PROJECT_WIDE_ACTIONS
 
+using UnityEngine;
+using UnityEngine.InputSystem;
+
 namespace UnityEngine.InputSystem.Samples.ProjectWideActionsSample
 {
     public class ProjectWideActionsSample : MonoBehaviour
     {
         [SerializeField] public GameObject cube;
 
+        InputAction move;
+        InputAction look;
+        InputAction attack;
+        InputAction jump;
+        InputAction interact;
+        InputAction next;
+        InputAction previous;
+        InputAction sprint;
+        InputAction crouch;
+
         // Start is called before the first frame update
         void Start()
         {
+            move = InputSystem.actions.FindAction("Player/Move");
+            look = InputSystem.actions.FindAction("Player/Look");
+            attack = InputSystem.actions.FindAction("Player/Attack");
+            jump = InputSystem.actions.FindAction("Player/Jump");
+            interact = InputSystem.actions.FindAction("Player/Interact");
+            next = InputSystem.actions.FindAction("Player/Next");
+            previous = InputSystem.actions.FindAction("Player/Previous");
+            sprint = InputSystem.actions.FindAction("Player/Sprint");
+            crouch = InputSystem.actions.FindAction("Player/Crouch");
         }
 
         // Update is called once per frame
         void Update()
         {
-            // Project-Wide Actions via Source Generated type-safe API
-            if (InputActions.player.attack.wasPressedThisFrame)
+            if (attack.WasPressedThisFrame())
             {
                 cube.GetComponent<Renderer>().material.color = Color.red;
             }
-            else if (InputActions.player.attack.wasReleasedThisFrame)
+            else if (attack.WasReleasedThisFrame())
             {
                 cube.GetComponent<Renderer>().material.color = Color.green;
             }
 
-            if (InputActions.player.move.value.x < 0.0f)
+            var moveVal = move.ReadValue<Vector2>();
+            if (moveVal.x < 0.0f)
             {
                 cube.transform.Translate(new Vector3(-10 * Time.deltaTime, 0, 0));
             }
-            else if (InputActions.player.move.value.x > 0.0f)
+            else if (moveVal.x > 0.0f)
             {
                 cube.transform.Translate(new Vector3(10 * Time.deltaTime, 0, 0));
             }
-            if (InputActions.player.move.value.y < 0.0f)
+            if (moveVal.y < 0.0f)
             {
                 cube.transform.Translate(new Vector3(0, -10 * Time.deltaTime, 0));
             }
-            else if (InputActions.player.move.value.y > 0.0f)
+            else if (moveVal.y > 0.0f)
             {
                 cube.transform.Translate(new Vector3(0, 10 * Time.deltaTime, 0));
             }
