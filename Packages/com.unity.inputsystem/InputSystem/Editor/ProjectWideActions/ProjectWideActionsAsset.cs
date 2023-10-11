4afdc28f2564480629b9e412d82890c9167cc3c8
James McGill <james.mcgill@unity3d.com>
2023-10-11 16:12:04 +0200

    Revert "Fix startup issues with Project Actions after changing to be AssetDatabase driven"
    This reverts commit 1f8ede24737245dd6d6b2e5c4c5cb5f13d04f474.

--- Packages/com.unity.inputsystem/InputSystem/Editor/ProjectWideActions/ProjectWideActionsAsset.cs
+++ Packages/com.unity.inputsystem/InputSystem/Editor/ProjectWideActions/ProjectWideActionsAsset.cs
@@ -14,7 +14,7 @@ namespace UnityEngine.InputSystem.Editor
         internal const string kInputManagerAssetPath = "ProjectSettings/InputManager.asset";
 
         internal const string kDefaultAssetDirectory = "Assets";
-        internal const string kDefaultAssetFilename = InputSystem.kProjectWideActionsAssetName + ".inputactions"; // NB. Importer will change the asset name to the filename, so they must match.
+        internal const string kDefaultAssetFilename = "ProjectActions.inputactions";
         internal const string kDefaultAssetPath = kDefaultAssetDirectory + "/" + kDefaultAssetFilename;
 
         static InputActionAsset s_TestAsset = null;
@@ -77,52 +77,29 @@ namespace UnityEngine.InputSystem.Editor
 
         internal static InputActionAsset LoadFromPath(string assetPath)
         {
+            string text;
             try
             {
-                AssetDatabase.ImportAsset(assetPath, ImportAssetOptions.ForceSynchronousImport);
-                var objects = AssetDatabase.LoadAllAssetsAtPath(assetPath);
-                if (objects == null)
-                    throw new FileNotFoundException();
-
-                // This can happen when opening a project and the AssetDatabase is not ready yet.
-                // Loading without the AssetDatabase as a temporary stop-gap to prop up `InputSystem.actions` until then.
-                if (objects.Length == 0)
-                    return LoadDirectFromPath(assetPath);
-
-                var inputActionsAsset = objects.FirstOrDefault(o => o is InputActionAsset) as InputActionAsset;
-                inputActionsAsset.name = InputSystem.kProjectWideActionsAssetName;
-                return inputActionsAsset;
+                text = File.ReadAllText(assetPath);
             }
-            catch (Exception exception)
+            catch
             {
-                Debug.LogError($"InputSystem could not load actions asset: '{assetPath}' ({exception})");
                 return null;
             }
-        }
 
-        // Load without using the AssetDatabase.
-        // There will be no GUID available for the returned asset, so you cannot reference it in EditorBuildSettings.
-        internal static InputActionAsset LoadDirectFromPath(string assetPath)
-        {
             var asset = ScriptableObject.CreateInstance<InputActionAsset>();
             asset.name = InputSystem.kProjectWideActionsAssetName;
             try
             {
-                string text = File.ReadAllText(assetPath);
                 asset.LoadFromJson(text);
-                return asset;
-            }
-            catch (FileNotFoundException exception)
-            {
-                Debug.LogError($"InputSystem could not load actions asset: '{assetPath}' ({exception})");
             }
             catch (Exception exception)
             {
                 Debug.LogError($"InputSystem could not parse input actions in JSON format from '{assetPath}' ({exception})");
+                Object.DestroyImmediate(asset);
+                return null;
             }
-
-            Object.DestroyImmediate(asset);
-            return null;
+            return asset;
         }
 
         internal static InputActionAsset LoadFromInputManagerSettings()
@@ -153,31 +130,19 @@ namespace UnityEngine.InputSystem.Editor
                 return actionsAsset;
             }
 
-            InputActionAsset asset = TryLoadingFromDefaultLocation();
-
             // v1.8.0-pre1 stored the Actions in ProjectSettings/InputManager.asset.
             // Check if we have a project that was saved on that version and migrate it
             // to save in the default location used from the pre2 release.
-            if (asset == null)
-            {
-                asset = LoadFromInputManagerSettings();
-                if (asset != null)
-                    asset = CreateAndLoadNewAsset(content: asset);
-            }
+            InputActionAsset asset = LoadFromInputManagerSettings();
+            if (asset != null)
+                asset = CreateAndLoadNewAsset(asset);
 
             // Create a new one if we couldn't find any existing one to load.
             if (asset == null)
                 asset = CreateAndLoadNewAsset();
 
-            if (asset == null)
-            {
-                Debug.LogError("Inputsystem could not create a Project-Wide InputActionAsset");
-                return null;
-            }
-
             // Mark it as being _the_ project-wide actions asset.
-            // If AssetDatabase was busy, we cannot do this here and will need to try again later.
-            if (!string.IsNullOrEmpty(AssetDatabase.GetAssetPath(asset)))
+            if (asset != null && !string.IsNullOrEmpty(AssetDatabase.GetAssetPath(asset)))
             {
                 EditorBuildSettings.AddConfigObject(
                     InputActionsEditorSettingsProvider.kProjectActionsConfigKey,
@@ -188,45 +153,30 @@ namespace UnityEngine.InputSystem.Editor
             return asset;
         }
 
-        static InputActionAsset TryLoadingFromDefaultLocation()
-        {
-            try
-            {
-                if (File.Exists(kDefaultAssetPath))
-                    return LoadFromPath(kDefaultAssetPath);
-            }
-            catch {}
-            return null;
-        }
-
         /// <summary>
         /// Automatically creates the file ProjectActions.inputactions file in the user's Asset directory and loads it.
         /// </summary>
-        /// <param name="content">Content to populate the asset file with. If null, the project-wide actions template content will be used.</param>
-        static InputActionAsset CreateAndLoadNewAsset(InputActionAsset content = null)
+        /// <param name="asset">Content to populate asset with. If null, the project-wide actions template content will be used.</param>
+        static InputActionAsset CreateAndLoadNewAsset(InputActionAsset assetContent = null)
         {
             try
             {
+                // Never overwrite a user's file
+                if (File.Exists(kDefaultAssetPath))
+                    throw new Exception("File already exists.");
+
                 if (!Directory.Exists(kDefaultAssetDirectory))
                     Directory.CreateDirectory(kDefaultAssetDirectory);
 
-                if (content == null)
+                if (assetContent == null)
                 {
-                    // Read the template actions actions and regenerate the guids.
-                    var text = File.ReadAllText(kTemplateAssetPath);
-                    content = InputActionAsset.FromJson(text);
-                    foreach (var map in content.actionMaps)
-                    {
-                        map.m_Id = Guid.NewGuid().ToString();
-                        foreach (var action in map.actions)
-                            action.m_Id = Guid.NewGuid().ToString();
-                    }
+                    File.Copy(kTemplateAssetPath, kDefaultAssetPath);
                 }
-
-                // Write the file to disk
-                content.name = InputSystem.kProjectWideActionsAssetName;
-                var assetJson = content.ToJson();
-                File.WriteAllText(kDefaultAssetPath, assetJson);
+                else
+                {
+                    var assetJson = assetContent.ToJson();
+                    File.WriteAllText(kDefaultAssetPath, assetJson);
+                }
 
                 return LoadFromPath(kDefaultAssetPath);
             }
