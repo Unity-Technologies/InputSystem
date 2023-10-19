90ce742d3bf043d6a80144c6c9a5b395c1608c25
Chris Massie <chris.massie@unity3d.com>
2023-10-19 10:59:52 -0700

    Fixed API_MinorVersionsHaveNoBreakingChanges by reverting renames
    The names won't match the UnityEngine.XR and IUnityXRInput.h names, but this is needed to avoid a breaking change. Renamed the optimal size field to more closely match the naming of maxBufferSize for consistency within Input System.

--- Packages/com.unity.inputsystem/InputSystem/Plugins/XR/Haptics/GetHapticCapabilitiesCommand.cs
+++ Packages/com.unity.inputsystem/InputSystem/Plugins/XR/Haptics/GetHapticCapabilitiesCommand.cs
@@ -1,6 +1,5 @@
 // ENABLE_VR is not defined on Game Core but the assembly is available with limited features when the XR module is enabled.
 #if UNITY_INPUT_SYSTEM_ENABLE_XR && (ENABLE_VR || UNITY_GAMECORE) || PACKAGE_DOCS_GENERATION
-using System;
 using System.Runtime.InteropServices;
 using UnityEngine.InputSystem.LowLevel;
 using UnityEngine.InputSystem.Utilities;
@@ -10,7 +9,7 @@ namespace UnityEngine.InputSystem.XR.Haptics
     /// <summary>
     /// Describes the haptic capabilities of a specific device.
     /// </summary>
-    public readonly struct HapticCapabilities
+    public struct HapticCapabilities
     {
         /// <summary>
         /// Initializes and returns an instance of <see cref="HapticCapabilities"/>.
@@ -18,17 +17,17 @@ namespace UnityEngine.InputSystem.XR.Haptics
         /// <param name="numChannels">The number of haptic channels available on this device.</param>
         /// <param name="supportsImpulse">This device supports sending a haptic impulse.</param>
         /// <param name="supportsBuffer">This device supports sending a haptic buffer.</param>
-        /// <param name="bufferFrequencyHz">The buffer frequency the device operates at in Hertz.</param>
-        /// <param name="bufferMaxSize">The max amount of buffer data that can be stored by the device.</param>
-        /// <param name="bufferOptimalSize">The optimal size of a device's buffer, taking into account frequency and latency.</param>
-        public HapticCapabilities(uint numChannels, bool supportsImpulse, bool supportsBuffer, uint bufferFrequencyHz, uint bufferMaxSize, uint bufferOptimalSize)
+        /// <param name="frequencyHz">The buffer frequency the device operates at in Hertz.</param>
+        /// <param name="maxBufferSize">The max amount of buffer data that can be stored by the device.</param>
+        /// <param name="optimalBufferSize">The optimal size of a device's buffer, taking into account frequency and latency.</param>
+        public HapticCapabilities(uint numChannels, bool supportsImpulse, bool supportsBuffer, uint frequencyHz, uint maxBufferSize, uint optimalBufferSize)
         {
             this.numChannels = numChannels;
             this.supportsImpulse = supportsImpulse;
             this.supportsBuffer = supportsBuffer;
-            this.bufferFrequencyHz = bufferFrequencyHz;
-            this.bufferMaxSize = bufferMaxSize;
-            this.bufferOptimalSize = bufferOptimalSize;
+            this.frequencyHz = frequencyHz;
+            this.maxBufferSize = maxBufferSize;
+            this.optimalBufferSize = optimalBufferSize;
         }
 
         /// <summary>
@@ -38,7 +37,6 @@ namespace UnityEngine.InputSystem.XR.Haptics
         /// <param name="numChannels">The number of haptic channels available on this device.</param>
         /// <param name="frequencyHz">The buffer frequency the device operates at in Hertz.</param>
         /// <param name="maxBufferSize">The max amount of buffer data that can be stored by the device.</param>
-        [Obsolete("Deprecated. Use other constructor with all properties instead.")]
         public HapticCapabilities(uint numChannels, uint frequencyHz, uint maxBufferSize)
             : this(numChannels, false, false, frequencyHz, maxBufferSize, 0U)
         {
@@ -67,29 +65,17 @@ namespace UnityEngine.InputSystem.XR.Haptics
         /// <remarks>
         /// This value is greater than 0 if <see cref="supportsBuffer"/> is <see langword="true"/>, and 0 otherwise.
         /// </remarks>
-        public uint bufferFrequencyHz { get; }
+        public uint frequencyHz { get; }
 
         /// <summary>
         /// The max amount of buffer data that can be stored by the device.
         /// </summary>
-        public uint bufferMaxSize { get; }
+        public uint maxBufferSize { get; }
 
         /// <summary>
         /// The optimal size of a device's buffer, taking into account frequency and latency.
         /// </summary>
-        public uint bufferOptimalSize { get; }
-
-        /// <summary>
-        /// Deprecated. Use <see cref="bufferFrequencyHz"/> instead.
-        /// </summary>
-        [Obsolete("frequencyHz has been deprecated. Use bufferFrequencyHz instead. (UnityUpgradable) -> bufferFrequencyHz")]
-        public uint frequencyHz => bufferFrequencyHz;
-
-        /// <summary>
-        /// Deprecated. Use <see cref="bufferMaxSize"/> instead.
-        /// </summary>
-        [Obsolete("maxBufferSize has been deprecated. Use bufferMaxSize instead. (UnityUpgradable) -> bufferMaxSize")]
-        public uint maxBufferSize => bufferMaxSize;
+        public uint optimalBufferSize { get; }
     }
 
     [StructLayout(LayoutKind.Explicit, Size = kSize)]
@@ -115,35 +101,15 @@ namespace UnityEngine.InputSystem.XR.Haptics
         public bool supportsBuffer;
 
         [FieldOffset(InputDeviceCommand.kBaseCommandSize + 8)]
-        public uint bufferFrequencyHz;
+        public uint frequencyHz;
 
         [FieldOffset(InputDeviceCommand.kBaseCommandSize + 12)]
-        public uint bufferMaxSize;
+        public uint maxBufferSize;
 
         [FieldOffset(InputDeviceCommand.kBaseCommandSize + 16)]
-        public uint bufferOptimalSize;
+        public uint optimalBufferSize;
 
-        /// <summary>
-        /// Deprecated. Use <see cref="bufferFrequencyHz"/> instead.
-        /// </summary>
-        [Obsolete("frequencyHz has been deprecated. Use bufferFrequencyHz instead.")]
-        public uint frequencyHz
-        {
-            get => bufferFrequencyHz;
-            set => bufferFrequencyHz = value;
-        }
-
-        /// <summary>
-        /// Deprecated. Use <see cref="bufferMaxSize"/> instead.
-        /// </summary>
-        [Obsolete("maxBufferSize has been deprecated. Use bufferMaxSize instead.")]
-        public uint maxBufferSize
-        {
-            get => bufferMaxSize;
-            set => bufferMaxSize = value;
-        }
-
-        public HapticCapabilities capabilities => new HapticCapabilities(numChannels, supportsImpulse, supportsBuffer, bufferFrequencyHz, bufferMaxSize, bufferOptimalSize);
+        public HapticCapabilities capabilities => new HapticCapabilities(numChannels, supportsImpulse, supportsBuffer, frequencyHz, maxBufferSize, optimalBufferSize);
 
         public static GetHapticCapabilitiesCommand Create()
         {
