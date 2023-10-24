698dc8c9f4fd24a9e62084c31a1d7aed45488e0b
João Freire <joao.freire@unity3d.com>
2023-10-24 12:34:21 +0200

    Add InputActionReference advanced picker
    This new picker allows us to search InputActionReferences from the project wide actions asset. Since this is asset is in ProjectSettings, a normal object picker doesn't work.

---
+++ Packages/com.unity.inputsystem/InputSystem/Editor/AssetImporter/InputActionAssetIconLoader.cs
@@ -0,0 +1,35 @@
+#if UNITY_EDITOR
+using UnityEditor;
+
+namespace UnityEngine.InputSystem.Editor
+{
+    /// <summary>
+    /// Provides access to icons associated with <code>InputActionAsset</code>.
+    /// </summary>
+    internal static class InputActionAssetIconLoader
+    {
+        private const string kActionIcon = "Packages/com.unity.inputsystem/InputSystem/Editor/Icons/InputAction.png";
+        private const string kAssetIcon = "Packages/com.unity.inputsystem/InputSystem/Editor/Icons/InputActionAsset.png";
+
+        /// <summary>
+        /// Attempts to load the icon associated with an <code>InputActionAsset</code> (.inputactions) asset.
+        /// </summary>
+        /// <returns>Icon resource reference or <code>null</code> if the resource could not be loaded.</returns>
+        public static Texture2D LoadAssetIcon()
+        {
+            return (Texture2D)EditorGUIUtility.Load(kAssetIcon);
+        }
+
+        /// <summary>
+        /// Attempts to load the icon associated with an <code>InputActionReference</code> sub-asset of an
+        /// <code>InputActionAsset</code> (.inputactions) asset.
+        /// </summary>
+        /// <returns>Icon resource reference or <code>null</code> if the resource could not be loaded.</returns>
+        public static Texture2D LoadActionIcon()
+        {
+            return (Texture2D)EditorGUIUtility.Load(kActionIcon);
+        }
+    }
+}
+
+#endif // #if UNITY_EDITOR
