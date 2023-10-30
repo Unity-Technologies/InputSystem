4e16a510589a771a6244c049b9eaecd2d9a8063a
João Freire <joao.freire@unity3d.com>
2023-10-30 18:53:17 +0100

    Test updating references when asset actions are changed

--- Assets/Tests/InputSystem/CoreTests_ProjectWideActions.cs
+++ Assets/Tests/InputSystem/CoreTests_ProjectWideActions.cs
@@ -1,7 +1,9 @@
 #if UNITY_INPUT_SYSTEM_PROJECT_WIDE_ACTIONS
 
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using NUnit.Framework;
 using UnityEditor;
 using UnityEngine;
@@ -209,6 +211,49 @@ internal partial class CoreTests
         Assert.That(InputSystem.actions.actionMaps[0].actions[0].name, Is.EqualTo("replacedAction1"));
         Assert.That(InputSystem.actions.actionMaps[1].actions.Count, Is.EqualTo(1));
         Assert.That(InputSystem.actions.actionMaps[1].actions[0].name, Is.EqualTo("replacedAction4"));
+    }
+
+    [Test]
+    [Category(TestCategory)]
+    public void ProjectWideActions_CanUpdateInputActionReferencesFromAsset()
+    {
+        List<InputActionReference> UpdateAndReturnNewReferences(InputActionAsset inputActionAsset)
+        {
+            ProjectWideActionsAsset.UpdateInputActionReferences();
+            var inputActionReferences = InputActionImporter.LoadInputActionReferencesFromAsset(inputActionAsset).ToList();
+            return inputActionReferences;
+        }
+
+        var asset = ProjectWideActionsAsset.GetOrCreate();
+        asset.Disable();
+
+        var initialReferences = InputActionImporter.LoadInputActionReferencesFromAsset(asset).ToList();
+
+        Assert.That(initialReferences.Find(r => r.action.name == "InitialActionOne"), Is.Not.Null);
+        Assert.That(initialReferences.Find(r => r.action.name == "InitialActionTwo"), Is.Not.Null);
+
+        // Remove action
+        asset.m_ActionMaps[0].m_Actions[0].RemoveAction();
+
+        var updatedReferences = UpdateAndReturnNewReferences(asset);
+
+        Assert.That(updatedReferences.Find(r => r.action.name == "InitialActionOne"), Is.Null);
+        Assert.That(updatedReferences.Find(r => r.action.name == "InitialActionTwo"), Is.Not.Null);
+
+        // Rename action
+        asset.m_ActionMaps[0].m_Actions[0].Rename("InitialActionTwoRenamed");
+
+        updatedReferences = UpdateAndReturnNewReferences(asset);
+
+        Assert.That(updatedReferences.Find(r => r.action.name == "InitialActionTwoRenamed"), Is.Not.Null);
+
+        // Adding a new action
+        asset.m_ActionMaps[0].AddAction("AddedNewAction", InputActionType.Button);
+
+        updatedReferences = UpdateAndReturnNewReferences(asset);
+
+        Assert.That(updatedReferences.Find(r => r.action.name == "InitialActionTwoRenamed"), Is.Not.Null);
+        Assert.That(updatedReferences.Find(r => r.action.name == "AddedNewAction"), Is.Not.Null);
     }
 }
 
