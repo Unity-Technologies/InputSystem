869ec4145a18940157612ead896cd897601c90bf
Tim Keosababian <36088732+timkeo@users.noreply.github.com>
2024-01-22 16:16:07 -0800

    Implement basic support for FEPM (ISX-1842)
    - Rework InputSystem's Init functions to handle optional domain reloads
    - Bypass InitializeInPlayer() "Reset" and "Restore" functions if DRs disabled
    
    Only the first step to getting this working, but enables FEPM support for golden path scenarios.

--- Packages/com.unity.inputsystem/InputSystem/InputSystem.cs
+++ Packages/com.unity.inputsystem/InputSystem/InputSystem.cs
@@ -80,9 +80,19 @@ namespace UnityEngine.InputSystem
 #if UNITY_EDITOR
     [InitializeOnLoad]
 #endif
-
     public static partial class InputSystem
     {
+        static InputSystem()
+        {
+            GlobalInitialize(true);
+        }
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        private static void RuntimeInitialize()
+        {
+            GlobalInitialize(false);
+        }
+
         #region Layouts
 
         /// <summary>
@@ -3390,7 +3400,7 @@ namespace UnityEngine.InputSystem
             s_RemoteConnection.Subscribe(s_Remote); // Feed messages from editor into players.
         }
 
-        #if !UNITY_EDITOR
+#if !UNITY_EDITOR
         private static bool ShouldEnableRemoting()
         {
 #if UNITY_INCLUDE_TESTS
@@ -3403,36 +3413,48 @@ namespace UnityEngine.InputSystem
             return true;
         }
 
-        #endif
+#endif
 #endif // DEVELOPMENT_BUILD || UNITY_EDITOR
 
         // The rest here is internal stuff to manage singletons, survive domain reloads,
         // and to support the reset ability for tests.
-        static InputSystem()
+
+        private static bool IsDomainReloadDisabledForPlayMode()
         {
-            #if UNITY_EDITOR
-            InitializeInEditor();
-            #else
-            InitializeInPlayer();
-            #endif
+#if UNITY_EDITOR && !ENABLE_CORECLR
+            if (!EditorSettings.enterPlayModeOptionsEnabled || (EditorSettings.enterPlayModeOptions & EnterPlayModeOptions.DisableDomainReload) == 0)
+                return false;
+#endif
+            return true;
         }
 
-        ////FIXME: Unity is not calling this method if it's inside an #if block that is not
-        ////       visible to the editor; that shouldn't be the case
-        [RuntimeInitializeOnLoadMethod(loadType: RuntimeInitializeLoadType.SubsystemRegistration)]
-        private static void RunInitializeInPlayer()
+        private static void GlobalInitialize(bool calledFromCtor)
         {
-            // We're using this method just to make sure the class constructor is called
-            // so we don't need any code in here. When the engine calls this method, the
-            // class constructor will be run if it hasn't been run already.
+            // This method is called twice: once from the static ctor and again from RuntimeInitialize().
+            // We handle the calls differently for the Editor and Player.
 
-            // IL2CPP has a bug that causes the class constructor to not be run when
-            // the RuntimeInitializeOnLoadMethod is invoked. So we need an explicit check
-            // here until that is fixed (case 1014293).
-            #if !UNITY_EDITOR
-            if (s_Manager == null)
+#if UNITY_EDITOR
+            // If Domain Reloads are enabled, InputSystem is initialized via the ctor and we can ignore
+            // the second call from "Runtime", otherwise (DRs are disabled) the ctor isn't fired, so we
+            // must initialize via the Runtime call.
+
+            if (calledFromCtor || IsDomainReloadDisabledForPlayMode())
+            {
+                InitializeInEditor(calledFromCtor);
+            }
+#else
+            // In the Player, simply initialize InputSystem from the ctor and then execute the initial update
+            // from the second call. This saves us from needing another RuntimeInitializeOnLoad attribute.
+
+            if (calledFromCtor)
+            {
                 InitializeInPlayer();
-            #endif
+            }
+            else
+            {
+                RunInitialUpdate();
+            }
+#endif
         }
 
         // Initialization is triggered by accessing InputSystem. Some parts (like InputActions)
@@ -3445,39 +3467,47 @@ namespace UnityEngine.InputSystem
 #if UNITY_EDITOR
         internal static InputSystemObject s_SystemObject;
 
-        internal static void InitializeInEditor(IInputRuntime runtime = null)
+        internal static void InitializeInEditor(bool calledFromCtor, IInputRuntime runtime = null)
         {
             Profiler.BeginSample("InputSystem.InitializeInEditor");
-            Reset(runtime: runtime);
+
+            // This is only necessary after a Domain Reload but otherwise can be skipped.
+            bool globalReset = calledFromCtor || !IsDomainReloadDisabledForPlayMode();
+
+            if (globalReset)
+                Reset(runtime: runtime);
 
             var existingSystemObjects = Resources.FindObjectsOfTypeAll<InputSystemObject>();
             if (existingSystemObjects != null && existingSystemObjects.Length > 0)
             {
-                ////FIXME: does not preserve action map state
+                if (globalReset)
+                {
+                    ////FIXME: does not preserve action map state
 
-                // We're coming back out of a domain reload. We're restoring part of the
-                // InputManager state here but we're still waiting from layout registrations
-                // that happen during domain initialization.
+                    // If we're coming back out of a domain reload. We're restoring part of the
+                    // InputManager state here but we're still waiting from layout registrations
+                    // that happen during domain initialization.
 
-                s_SystemObject = existingSystemObjects[0];
-                s_Manager.RestoreStateWithoutDevices(s_SystemObject.systemState.managerState);
-                InputDebuggerWindow.ReviveAfterDomainReload();
+                    s_SystemObject = existingSystemObjects[0];
+                    s_Manager.RestoreStateWithoutDevices(s_SystemObject.systemState.managerState);
+                    InputDebuggerWindow.ReviveAfterDomainReload();
 
-                // Restore remoting state.
-                s_RemoteConnection = s_SystemObject.systemState.remoteConnection;
-                SetUpRemoting();
-                s_Remote.RestoreState(s_SystemObject.systemState.remotingState, s_Manager);
+                    // Restore remoting state.
+                    s_RemoteConnection = s_SystemObject.systemState.remoteConnection;
+                    SetUpRemoting();
+                    s_Remote.RestoreState(s_SystemObject.systemState.remotingState, s_Manager);
 
-                // Get manager to restore devices on first input update. By that time we
-                // should have all (possibly updated) layout information in place.
-                s_Manager.m_SavedDeviceStates = s_SystemObject.systemState.managerState.devices;
-                s_Manager.m_SavedAvailableDevices = s_SystemObject.systemState.managerState.availableDevices;
+                    // Get manager to restore devices on first input update. By that time we
+                    // should have all (possibly updated) layout information in place.
+                    s_Manager.m_SavedDeviceStates = s_SystemObject.systemState.managerState.devices;
+                    s_Manager.m_SavedAvailableDevices = s_SystemObject.systemState.managerState.availableDevices;
 
-                // Restore editor settings.
-                InputEditorUserSettings.s_Settings = s_SystemObject.systemState.userSettings;
+                    // Restore editor settings.
+                    InputEditorUserSettings.s_Settings = s_SystemObject.systemState.userSettings;
 
-                // Get rid of saved state.
-                s_SystemObject.systemState = new State();
+                    // Get rid of saved state.
+                    s_SystemObject.systemState = new State();
+                }
             }
             else
             {
@@ -3661,7 +3691,6 @@ namespace UnityEngine.InputSystem
 
 #endif // UNITY_EDITOR
 
-        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
         private static void RunInitialUpdate()
         {
             // Request an initial Update so that user methods such as Start and Awake
