df9e0bfa6b7c38561fee5696993e95fa89af3ffc
Alex Tyrer <alextyrer@unity3d.com>
2024-03-09 14:55:38 +0000

    [Input] Testing revert of a91a900 (which  removed DisableProjectWideActions from Editor_LeavingPlayMode_DestroysAllActionStates)

--- Assets/Tests/InputSystem/CoreTests_Editor.cs
+++ Assets/Tests/InputSystem/CoreTests_Editor.cs
@@ -2890,10 +2890,27 @@ partial class CoreTests
         Assert.That(action.ReadValue<float>(), Is.EqualTo(0.75f).Within(0.00001f));
     }
 
+    private static void DisableProjectWideActions() // AlexT - testing
+    {
+#if UNITY_INPUT_SYSTEM_PROJECT_WIDE_ACTIONS
+        // If the system has project-wide input actions they will also trigger enable/disable via
+        // play mode change triggers above. Hence we adjust extra variable to compensate of
+        // state allocated by project-wide actions.
+        if (InputSystem.actions)
+        {
+            Assert.That(InputActionState.s_GlobalState.globalList.length, Is.EqualTo(1));
+            InputSystem.actions.Disable();
+            InputActionState.DestroyAllActionMapStates();
+        }
+#endif
+    }
+
     [Test]
     [Category("Editor")]
     public void Editor_LeavingPlayMode_DestroysAllActionStates()
     {
+        DisableProjectWideActions();    // AlexT - testing
+
         // Initial state
         Assert.That(InputActionState.s_GlobalState.globalList.length, Is.EqualTo(0));
 
@@ -2902,6 +2919,8 @@ partial class CoreTests
         // Enter play mode.
         InputSystem.OnPlayModeChange(PlayModeStateChange.ExitingEditMode);
         InputSystem.OnPlayModeChange(PlayModeStateChange.EnteredPlayMode);
+
+        DisableProjectWideActions();    // AlexT - testing
 
         var action = new InputAction(binding: "<Gamepad>/buttonSouth");
         action.Enable();
