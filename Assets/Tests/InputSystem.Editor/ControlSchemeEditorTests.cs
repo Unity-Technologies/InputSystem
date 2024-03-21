a518d23f28a9f34271e0e7cc5956eb286e2adc3f
Stefan Schubert <stefan@unity3d.com>
2024-03-21 12:37:47 +0100

    formatting

--- Assets/Tests/InputSystem.Editor/ControlSchemeEditorTests.cs
+++ Assets/Tests/InputSystem.Editor/ControlSchemeEditorTests.cs
@@ -260,7 +260,7 @@ public class ControlSchemesEditorTests
     }
 
     [Test]
-    [Category("AssetEditor")]    
+    [Category("AssetEditor")]
     public void DeleteControlSchemeCommand_DeletesSelectedControlScheme()
     {
         var asset = TestData.inputActionAsset.WithControlScheme(TestData.controlScheme.WithOptionalDevice()).Generate();
@@ -281,7 +281,7 @@ public class ControlSchemesEditorTests
     [TestCase(3, 1, 1, "Test2")]
     [TestCase(3, 2, 1, "Test1")]
     [TestCase(1, 0, -1, null)]
-    [Category("AssetEditor")]  
+    [Category("AssetEditor")]
     public void DeleteControlSchemeCommand_SelectsAnotherControlSchemeAfterDelete(
         int controlSchemeCount,
         int selectedControlSchemeIndex,
