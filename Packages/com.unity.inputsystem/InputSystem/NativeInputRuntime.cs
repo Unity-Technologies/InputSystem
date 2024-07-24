5fd4595dd1e2f9b9ee0953384b50b37e5506acd9
João Freire <joao.freire@unity3d.com>
2024-07-24 10:53:07 +0200

    Improvement based on PR review

--- Packages/com.unity.inputsystem/InputSystem/NativeInputRuntime.cs
+++ Packages/com.unity.inputsystem/InputSystem/NativeInputRuntime.cs
@@ -393,22 +393,24 @@ namespace UnityEngine.InputSystem.LowLevel
 
         public void SendAnalytic(InputAnalytics.IInputAnalytic analytic)
         {
-        #if (UNITY_EDITOR && ENABLE_CLOUD_SERVICES_ANALYTICS)
-            #if (UNITY_2023_2_OR_NEWER)
+        #if ENABLE_CLOUD_SERVICES_ANALYTICS
+            #if (UNITY_EDITOR)
+                #if (UNITY_2023_2_OR_NEWER)
             EditorAnalytics.SendAnalytic(analytic);
-            #else
+                #else
             var info = analytic.info;
             EditorAnalytics.RegisterEventWithLimit(info.Name, info.MaxEventsPerHour, info.MaxNumberOfElements, InputAnalytics.kVendorKey);
             EditorAnalytics.SendEventWithLimit(info.Name, analytic);
-            #endif // UNITY_2023_2_OR_NEWER
-        #elif (!UNITY_EDITOR && ENABLE_CLOUD_SERVICES_ANALYTICS) // Implicitly: !UNITY_EDITOR
+                #endif // UNITY_2023_2_OR_NEWER
+            #elif (UNITY_ANALYTICS) // Implicitly: !UNITY_EDITOR
             var info = analytic.info;
             Analytics.Analytics.RegisterEvent(info.Name, info.MaxEventsPerHour, info.MaxNumberOfElements, InputAnalytics.kVendorKey);
             if (analytic.TryGatherData(out var data, out var error))
                 Analytics.Analytics.SendEvent(info.Name, data);
             else
-                Debug.Log(error); // Non fatal
-        #endif //UNITY_EDITOR && ENABLE_CLOUD_SERVICES_ANALYTICS
+                Debug.Log(error);     // Non fatal
+            #endif //UNITY_EDITOR
+        #endif //ENABLE_CLOUD_SERVICES_ANALYTICS
         }
 
         #endif // UNITY_ANALYTICS || UNITY_EDITOR
