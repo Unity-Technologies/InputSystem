67191093f5bfa1d457cbbab5ed7a1ba610722694
Simon Wittber <simonwittber@unity3d.com>
2024-10-31 17:24:36 +0800

    modified suggested fix

--- Packages/com.unity.inputsystem/InputSystem/Plugins/XR/TrackedPoseDriver.cs
+++ Packages/com.unity.inputsystem/InputSystem/Plugins/XR/TrackedPoseDriver.cs
@@ -492,7 +492,26 @@ namespace UnityEngine.InputSystem.XR
                 // Treat an Input Action Reference with no reference the same as
                 // an enabled Input Action with no authored bindings, and allow driving the Transform pose.
                 // TODO Remove if suggested fix seems valid. m_CurrentTrackingState = TrackingStates.Position | TrackingStates.Rotation;
-                m_CurrentTrackingState = TrackingStates.None;
+                
+                // Check if we have transform and rotation controls to drive the pose.
+                var positionInputAction = m_PositionInput.action;
+                var rotationInputAction = m_RotationInput.action;
+                if (positionInputAction != null && positionInputAction.m_BindingsCount > 0 && rotationInputAction != null && rotationInputAction.m_BindingsCount > 0)
+                {
+                    m_CurrentTrackingState = TrackingStates.Position | TrackingStates.Rotation;
+                }
+                else if (positionInputAction != null && positionInputAction.m_BindingsCount > 0)
+                {
+                    m_CurrentTrackingState = TrackingStates.Position;
+                }
+                else if (rotationInputAction != null && rotationInputAction.m_BindingsCount > 0)
+                {
+                    m_CurrentTrackingState = TrackingStates.Rotation;
+                }
+                else
+                {
+                    m_CurrentTrackingState = TrackingStates.None;
+                }
                 return;
             }
 
