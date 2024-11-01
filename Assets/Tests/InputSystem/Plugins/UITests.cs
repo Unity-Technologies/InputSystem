058aab1c9dc8bd4e7ad73c0dcfd74e039b65f2ba
Benoît Malrat <benoit.malrat@unity3d.com>
2024-11-01 13:14:40 -0400

    fix test

--- Assets/Tests/InputSystem/Plugins/UITests.cs
+++ Assets/Tests/InputSystem/Plugins/UITests.cs
@@ -2630,8 +2630,10 @@ internal partial class UITests : CoreTestsFixture
         Release(mouse.leftButton);
         scene.eventSystem.InvokeUpdate();
 
+#if UNITY_2023_2_OR_NEWER // UnityEngine.InputForUI Module unavailable in earlier releases
         // Process all queued UI events to ensure that next events will not make the events list capacity growing
         UnityEngine.InputForUI.EventProvider.NotifyUpdate();
+#endif
 
         var kProfilerRegion = "UI_ClickDraggingDoesNotAllocateGCMemory";
 
