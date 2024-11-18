800a97d23c9a110f47d4f639ba3b9aeab9354b74
Stefan Schubert <stefan@unity3d.com>
2024-11-18 14:23:20 +0100

    initial setup

---
+++ Tools/CI/Settings/InputSystemSettings.cs
@@ -0,0 +1,30 @@
+﻿using RecipeEngine.Api.Settings;
+using RecipeEngine.Modules.Wrench.Models;
+using RecipeEngine.Modules.Wrench.Settings;
+
+namespace InputSystem.Cookbook.Settings;
+
+public class InputSystemSettings : AnnotatedSettingsBase
+{
+    // Path from the root of the repository where packages are located.
+    readonly string[] PackagesRootPaths = {"Packages"};
+
+    // update this to list all packages in this repo that you want to release.
+    Dictionary<string, PackageOptions> PackageOptions = new()
+    {
+        {
+            "com.unity.inputsystem",
+            new PackageOptions() { ReleaseOptions = new ReleaseOptions() { IsReleasing = true } }
+        }
+    };
+
+    public InputSystemSettings()
+    {
+        Wrench = new WrenchSettings(
+            PackagesRootPaths,
+            PackageOptions
+        );      
+    }
+
+    public WrenchSettings Wrench { get; private set; }
+}
