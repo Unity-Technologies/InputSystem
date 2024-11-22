73b7bcb5889cfcf17ed83ee7a9c33b5cab8db441
Rita Merkl <rita.merkl@unity3d.com>
2024-11-22 15:00:35 +0100

    set active input handler to InputManager when package is removed

---
+++ Packages/com.unity.inputsystem/InputSystem/Editor/InputSystemPackageControl.cs
@@ -0,0 +1,50 @@
+#if UNITY_EDITOR
+using System;
+using System.Collections.ObjectModel;
+using UnityEditor;
+using UnityEditor.PackageManager;
+
+
+namespace UnityEngine.InputSystem.Editor
+{
+    /// <summary>
+    /// Force restart if InputSystem package is added to activate and initialize it on managed side.
+    /// Set Project Settings input handling to reflect the presence of the Input System package, since it is not available in the UI.
+    /// </summary>
+    internal class InputSystemPackageControl
+    {
+        const string packageName = "com.unity.inputsystem";
+
+        [InitializeOnLoadMethod]
+        static void  SubscribePackageManagerEvent()
+        {
+            //there's a number of cases where it might not be called, for instance if the user changed the project manifest and deleted the Library folder before opening the project
+            UnityEditor.PackageManager.Events.registeringPackages += CheckForInputSystemPackageRemoved;
+        }
+
+        private static void CheckForInputSystemPackageRemoved(PackageRegistrationEventArgs packageArgs)
+        {
+            if (InputSystemAddedRemoved(packageArgs.removed))
+                HandleInputSystemRemoved();
+        }
+
+        private static bool InputSystemAddedRemoved(ReadOnlyCollection<UnityEditor.PackageManager.PackageInfo> packages)
+        {
+            foreach (var package in packages)
+            {
+                if (package.name == packageName)
+                    return true;
+            }
+            return false;
+        }
+
+        private static void HandleInputSystemRemoved()
+        {
+            //set input handling to none
+            EditorPlayerSettingHelpers.newSystemBackendsEnabled = false;
+            if (EditorUtility.DisplayDialog("Unity editor restart required", "You've removed the input system package. This requires a restart of the Editor.", "Restart Editor", "Ignore (Not recommended)"))
+                EditorApplication.OpenProject(Environment.CurrentDirectory);
+        }
+    }
+}
+#endif
