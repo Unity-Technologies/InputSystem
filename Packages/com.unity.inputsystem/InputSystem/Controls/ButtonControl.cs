f83179e3bf8a8475e8075beabdb76ade0b50494f
Stefan Schubert <stefan@unity3d.com>
2024-12-05 21:00:18 +0100

    update

--- Packages/com.unity.inputsystem/InputSystem/Controls/ButtonControl.cs
+++ Packages/com.unity.inputsystem/InputSystem/Controls/ButtonControl.cs
@@ -34,7 +34,6 @@ namespace UnityEngine.InputSystem.Controls
         /// <summary>
         /// The minimum value the button has to reach for it to be considered pressed.
         /// </summary>
-        /// <value>Button press threshold.</value>
         /// <remarks>
         /// The button is considered pressed, if it has a value equal to or greater than
         /// this value.
@@ -64,7 +63,6 @@ namespace UnityEngine.InputSystem.Controls
         /// <summary>
         /// Return <see cref="pressPoint"/> if set, otherwise return <see cref="InputSettings.defaultButtonPressPoint"/>.
         /// </summary>
-        /// <value>Effective value to use for press point thresholds.</value>
         public float pressPointOrDefault => pressPoint > 0 ? pressPoint : s_GlobalDefaultButtonPressPoint;
 
         /// <summary>
@@ -143,7 +141,6 @@ namespace UnityEngine.InputSystem.Controls
         /// <summary>
         /// Whether the button is currently pressed.
         /// </summary>
-        /// <value>True if button is currently pressed.</value>
         /// <remarks>
         /// A button is considered pressed if its value is equal to or greater
         /// than its button press threshold (<see cref="pressPointOrDefault"/>).
@@ -242,7 +239,6 @@ namespace UnityEngine.InputSystem.Controls
         /// <summary>
         /// Whether the press started this frame.
         /// </summary>
-        /// <value>True if the current press of the button started this frame.</value>
         /// <remarks>
         /// The first time this function - or wasReleasedThisFrame - are called, it's possible that extremely fast
         /// inputs (or very slow frame update times) will result in presses/releases being missed.
@@ -304,16 +300,16 @@ namespace UnityEngine.InputSystem.Controls
         /// <summary>
         /// Whether the press ended this frame.
         /// </summary>
-        /// <value>True if the current press of the button ended this frame.</value>
         /// <remarks>
         /// _Note_: The Input System identifies keys by physical layout, not according to the current language mapping of the keyboard. To query the name of the key according to the language mapping, use <see cref="InputControl.displayName"/>.
         /// </remarks>
         /// <example>
         /// <para>An example showing the use of this property on a gamepad button and a keyboard key:</para>
+        /// 
         /// <code>
         /// using UnityEngine;
         /// using UnityEngine.InputSystem;
-        ///
+        /// 
         /// public class ExampleScript : MonoBehaviour
         /// {
         ///     void Update()
