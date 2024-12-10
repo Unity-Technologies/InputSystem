38421aa10aaca37bf7157584181de4e1e72b76ef
Stefan Schubert <stefan@unity3d.com>
2024-12-10 14:35:23 +0100

    Update InputActionsEditorTests.cs

--- Assets/Tests/InputSystem.Editor/InputActionsEditorTests.cs
+++ Assets/Tests/InputSystem.Editor/InputActionsEditorTests.cs
@@ -148,6 +148,7 @@ internal class InputActionsEditorTests : UIToolkitBaseTestWindow<InputActionsEdi
     }
 
     [UnityTest]
+    [Ignore("Instability, see ISXB-1284")]
     public IEnumerator CanDeleteActionMap()
     {
         var actionMapsContainer = m_Window.rootVisualElement.Q("action-maps-container");
