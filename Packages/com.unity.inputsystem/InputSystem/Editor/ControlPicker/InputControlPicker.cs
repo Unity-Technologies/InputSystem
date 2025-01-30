c2550974251fa8148cab731a6f3c0342767ebcdf
Simon Wittber <simonwittber@unity3d.com>
2025-01-30 12:23:04 +0800

    remove unused code

--- Packages/com.unity.inputsystem/InputSystem/Editor/ControlPicker/InputControlPicker.cs
+++ Packages/com.unity.inputsystem/InputSystem/Editor/ControlPicker/InputControlPicker.cs
@@ -5,32 +5,8 @@ using System;
 
 namespace UnityEngine.InputSystem.Editor
 {
-    /// <summary>
-    /// A popup that allows picking input controls graphically.
-    /// </summary>
-    public sealed class InputControlPicker : IDisposable
+    public sealed class InputControlPicker
     {
-        public InputControlPicker(Mode mode, Action<string> onPick, InputControlPickerState state)
-        {
-            m_State = state ?? new InputControlPickerState();
-            m_Dropdown = new InputControlPickerDropdown(state, onPick, mode: mode);
-        }
-
-        public void Show(Rect rect)
-        {
-            m_Dropdown.Show(rect);
-        }
-
-        public void Dispose()
-        {
-            m_Dropdown?.Dispose();
-        }
-
-        public InputControlPickerState state => m_State;
-
-        private readonly InputControlPickerDropdown m_Dropdown;
-        private readonly InputControlPickerState m_State;
-
         public enum Mode
         {
             PickControl,
