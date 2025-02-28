0e4b64b8c6210bcc0daf0305b403e4462ad84414
Rita Merkl <rita.merkl@unity3d.com>
2025-02-28 12:45:17 +0100

    disabling all maps is not necessary anymore with copy

--- Packages/com.unity.inputsystem/InputSystem/Plugins/PlayerInput/PlayerInput.cs
+++ Packages/com.unity.inputsystem/InputSystem/Plugins/PlayerInput/PlayerInput.cs
@@ -960,11 +960,6 @@ namespace UnityEngine.InputSystem
 
             m_InputActive = true;
 
-            // reset state to default, only one action map is enabled at the initial state
-            // Project wide actions may have enabled action maps
-            if (m_Actions)
-                m_Actions.Disable();
-
             // If we have no current action map but there's a default
             // action map, make it current.
             if (m_CurrentActionMap == null && m_Actions != null && !string.IsNullOrEmpty(m_DefaultActionMap))
