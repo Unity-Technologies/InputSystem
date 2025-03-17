10a941c336499b43dff033f055ed8e68dd0b96cf
Trudy Spiller <trudy@unity3d.com>
2025-03-17 10:39:17 +0000

    Update to check for composite and partofcomposite

--- Packages/com.unity.inputsystem/InputSystem/Editor/AssetEditor/InputActionTreeView.cs
+++ Packages/com.unity.inputsystem/InputSystem/Editor/AssetEditor/InputActionTreeView.cs
@@ -796,7 +796,7 @@ namespace UnityEngine.InputSystem.Editor
             if (location.item is ActionTreeItemBase dropTarget)
             {
                 // Specific case - Composite parts cannot be dropped into Bindings
-                if (location.item is not CompositeBindingTreeItem && tag == k_PartOfCompositeBindingTag)
+                if (tag == k_PartOfCompositeBindingTag && location.item is not(CompositeBindingTreeItem or PartOfCompositeBindingTreeItem))
                     return;
 
                 if (!dropTarget.GetDropLocation(itemType, location.childIndex, ref array, ref arrayIndex))
