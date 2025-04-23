774190a60901dab8ebc4e1783da23d23f496fcb0
Aswin Gopal <ashwin.g@rythmos.com>
2025-04-23 16:53:12 +0530

    Address PR feedbacks, changelog changed, test name modified, comments added, made classes and enums internal and refactor.

--- Assets/Tests/InputSystem.Editor/CustomProcessorEnumTest.cs
+++ Assets/Tests/InputSystem.Editor/CustomProcessorEnumTest.cs
@@ -1,4 +1,4 @@
-#if UNITY_EDITOR && UNITY_INPUT_SYSTEM_PROJECT_WIDE_ACTIONS && UNITY_6000_0_OR_NEWER
+#if UNITY_EDITOR && UNITY_INPUT_SYSTEM_PROJECT_WIDE_ACTIONS
 
 using System;
 using NUnit.Framework;
@@ -11,18 +11,16 @@ using UnityEngine.InputSystem.Editor;
 using UnityEngine.TestTools;
 using UnityEngine.UIElements;
 
-public enum SomeEnum
+internal enum SomeEnum
 {
     OptionA = 10,
-    OptionB = 20,
-    OptionC = 50,
-    OptionD = 100
+    OptionB = 20
 }
 
 #if UNITY_EDITOR
 [InitializeOnLoad]
 #endif
-public class CustomProcessor : InputProcessor<float>
+internal class CustomProcessor : InputProcessor<float>
 {
     public SomeEnum SomeEnum;
 
@@ -72,29 +70,30 @@ internal class CustomProcessorEnumTest : UIToolkitBaseTestWindow<InputActionsEdi
     }
     
     [UnityTest]
-    public IEnumerator CustomProcessorEnum_SerialiseTheValue_InAsset()
+    public IEnumerator ProcessorEnum_ShouldSerializeByValue_WhenSerializedToAsset()
     {
+        // Serialize current asset to JSON, and check that initial JSON contains default enum value for OptionA
         var json = m_Window.currentAssetInEditor.ToJson();
         
         Assert.That(json.Contains("Custom(SomeEnum=10)"), Is.True,
             "Serialized JSON does not contain the expected custom processor string for OptionA.");
-        
-        var dropdownList = m_Window.rootVisualElement.Query<DropdownField>().Where(d => d.choices != null && d.choices.Contains("OptionA") && d.choices.Contains("OptionB")).ToList();
+
+        // Query the dropdown with exactly two enum choices and check that the drop down is present in the UI
+        var dropdownList = m_Window.rootVisualElement.Query<DropdownField>().Where(d => d.choices.Count == 2).ToList();
         Assume.That(dropdownList.Count > 0, Is.True, "Enum parameter dropdown not found in the UI.");
 
+        // Determine the new value to be set in the dropdown, focus the dropdown before dispatching the change 
         var dropdown = dropdownList.First();
-
-        var newIndex = dropdown.choices.IndexOf("OptionB"); 
-        var oldValue = dropdown.value;
-        var newValue = dropdown.choices[newIndex];
-
+        var newValue = dropdown.choices[1];
         dropdown.Focus();
         dropdown.value = newValue;
 
-        var changeEvent = ChangeEvent<Enum>.GetPooled(SomeEnum.OptionA, SomeEnum.OptionC);
+        // Create and send a change event from OptionA to OptionB
+        var changeEvent = ChangeEvent<Enum>.GetPooled(SomeEnum.OptionA, SomeEnum.OptionB);
         changeEvent.target = dropdown;
         dropdown.SendEvent(changeEvent);
         
+        // Find the save button in the window, focus and click the save button to persist the changes
         var saveButton = m_Window.rootVisualElement.Q<Button>("save-asset-toolbar-button");
         Assume.That(saveButton, Is.Not.Null, "Save Asset button not found in the UI.");
         saveButton.Focus();
@@ -102,8 +101,8 @@ internal class CustomProcessorEnumTest : UIToolkitBaseTestWindow<InputActionsEdi
 
         Assert.That(dropdown.value, Is.EqualTo(newValue));
 
+        // Verify that the updated JSON contains the new enum value for OpitonB
         var updatedJson = m_Window.currentAssetInEditor.ToJson();
-        
         Assert.That(updatedJson.Contains("Custom(SomeEnum=20)"), Is.True, "Serialized JSON does not contain the updated custom processor string for OptionB.");
 
         yield return null;
