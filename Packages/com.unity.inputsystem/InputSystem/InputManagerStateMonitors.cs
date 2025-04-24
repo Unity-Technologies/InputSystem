7d07d89910e04200af6477eb4a66e65249599691
Håkan Sidenvall <hakan.sidenvall@unity3d.com>
2025-04-24 14:49:49 +0200

    Undo debug comment

--- Packages/com.unity.inputsystem/InputSystem/InputManagerStateMonitors.cs
+++ Packages/com.unity.inputsystem/InputSystem/InputManagerStateMonitors.cs
@@ -391,7 +391,6 @@ namespace UnityEngine.InputSystem
                 var listener = listeners[i];
                 try
                 {
-                    // CALLBACK HERE
                     listener.monitor.NotifyControlStateChanged(listener.control, time, eventPtr,
                         listener.monitorIndex);
                 }
