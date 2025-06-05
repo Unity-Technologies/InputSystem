657edfc50df8923830243a87036b19d93a62b5cb
Jason Diehl <jason.diehl@unity3d.com>
2025-06-05 11:49:09 -0400

    revert back to filter yaml

--- Packages/com.unity.inputsystem/InputSystem/Plugins/XR/GenericXRDevice.cs
+++ Packages/com.unity.inputsystem/InputSystem/Plugins/XR/GenericXRDevice.cs
@@ -1,4 +1,3 @@
-#if PACKAGE_DOCS_GENERATION
 using UnityEngine.InputSystem.Controls;
 using UnityEngine.InputSystem.XR.Haptics;
 using UnityEngine.InputSystem.Layouts;
@@ -154,4 +153,3 @@ namespace UnityEngine.InputSystem.XR
         }
     }
 }
-#endif
