8519aa3e40ef3a898c8b1a166f82b2ee5b092807
Jason Diehl <jason.diehl@unity3d.com>
2025-06-09 13:53:57 -0400

    move summary

--- Packages/com.unity.inputsystem/InputSystem/Plugins/XR/GenericXRDevice.cs
+++ Packages/com.unity.inputsystem/InputSystem/Plugins/XR/GenericXRDevice.cs
@@ -6,31 +6,27 @@ using UnityEngine.XR;
 namespace UnityEngine.InputSystem.XR
 {
     /// <summary>
-    /// The base type for all XR head mounted displays.
+    /// The base type of all XR head mounted displays.  This can help organize shared behaviour across all HMDs.
     /// </summary>
+    ///
+    /// <remarks>
+    ///
+    /// To give your head tracking an extra update before rendering:
+    /// First, enable before render updates on your Device.
+    ///
+    ///     // JSON
+    ///     {
+    ///         "name" : "MyHMD",
+    ///         "extend" : "HMD",
+    ///         "beforeRender" : "Update"
+    ///     }
+    ///
+    /// Then, make sure you put extra `StateEvents` for your HMD on the queue right in time before rendering. Also, if your HMD is a combination of non-tracking and tracking controls, you can update just the tracking by sending a delta event instead of a full state event.
+    ///
+    /// </remarks>
     [InputControlLayout(isGenericTypeOfDevice = true, displayName = "XR HMD", canRunInBackground = true)]
     public class XRHMD : TrackedDevice
     {
-        /// <summary>
-        /// The base type of all XR head mounted displays.  This can help organize shared behaviour across all HMDs.
-        /// </summary>
-        ///
-        /// <remarks>
-        ///
-        /// To give your head tracking an extra update before rendering:
-        /// First, enable before render updates on your Device.
-        ///
-        ///     // JSON
-        ///     {
-        ///         "name" : "MyHMD",
-        ///         "extend" : "HMD",
-        ///         "beforeRender" : "Update"
-        ///     }
-        ///
-        /// Then, make sure you put extra `StateEvents` for your HMD on the queue right in time before rendering. Also, if your HMD is a combination of non-tracking and tracking controls, you can update just the tracking by sending a delta event instead of a full state event.
-        ///
-        /// </remarks>
-
         /// <summary>
         /// Accessor for left eye position.
         /// </summary>
