fce3fbcde7a77819913db4dc516676333b2f2c08
João Freire <joao.freire@unity3d.com>
2025-07-01 13:57:18 +0300

    Increase even more the timeout

--- Assets/Tests/InputSystem.Editor/InputActionsEditorTests.cs
+++ Assets/Tests/InputSystem.Editor/InputActionsEditorTests.cs
@@ -45,7 +45,7 @@ internal class InputActionsEditorTests : UIToolkitBaseTestWindow<InputActionsEdi
 
     #region Helper methods
 
-    IEnumerator WaitForActionMapRename(int index, bool isActive, double timeoutSecs = 10.0)
+    IEnumerator WaitForActionMapRename(int index, bool isActive, double timeoutSecs = 20.0)
     {
         return WaitUntil(() =>
         {
@@ -58,7 +58,7 @@ internal class InputActionsEditorTests : UIToolkitBaseTestWindow<InputActionsEdi
         }, $"WaitForActionMapRename {index} {isActive}", timeoutSecs);
     }
 
-    IEnumerator WaitForActionRename(int index, bool isActive, double timeoutSecs = 10.0)
+    IEnumerator WaitForActionRename(int index, bool isActive, double timeoutSecs = 20.0)
     {
         return WaitUntil(() =>
         {
