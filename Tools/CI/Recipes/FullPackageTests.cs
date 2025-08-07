545e75ec0b2e0a26432b72033983fd655273d353
wind.xu <wind.xu@unity3d.com>
2025-08-07 14:35:13 -0400

    Only install netfx on Windows

--- Tools/CI/Recipes/FullPackageTests.cs
+++ Tools/CI/Recipes/FullPackageTests.cs
@@ -2,6 +2,7 @@ using InputSystem.Cookbook.Settings;
 using RecipeEngine.Api.Artifacts;
 using RecipeEngine.Api.Extensions;
 using RecipeEngine.Api.Jobs;
+using RecipeEngine.Api.Platforms;
 using RecipeEngine.Modules.UnifiedTestRunner;
 using RecipeEngine.Modules.Wrench.Models;
 using RecipeEngine.Platforms;
@@ -19,12 +20,18 @@ public class FullPackageTests: InputBaseRecipe
             "%GSUDO% choco install netfx-4.7.1-devpack -y --ignore-detected-reboot --ignore-package-codes";
         var doctoolsInstallCmd =
             "git clone --branch \"2.3.0-preview\" git@github.cds.internal.unity3d.com:unity/com.unity.package-manager-doctools.git Packages/com.unity.package-manager-doctools";
-        
+
         IJobBuilder job = JobBuilder.Create(jobName)
             .WithDescription(jobName)
-            .WithPlatform(platform)
-            .WithCommands(c => c
-                .Add(netfxInstallCmd)
+            .WithPlatform(platform);
+
+        if (platform.System == SystemType.Windows)
+        {
+            job.WithCommands(c => c
+                .Add(netfxInstallCmd));
+        }
+
+        job.WithCommands(c => c
                 .Add(doctoolsInstallCmd)
                 .Add(Utilities.GetEditorDownloadCommand(unityBranch, platform))
                 //.Add($"upm-pvp create-test-project {ProjectPath} --packages \"upm-ci~/packages/*.tgz\" --unity .Editor")
@@ -35,8 +42,8 @@ public class FullPackageTests: InputBaseRecipe
                     .WithCategory("!Performance")
                     .WithRerun(1, true)
                     .WithArtifacts("artifacts"))))
-            //.WithDependencies(Settings.Wrench.WrenchJobs[InputSystemSettings.InputSystemPackageName][JobTypes.Pack])
             .WithArtifact(new Artifact("artifacts", "artifacts/**/*"));
+            //.WithDependencies(Settings.Wrench.WrenchJobs[InputSystemSettings.InputSystemPackageName][JobTypes.Pack])
         return job;
     }
 }
