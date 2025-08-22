9dfc7a3c1edb1106448b464313f1d21f0a104e86
wind.xu <wind.xu@unity3d.com>
2025-08-22 11:12:39 -0400

    Update Wrench to v1.0.1 and update Ubuntu image to 22.04

--- Tools/CI/Settings/InputSystemSettings.cs
+++ Tools/CI/Settings/InputSystemSettings.cs
@@ -91,6 +91,11 @@ public class InputSystemSettings : AnnotatedSettingsBase
             wrenchCsProjectPath: "/Tools/CI/InputSystem.Cookbook.csproj",
             useLocalPvpExemptions: true
         );
+        
+        var defaultUbuntuPlatform = WrenchPackage.DefaultEditorPlatforms[SystemType.Ubuntu];
+        // Use Ubuntu image package-ci/ubuntu-22.04 which is required by 6000.0+ versions.
+        Wrench.Packages["com.unity.inputsystem"].EditorPlatforms[SystemType.Ubuntu] = new Platform(new Agent("package-ci/ubuntu-22.04:default", 
+            defaultUbuntuPlatform.Agent.Flavor, defaultUbuntuPlatform.Agent.Resource), defaultUbuntuPlatform.System);
 
         // ignore packages listed below in PreviewAPV
         InputSystemPackage.DependantsToIgnoreInPreviewApv = new Dictionary<Editor, ISet<string>>()
