8ca84f40f0656668c6ad60c35ca6e32ae3ffbbc1
Darren Kelly <darren.kelly@unity3d.com>
2025-08-28 13:06:22 +0200

    Update code to reflect code reivew changes.

--- Packages/com.unity.inputsystem/InputSystem/Plugins/InputForUI/InputActionAssetVerifier.cs
+++ Packages/com.unity.inputsystem/InputSystem/Plugins/InputForUI/InputActionAssetVerifier.cs
@@ -70,11 +70,7 @@ namespace UnityEngine.InputSystem.Plugins.InputForUI
             private string GetAssetReference()
             {
                 var path = AssetDatabase.GetAssetPath(asset);
-                if (string.IsNullOrEmpty(path)) 
-                {
-                    return asset.name;
-                }
-                return path;
+                return string.IsNullOrEmpty(path) ? asset.name : path;
             }
 
             private void ActionMapWarning(string actionMap, string problem)
