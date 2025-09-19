70c1839251ea8d040c01926a7a1a9376670bf8ab
Amy Louise Green <amylouise@unity3d.com>
2025-09-19 12:17:03 -0400

    DOCATT-8610: Fixed bad wording for joining the action in PlayerInputManager.cs

--- Packages/com.unity.inputsystem/InputSystem/Plugins/PlayerInput/PlayerInputManager.cs
+++ Packages/com.unity.inputsystem/InputSystem/Plugins/PlayerInput/PlayerInputManager.cs
@@ -335,8 +335,8 @@ namespace UnityEngine.InputSystem
         /// Inhibit players from joining the game.
         /// </summary>
         /// <remarks>
-        /// Note that this method might disable the action, depending on how the action
-        /// was joined initially. Specifically, if the initial joining was triggered using
+        /// Note that this method might disable the action, depending on how the player
+        /// joined initially. Specifically, if the initial joining was triggered using
         /// the <see cref="PlayerJoinBehavior.JoinPlayersWhenJoinActionIsTriggered"/> behavior,
         /// this method also disables the join action.
         /// </remarks>
