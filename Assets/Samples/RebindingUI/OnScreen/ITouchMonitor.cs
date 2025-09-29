df6a2b8b5f34100cdc0b1425b2ce3e4fdbd39244
Håkan Sidenvall <hakan.sidenvall@unity3d.com>
2025-09-29 19:59:28 +0200

    Removed files that will be routed via other PRs

--- Assets/Samples/RebindingUI/OnScreen/ITouchMonitor.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine.InputSystem.LowLevel;
-
-namespace UnityEngine.InputSystem.Samples.RebindUI
-{
-    internal interface ITouchMonitor : IInputStateChangeMonitor
-    {
-        void Reset(Rect bounds, AreaShape shape, Detector.GestureEventHandler handler);
-    }
-}
