33fd875cb30bea5ce005d5d5bbdc74a7423a8ead
Anthony Yakovlev <anthony@unity3d.com>
2025-11-14 17:44:54 +0300

    fixup typo

--- Packages/com.unity.inputsystem/InputSystem/Actions/Composites/AxisComposite.cs
+++ Packages/com.unity.inputsystem/InputSystem/Actions/Composites/AxisComposite.cs
@@ -212,7 +212,7 @@ namespace UnityEngine.InputSystem.Composites
     internal class AxisCompositeEditor : InputParameterEditor<AxisComposite>
     {
         private const string label = "Which Side Wins";
-        private const string tolltip = "Determine which axis 'wins' if both are actuated at the same time. "
+        private const string tooltip = "Determine which axis 'wins' if both are actuated at the same time. "
             + "If 'Neither' is selected, the result is 0 (or, more precisely, "
             + "the midpoint between minValue and maxValue).";
 
@@ -220,7 +220,7 @@ namespace UnityEngine.InputSystem.Composites
         {
             var modeField = new EnumField(label, target.whichSideWins)
             {
-                tooltip = tolltip
+                tooltip = tooltip
             };
 
             modeField.RegisterValueChangedCallback(evt =>
