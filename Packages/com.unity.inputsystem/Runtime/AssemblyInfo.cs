9306b7e877668ac40283bd32f60a9879860615d7
João Freire <joao.freire@unity3d.com>
2025-11-28 11:47:32 +0200

    Update assemblies dependencies
    Also exposes the required internals between assemblies.

--- Packages/com.unity.inputsystem/Runtime/AssemblyInfo.cs
+++ Packages/com.unity.inputsystem/Runtime/AssemblyInfo.cs
@@ -5,6 +5,7 @@ using UnityEngine.InputSystem;
 [assembly: AssemblyVersion(InputSystem.kAssemblyVersion)]
 [assembly: InternalsVisibleTo("Unity.InputSystem.TestFramework")]
 [assembly: InternalsVisibleTo("Unity.InputSystem.Tests.Editor")]
+[assembly: InternalsVisibleTo("Unity.InputSystem.Editor")]
 [assembly: InternalsVisibleTo("Unity.InputSystem.Tests")]
 [assembly: InternalsVisibleTo("Unity.InputSystem.IntegrationTests")]
 [assembly: InternalsVisibleTo("Unity.InputSystem.ForUI")] // To avoid minor bump
