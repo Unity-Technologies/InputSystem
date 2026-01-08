209c97c5ed340642dea049208cbd0d9994dd87a7
josepmariapujol-unity <59828124+josepmariapujol-unity@users.noreply.github.com>
2026-01-08 17:04:13 +0100

    Adding todo mention

--- Packages/com.unity.inputsystem/InputSystem/Controls/InputControlPath.cs
+++ Packages/com.unity.inputsystem/InputSystem/Controls/InputControlPath.cs
@@ -1016,13 +1016,10 @@ namespace UnityEngine.InputSystem
                         break;
                     }
 
+                    ////TODO: allow only single '*' and recognize '**'
                     // If we've reached a '*' in the path, skip character in name.
                     if (nextCharInPath == '*')
                     {
-                        // Collapse consecutive '*' so matching logic here only needs to handle a single '*'.
-                        while (indexInPath + 1 < pathLength && path[indexInPath + 1] == '*')
-                            ++indexInPath;
-                        
                         // But first let's see if we have something after the wildcard that matches the rest of the component.
                         // This could be when, for example, we hit "T" on matching "leftTrigger" against "*Trigger". We have to stop
                         // gobbling up characters for the wildcard when reaching "Trigger" in the component name.
