93585a1c6ad7a140ccf454e675dda9683b14d27f
JosepMariaPujol <josep.pujol@unity3d.com>
2026-01-09 14:43:00 +0100

    Reverting changes from refactoring

--- Packages/com.unity.inputsystem/InputSystem/Controls/InputControlLayout.cs
+++ Packages/com.unity.inputsystem/InputSystem/Controls/InputControlLayout.cs
@@ -369,7 +369,7 @@ namespace UnityEngine.InputSystem.Layouts
 
         public IEnumerable<InternedString> appliedOverrides => m_AppliedOverrides;
 
-        public ReadOnlyArray<InternedString> usages => new ReadOnlyArray<InternedString>(m_Usages);
+        public ReadOnlyArray<InternedString> commonUsages => new ReadOnlyArray<InternedString>(m_CommonUsages);
 
         /// <summary>
         /// List of child controls defined for the layout.
@@ -947,9 +947,9 @@ namespace UnityEngine.InputSystem.Layouts
                 isNoisy = layoutAttribute?.isNoisy ?? false
             };
 
-            if (layoutAttribute?.usages != null)
-                layout.m_Usages =
-                    ArrayHelpers.Select(layoutAttribute.usages, x => new InternedString(x));
+            if (layoutAttribute?.commonUsages != null)
+                layout.m_CommonUsages =
+                    ArrayHelpers.Select(layoutAttribute.commonUsages, x => new InternedString(x));
 
             return layout;
         }
@@ -978,7 +978,7 @@ namespace UnityEngine.InputSystem.Layouts
         internal bool? m_UpdateBeforeRender;
         internal InlinedArray<InternedString> m_BaseLayouts;
         private InlinedArray<InternedString> m_AppliedOverrides;
-        private InternedString[] m_Usages;
+        private InternedString[] m_CommonUsages;
         internal ControlItem[] m_Controls;
         internal string m_DisplayName;
         private string m_Description;
@@ -1319,7 +1319,7 @@ namespace UnityEngine.InputSystem.Layouts
                 m_StateFormat = other.m_StateFormat;
 
             // Combine common usages.
-            m_Usages = ArrayHelpers.Merge(other.m_Usages, m_Usages);
+            m_CommonUsages = ArrayHelpers.Merge(other.m_CommonUsages, m_CommonUsages);
 
             // Retain list of overrides.
             m_AppliedOverrides.Merge(other.m_AppliedOverrides);
@@ -1565,7 +1565,7 @@ namespace UnityEngine.InputSystem.Layouts
             public string format;
             public string beforeRender; // Can't be simple bool as otherwise we can't tell whether it was set or not.
             public string runInBackground;
-            public string[] usages;
+            public string[] commonUsages;
             public string displayName;
             public string description;
             public string type; // This is mostly for when we turn arbitrary InputControlLayouts into JSON; less for layouts *coming* from JSON.
@@ -1609,7 +1609,7 @@ namespace UnityEngine.InputSystem.Layouts
                     isGenericTypeOfDevice = isGenericTypeOfDevice,
                     hideInUI = hideInUI,
                     m_Variants = new InternedString(variant),
-                    m_Usages = ArrayHelpers.Select(usages, x => new InternedString(x)),
+                    m_CommonUsages = ArrayHelpers.Select(commonUsages, x => new InternedString(x)),
                 };
                 if (!string.IsNullOrEmpty(format))
                     layout.m_StateFormat = new FourCC(format);
@@ -1676,7 +1676,7 @@ namespace UnityEngine.InputSystem.Layouts
                     extend = layout.m_BaseLayouts.length == 1 ? layout.m_BaseLayouts[0].ToString() : null,
                     extendMultiple = layout.m_BaseLayouts.length > 1 ? layout.m_BaseLayouts.ToArray(x => x.ToString()) : null,
                     format = layout.stateFormat.ToString(),
-                    usages = ArrayHelpers.Select(layout.m_Usages, x => x.ToString()),
+                    commonUsages = ArrayHelpers.Select(layout.m_CommonUsages, x => x.ToString()),
                     controls = ControlItemJson.FromControlItems(layout.m_Controls),
                     beforeRender = layout.m_UpdateBeforeRender != null ? (layout.m_UpdateBeforeRender.Value ? "Update" : "Ignore") : null,
                 };
