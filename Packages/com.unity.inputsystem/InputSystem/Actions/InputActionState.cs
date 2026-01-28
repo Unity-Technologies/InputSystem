0fd8a81d8dcbfd9fb55166d97dd57400e6d6bb81
Darren Kelly <darren.kelly@unity3d.com>
2026-01-28 14:39:07 +0100

    Add possible fix for exception reported by Paulius.

--- Packages/com.unity.inputsystem/InputSystem/Actions/InputActionState.cs
+++ Packages/com.unity.inputsystem/InputSystem/Actions/InputActionState.cs
@@ -1483,6 +1483,16 @@ namespace UnityEngine.InputSystem
 
                     // Store magnitude. We do this once and then only read it from here.
                     var control = controls[controlIndex];
+
+                    // We might end up here if an action map is enabled from e.g. an event processing callback such as
+                    // InputAction.cancel event handler (ISXB-1767). In this case we must skip controls associated with
+                    // a device that is not connected to the system (Have deviceIndex < 0). We check this here to not
+                    // cause side effects if aborting later in the call-chain.
+                    if (!controls[controlIndex].device.added)
+                    {
+                        return;
+                    }
+
                     trigger.magnitude = control.CheckStateIsAtDefault() ? 0f : control.magnitude;
                     controlMagnitudes[controlIndex] = trigger.magnitude;
 
