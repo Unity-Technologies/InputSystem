a9714c414f3109558ebf6dc76a6147bccc073978
Darren Kelly <darren.kelly@unity3d.com>
2026-01-28 14:39:59 +0100

    Update fix.

--- Packages/com.unity.inputsystem/InputSystem/Actions/InputActionState.cs
+++ Packages/com.unity.inputsystem/InputSystem/Actions/InputActionState.cs
@@ -1481,9 +1481,6 @@ namespace UnityEngine.InputSystem
                     if (m_OnBeforeUpdateHooked)
                         bindingStatePtr->initialStateCheckPending = false;
 
-                    // Store magnitude. We do this once and then only read it from here.
-                    var control = controls[controlIndex];
-
                     // We might end up here if an action map is enabled from e.g. an event processing callback such as
                     // InputAction.cancel event handler (ISXB-1767). In this case we must skip controls associated with
                     // a device that is not connected to the system (Have deviceIndex < 0). We check this here to not
@@ -1492,6 +1489,9 @@ namespace UnityEngine.InputSystem
                     {
                         return;
                     }
+
+                    // Store magnitude. We do this once and then only read it from here.
+                    var control = controls[controlIndex];
 
                     trigger.magnitude = control.CheckStateIsAtDefault() ? 0f : control.magnitude;
                     controlMagnitudes[controlIndex] = trigger.magnitude;
