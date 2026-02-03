1fd3ec500ae9372c6ecb9b262657abb700d79f68
Aswin Gopal <ashwin.g@rythmos.com>
2026-02-03 15:47:20 +0530

    Fix handled unpaired device events still triggering actions

--- Packages/com.unity.inputsystem/InputSystem/Utilities/DelegateHelpers.cs
+++ Packages/com.unity.inputsystem/InputSystem/Utilities/DelegateHelpers.cs
@@ -1,5 +1,6 @@
 using System;
 using Unity.Profiling;
+using UnityEngine.InputSystem.LowLevel;
 
 namespace UnityEngine.InputSystem.Utilities
 {
@@ -81,6 +82,45 @@ namespace UnityEngine.InputSystem.Utilities
             }
             callbacks.UnlockForChanges();
             marker.End();
+        }
+
+        public static bool InvokeCallbacksSafeUntilHandled(ref CallbackArray<Action<InputEventPtr, InputDevice>> callbacks,
+            InputEventPtr eventPtr, InputDevice device, ProfilerMarker marker, string callbackName, bool stopOnHandled)
+        {
+            if (callbacks.length == 0)
+                return false;
+
+            marker.Begin();
+            callbacks.LockForChanges();
+            var handled = false;
+            try
+            {
+                for (var i = 0; i < callbacks.length; ++i)
+                {
+                    try
+                    {
+                        callbacks[i](eventPtr, device);
+                    }
+                    catch (Exception exception)
+                    {
+                        Debug.LogException(exception);
+                        Debug.LogError($"{exception.GetType().Name} while executing '{callbackName}' callbacks");
+                    }
+
+                    if (stopOnHandled && eventPtr.handled)
+                    {
+                        handled = true;
+                        break;
+                    }
+                }
+            }
+            finally
+            {
+                callbacks.UnlockForChanges();
+                marker.End();
+            }
+
+            return handled;
         }
 
         public static bool InvokeCallbacksSafe_AnyCallbackReturnsTrue<TValue1, TValue2>(ref CallbackArray<Func<TValue1, TValue2, bool>> callbacks,
