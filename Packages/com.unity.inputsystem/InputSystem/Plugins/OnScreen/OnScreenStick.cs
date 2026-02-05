94564dc2aa5d4a9a0e59c9c72800021f6eb70065
Aswin Gopal <ashwin.g@rythmos.com>
2026-02-05 17:18:21 +0530

    Revert "Fix : Allow isolated input raycast selection to accept hits on child objects"
    This reverts commit ee9aace2b13fee60685203473134d768c0186838.

--- Packages/com.unity.inputsystem/InputSystem/Plugins/OnScreen/OnScreenStick.cs
+++ Packages/com.unity.inputsystem/InputSystem/Plugins/OnScreen/OnScreenStick.cs
@@ -252,7 +252,7 @@ namespace UnityEngine.InputSystem.OnScreen
             var stickSelected = false;
             foreach (var result in m_RaycastResults)
             {
-                if (!result.gameObject.transform.IsChildOf(transform)) continue;
+                if (result.gameObject != gameObject) continue;
 
                 stickSelected = true;
                 break;
