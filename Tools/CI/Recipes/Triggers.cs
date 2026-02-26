72b7bf338b25788f9254421c2a8b03570e80538f
Stefan Schubert <stefan@unity3d.com>
2026-02-26 13:55:36 +0100

    Upgrade to Wench 2.0

--- Tools/CI/Recipes/Triggers.cs
+++ Tools/CI/Recipes/Triggers.cs
@@ -3,15 +3,14 @@ using RecipeEngine.Api.Extensions;
 using RecipeEngine.Api.Jobs;
 using RecipeEngine.Api.Recipes;
 using RecipeEngine.Api.Triggers;
-using RecipeEngine.Modules.Wrench.Models;
 using InputSystem.Cookbook.Settings;
 using RecipeEngine.Api.Triggers.Recurring;
+using RecipeEngine.Modules.Wrench.Models;
 
 namespace InputSystem.Cookbook.Recipes;
 
 public class Triggers: RecipeBase
 {
-    InputSystemSettings settings = InputSystemSettings.Instance;
     IEnumerable<Dependency> allEditorFunctionalTests = new EditorFunctionalTests().AsDependencies();
     IEnumerable<Dependency> allStandaloneFunctionalTests = new StandaloneFunctionalTests().AsDependencies();
     IEnumerable<Dependency> allStandaloneIl2CppFunctionalTests = new StandaloneIl2CppFunctionalTests().AsDependencies();
@@ -27,7 +26,9 @@ public class Triggers: RecipeBase
     IEnumerable<Dependency> allMobilePerformanceTests = new MobilePerformanceTests().AsDependencies().Where( d => !d.JobId.Contains("TvOS"));
     // Run performance build jobs on TvOS for all Unity versions.
     IEnumerable<Dependency> allTvOSPerformanceBuildJobs = new MobilePerformanceBuildJobs().AsDependencies().Where(d=> d.JobId.Contains("TvOS"));
-        
+    
+    IEnumerable<Dependency> allValidationJobsForPackage = InputSystemSettings.Instance.Wrench.WrenchJobs[InputSystemSettings.InputSystemPackageName][JobTypes.Validation];
+    
     protected override ISet<Job> LoadJobs()
         => Combine.Collections(GetTriggers()).SelectJobs();
 
@@ -52,12 +53,19 @@ public class Triggers: RecipeBase
                 .WithDependencies(allMobileFunctionalTests)
                 .WithDependencies(allTvOSFunctionalBuildJobs)
                 .WithDependencies(new Dependency("wrench/promotion-jobs", "publish_dry_run_inputsystem"))
+                .WithDependencies(new Dependency("wrench/api-validation-jobs", "all_api_validation_jobs"))
                 .WithPullRequestTrigger(pr => pr.ExcludeDraft().And().WithTargetBranch(InputSystemSettings.BranchName).And().WithoutChanges("**/*.md"), true, CancelLeftoverJobs.Always),
 
             JobBuilder.Create("Nightly trigger")
                 .WithDependencies(allStandaloneIl2CppFunctionalTests.Where(d => d.JobId.Contains("Ubuntu")))
                 .WithDependencies(new Dependency("triggers", "all_performance_tests"))
                 .WithScheduleTrigger(Schedule.RunDaily(InputSystemSettings.BranchName)),
+            
+            JobBuilder.Create("Documentation trigger")
+                .WithDependencies(allValidationJobsForPackage.Where(d => d.JobId.Contains("ubuntu")))
+                .WithPullRequestTrigger(pr => pr.ExcludeDraft().And().WithTargetBranch(InputSystemSettings.BranchName)
+                    .And().WithChanges("**/*.md")
+                    .Or().WithChanges("Documentation~/**"), true, CancelLeftoverJobs.Always),
         ];
         return builders;
     }
