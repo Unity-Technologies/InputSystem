06136defed78405a8347ae2c0a834987a5424ab8
JosepMariaPujol <josep.pujol@unity3d.com>
2026-03-10 10:40:01 +0100

    Revert the order of toggle buttons for better alignment with the rest

--- Packages/com.unity.inputsystem/InputSystem/Actions/Interactions/HoldInteraction.cs
+++ Packages/com.unity.inputsystem/InputSystem/Actions/Interactions/HoldInteraction.cs
@@ -133,8 +133,8 @@ namespace UnityEngine.InputSystem.Interactions
 
         public override void OnDrawVisualElements(VisualElement root, Action onChangedCallback)
         {
-            m_PressPointSetting.OnDrawVisualElements(root, onChangedCallback);
             m_DurationSetting.OnDrawVisualElements(root, onChangedCallback);
+            m_PressPointSetting.OnDrawVisualElements(root, onChangedCallback);
             CustomOrDefaultSetting.AddSharedDefaultSettingsFooter(root,
                 new[] { m_PressPointSetting, m_DurationSetting });
         }
