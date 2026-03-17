28640ebbb82a0ce3c006a389266b854da0ab3753
Darren Kelly <darren.kelly@unity3d.com>
2026-03-17 14:52:25 +0100

    Comment out the shortcutKeysConsume input setting.

--- Packages/com.unity.inputsystem/InputSystem/Actions/InputActionState.cs
+++ Packages/com.unity.inputsystem/InputSystem/Actions/InputActionState.cs
@@ -148,7 +148,7 @@ namespace UnityEngine.InputSystem
 
             // If shortcut support is disabled, we simply put put all bindings at complexity=1 and
             // in their own group.
-            var disableControlGrouping = !InputSystem.settings.shortcutKeysConsumeInput;
+            var disableControlGrouping = true; // !InputSystem.settings.shortcutKeysConsumeInput;
 
             var currentGroup = 1u;
             for (var i = 0; i < totalControlCount; ++i)
