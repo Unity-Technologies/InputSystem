89e0f9373804a46da72d5c0784726e84d098c90b
JosepMariaPujol <josep.pujol@unity3d.com>
2026-03-23 12:03:20 +0100

    commit one assembly

--- Packages/com.unity.inputsystem/Editor/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Unity.InputSystem.TestFramework")]
-[assembly: InternalsVisibleTo("Unity.InputSystem.Tests")]
-[assembly: InternalsVisibleTo("Unity.InputSystem.Tests.Editor")]
-[assembly: InternalsVisibleTo("Unity.InputSystem.IntegrationTests")]
-[assembly: InternalsVisibleTo("Unity.InputSystem.EditorDevelopmentTools")]
