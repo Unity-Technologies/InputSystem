41691cb30ae0b69fbe01a5bb3d1e31565e4d8a18
Morgan Hoarau <morgan.hoarau@unity3d.com>
2026-04-15 15:07:18 +0100

    format

--- Packages/com.unity.inputsystem/InputSystem/Runtime/Plugins/UI/InputSystemUIInputModule.cs
+++ Packages/com.unity.inputsystem/InputSystem/Runtime/Plugins/UI/InputSystemUIInputModule.cs
@@ -999,9 +999,9 @@ namespace UnityEngine.InputSystem.UI
             // react.
             get => explictlyIgnoreFocus || InputRuntime.s_Instance.runInBackground
 #if UNITY_EDITOR
-                || InputSystem.s_Manager.isReplayActive
+            || InputSystem.s_Manager.isReplayActive
 #endif
-                ;
+            ;
         }
 
         /// <summary>
