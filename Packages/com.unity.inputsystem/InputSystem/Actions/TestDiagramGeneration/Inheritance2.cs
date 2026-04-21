c6624b61277dd405d3c98f59e1e1bc0fa8391976
Darren Kelly <darren.kelly@unity3d.com>
2026-04-21 16:07:38 +0200

    Add commit 2.

---
+++ Packages/com.unity.inputsystem/InputSystem/Actions/TestDiagramGeneration/Inheritance2.cs
@@ -0,0 +1,7 @@
+namespace InputSystem.Actions.TestDiagramGeneration
+{
+    public class Inheritance2
+    {
+        private ClassWithinAClass classWithinAClass222 = new ClassWithinAClass();
+    }
+}
