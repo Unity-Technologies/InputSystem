f5e38afe0558c6d4cd71d02b8874017c7bd46d7d
João Freire <joao.freire@unity3d.com>
2026-04-29 10:38:22 +0300

    Revert setting capacityInBytes to zero
    Not necessary to fix things out of the scope of this PR

--- Packages/com.unity.inputsystem/InputSystem/Runtime/NativeInputRuntime.cs
+++ Packages/com.unity.inputsystem/InputSystem/Runtime/NativeInputRuntime.cs
@@ -99,7 +99,6 @@ namespace UnityEngine.InputSystem.LowLevel
                         {
                             eventBufferPtr->eventCount = 0;
                             eventBufferPtr->sizeInBytes = 0;
-                            eventBufferPtr->capacityInBytes = 0;
                         }
                     };
                 else
