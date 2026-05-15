d6d8ea4e4cea0ca98ca017bb04ad9665ff60c660
Paulius Dervinis <pauliusd@unity3d.com>
2026-05-15 14:40:07 +0300

    Address u-pr review feedback

--- Assets/Tests/InputSystem/CoreTests_Actions.cs
+++ Assets/Tests/InputSystem/CoreTests_Actions.cs
@@ -9277,6 +9277,21 @@ partial class CoreTests
             Is.EqualTo("A/LMB"));
     }
 
+    // https://issuetracker.unity3d.com/product/unity/issues/guid/UUM-141423
+    [Test]
+    [Category("Actions")]
+    public void Actions_WhenGettingDisplayTextForCompositeWithNoMatchingGroups_IsExcluded()
+    {
+        var action = new InputAction();
+
+        action.AddCompositeBinding("1DAxis")
+            .With("Negative", "<Keyboard>/a", groups: "Keyboard")
+            .With("Positive", "<Keyboard>/d", groups: "Keyboard");
+
+        Assert.That(action.GetBindingDisplayString(InputBinding.MaskByGroup("Gamepad")),
+            Is.Empty);
+    }
+
     // https://fogbugz.unity3d.com/f/cases/1321175/
     [Test]
     [Category("Actions")]
