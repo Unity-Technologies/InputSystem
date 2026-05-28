3775eb77a58c7b6b7b6f74495d20b47eeddff5a6
Aswin Gopal <ashwin.g@rythmos.com>
2026-05-28 18:56:11 +0530

    The teardown cleanup is unnecessary.

--- Assets/Tests/InputSystem/Plugins/InputForUITests.cs
+++ Assets/Tests/InputSystem/Plugins/InputForUITests.cs
@@ -65,8 +65,6 @@ public class InputForUITests : InputTestFixture
             EventProvider.ClearMockProvider();
         m_InputForUIEvents.Clear();
 
-        InputSystemProvider.SetOnRegisterActions(null);
-
         // InputSystem.actions setter throws in play mode, so we use the internal manager property here.
         InputSystem.manager.actions = storedActions;
 
