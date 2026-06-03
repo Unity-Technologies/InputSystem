b05d0fd2b3a4ac0c6946193ccc167c144e912c6b
Darren Kelly <darren.kelly@unity3d.com>
2026-06-03 20:50:12 +0200

    Attempt to fix the flaky Ubuntu tests due to running slow.

--- Tools/CI/Settings/InputSystemSettings.cs
+++ Tools/CI/Settings/InputSystemSettings.cs
@@ -135,6 +135,8 @@ public class InputSystemSettings : AnnotatedSettingsBase
 
     // Default FlavorType was changed for Win & Mac in Wrench 2.0.
     // Overriding this to keep them the same esp. for performance jobs.
+    // Ubuntu2204 is overridden to use a GPU VM to avoid software rendering slowdowns
+    // when running standalone player tests.
     private void OverridePackagePlatform(WrenchPackage package)
     {
         foreach (UnityEditor unityEditor in package.UnityEditors)
@@ -150,6 +152,10 @@ public class InputSystemSettings : AnnotatedSettingsBase
                     = new EditorPlatform(EditorPlatformType.MacOs13,
                     new Agent("package-ci/macos-13:v4", FlavorType.BuildExtraLarge, ResourceType.VmOsx));
             }
+
+            unityEditor.EditorPlatforms.Items[EditorPlatformType.Ubuntu2204]
+                = new EditorPlatform(EditorPlatformType.Ubuntu2204,
+                new Agent("package-ci/ubuntu-22.04:v4", FlavorType.BuildLarge, ResourceType.VmGpu));
         }
     }
 
