7efc12885cc620c64ee9d442b36876713a7ec609
Rita Merkl <rita.merkl@unity3d.com>
2026-07-29 14:38:39 +0200

    doc tools update

--- Tools/CI/Settings/InputSystemSettings.cs
+++ Tools/CI/Settings/InputSystemSettings.cs
@@ -21,9 +21,21 @@ public class InputSystemSettings : AnnotatedSettingsBase
     public static readonly string BranchName = "develop";
     public static readonly string InputSystemPackageName = "com.unity.inputsystem";
 
-    // Command to install .NET Framework 4.7.1 Developer Pack which is used by doctools on Windows.
-    public static readonly string NetfxInstallCmd = "%GSUDO% choco install netfx-4.7.1-devpack -y --ignore-detected-reboot --ignore-package-codes";
-    public static readonly string DoctoolsInstallCmd = "git clone --branch \"2.3.0-preview\" git@github.cds.internal.unity3d.com:unity/com.unity.package-manager-doctools.git Packages/com.unity.package-manager-doctools";
+    // PMDT (Package Manager Doctools) 2.3 has been unmaintained for ~3 years and its bundled DocFX
+    // started failing intermittently on CI (the "manual" folder silently not being generated) due to
+    // unrelated tooling backports on the Yamato images. Per USF Docs Engineering guidance, the recommended fix is
+    // to upgrade to a current PMDT release rather than continue pinning the old version.
+    //
+    // NOTE: Starting with PMDT 3.0.0, DocFX is no longer bundled with the package and must be
+    // installed separately as a dotnet tool. See:
+    // https://docs.unity3d.com/Packages/com.unity.package-manager-doctools@3.14/manual/installation.html
+    public static readonly string DocfxVersion = "2.70.0";
+
+    // Installs the DocFX version PMDT 3.x expects, as a dotnet tool, per-platform.
+    public static readonly string DocfxInstallCmdWindows = $"dotnet tool install docfx --version {DocfxVersion} --tool-path %USERPROFILE%/.pmdt";
+    public static readonly string DocfxInstallCmdUnix = $"dotnet tool install docfx --version {DocfxVersion} --tool-path $HOME/.pmdt";
+
+    public static readonly string DoctoolsInstallCmd = "git clone --branch \"3.14.8-preview\" git@github.cds.internal.unity3d.com:unity/com.unity.package-manager-doctools.git Packages/com.unity.package-manager-doctools";
 
     public WrenchPackage InputSystemPackage => Wrench.Packages[InputSystemPackageName];
 
