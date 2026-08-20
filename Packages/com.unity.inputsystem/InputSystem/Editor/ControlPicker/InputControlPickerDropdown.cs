060a26665b9b29e37b15aa4395dccad1a7380f15
Paulius Dervinis <pauliusd@unity3d.com>
2026-08-20 07:54:06 +0300

    FIX: Tune search-field inset to 8px and drop redundant comment [UUM-150217]
    Bump the PickDevice-mode search-field inset in DrawSearchFieldControl from 3px to 8px and remove the explanatory comment.
    
    Jira: https://issuetracker.unity3d.com/product/unity/issues/guid/UUM-150217

--- Packages/com.unity.inputsystem/InputSystem/Editor/ControlPicker/InputControlPickerDropdown.cs
+++ Packages/com.unity.inputsystem/InputSystem/Editor/ControlPicker/InputControlPickerDropdown.cs
@@ -600,9 +600,7 @@ namespace UnityEngine.InputSystem.Editor
                     }
                     else
                     {
-                        // In PickDevice mode there is no leading "Listen" block, so inset the search
-                        // field from the popup's left edge to match its right-side spacing.
-                        GUILayout.Space(3f);
+                        GUILayout.Space(8f);
                     }
 
                     ////FIXME: the search box doesn't clear out when listening; no idea why the new string isn't taking effect
