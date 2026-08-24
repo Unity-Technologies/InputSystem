360f9f72c744f3f8a790a743e618985cf93399d3
ChoshikaBagratee <choshika.bagratee@unity3d.com>
2026-08-24 15:19:53 -0400

    fix compile errors

--- Packages/com.unity.inputsystem/DocCodeSamples.Tests/ControlPaths.cs
+++ Packages/com.unity.inputsystem/DocCodeSamples.Tests/ControlPaths.cs
@@ -29,17 +29,17 @@ class ControlPathsExample
     {
         #region pathExamples
         // Matches all gamepads (also gamepads *based* on the Gamepad layout):
-        var allGamepads = "<Gamepad>";
+        _ = "<Gamepad>";
         // Matches the "Submit" control on all devices:
-        var allSubmit = "*/";
+        _ = "*/";
         // Matches the key that prints the "a" character on the current keyboard layout:
-        var aKey = "<Keyboard>/#(a)";
+        _ = "<Keyboard>/#(a)";
         // Matches the X axis of the left stick on a gamepad.
-        var leftStickXPath = "<Gamepad>/leftStick/x";
+        _ = "<Gamepad>/leftStick/x";
         // Matches the orientation control of the right-hand XR controller:
-        var xrOrientation = "<XRController>/orientation";
+        _ = "<XRController>/orientation";
         // Matches all buttons on a gamepad.
-        var allButtons = "<Gamepad>/<Button>";
+        _ = "<Gamepad>/<Button>";
         #endregion
     }
 }
